01e2c93983b5ab8a3c91e6f3d4c62c885f714190
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-02-21 17:01:06 +0100

    Added simulator_test_data_generator to build

---
+++ code/simulator_test_data_generator/inc/data_for_submarine_tests.hpp
@@ -0,0 +1,15 @@
+/*
+ * data_for_submarine_tests.hpp
+ *
+ *  Created on: 18 févr. 2014
+ *      Author: maroff
+ */
+
+#ifndef DATA_FOR_SUBMARINE_TESTS_HPP_
+#define DATA_FOR_SUBMARINE_TESTS_HPP_
+
+
+
+
+
+#endif /* DATA_FOR_SUBMARINE_TESTS_HPP_ */
---
+++ code/simulator_test_data_generator/src/data_for_submarine_tests.cpp
@@ -0,0 +1,10 @@
+/*
+ * data_for_submarine_tests.cpp
+ *
+ *  Created on: 18 févr. 2014
+ *      Author: maroff
+ */
+
+#include "data_for_submarine_tests.hpp"
+
+
