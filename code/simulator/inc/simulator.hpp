4d3c0dce491de1746993e61ccee6d1850ac50fcb
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-04 16:32:34 +0100

    Stubbed 'simulator'

---
+++ code/simulator/inc/simulator.hpp
@@ -0,0 +1,16 @@
+/*
+ * simulator_interface.hpp
+ *
+ *  Created on: 4 mars 2014
+ *      Author: maroff
+ */
+
+#ifndef SIMULATOR_INTERFACE_HPP_
+#define SIMULATOR_INTERFACE_HPP_
+
+#include "DataSource.hpp"
+
+DataSource make_ds(const std::string& yaml);
+void integrate(DataSource& ds, const double t0, const double t1);
+
+#endif /* SIMULATOR_INTERFACE_HPP_ */
---
+++ code/simulator/src/simulator.cpp
@@ -0,0 +1,10 @@
+/*
+ * simulator.cpp
+ *
+ *  Created on: 4 mars 2014
+ *      Author: maroff
+ */
+
+
+#include "simulator.hpp"
+
