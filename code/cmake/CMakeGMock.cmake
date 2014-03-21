1abea092a84d0fbfbada9b1252b0846a603f0bf3
jacquenot <jacquenot@debian-gj>
2014-03-21 17:47:38 +0100

    Fixed compilation issues under Debian 64 bits

--- code/cmake/CMakeGMock.cmake
+++ code/cmake/CMakeGMock.cmake
@@ -1,12 +1,12 @@
-IF(WIN32)
+#IF(WIN32)
     ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=0")
     ADD_DEFINITIONS("-Dgtest_disable_pthreads=0")
     SET(gtest_disable_pthreads ON  CACHE BOOL "")
-ELSE()
-    ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=1")
-    ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
-    SET(gtest_disable_pthreads OFF CACHE BOOL "")
-ENDIF()
+#ELSE()
+#    ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=1")
+#    ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
+#    SET(gtest_disable_pthreads OFF CACHE BOOL "")
+#ENDIF()
 ADD_SUBDIRECTORY(gmock)
 
 SET(GMOCK_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/gmock/include
