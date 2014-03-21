1d6b7c976294bdd4060801fff92f9476cefd2523
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-21 13:17:22 +0100

    Added observers

---
+++ code/simulator/inc/Observers.hpp
@@ -0,0 +1,83 @@
+/*
+ * Observers.hpp
+ *
+ *  Created on: 21 mars 2014
+ *      Author: maroff
+ */
+
+#ifndef OBSERVERS_HPP_
+#define OBSERVERS_HPP_
+
+template <typename IteratorType, typename Stream> void serialize(Stream& os, const IteratorType& begin, const IteratorType& end)
+{
+    if (end-begin>0)
+    {
+        IteratorType it = begin;
+        os << *it;++it;
+        for (;it!=end;++it)
+        {
+            os << "," << *it;
+        }
+        os << "\n";
+    }
+}
+
+template <typename SystemType, typename StateType> class DefaultObserver
+{
+    public:
+        DefaultObserver(std::ostream& os_) : os(os_), initialized(false) {}
+        void observe(const SystemType& sys, const double t)
+        {
+            const StateType x = sys.state;
+            if (not(initialized))
+            {
+                serialize_title(x.size());
+                initialized = true;
+            }
+            StateType v;
+            v.push_back(t);
+            v.insert(v.end(), x.begin(), x.end());
+            serialize(os, v.begin(), v.end());
+        }
+
+    private:
+
+        void serialize_title(const size_t nb_of_states)
+        {
+            std::vector<std::string> v;
+            v.push_back("t");
+            for (size_t i = 0 ; i < nb_of_states ; ++i)
+            {
+                std::stringstream ss;
+                ss << "x" << i;
+                v.push_back(ss.str());
+            }
+            serialize(os, v.begin(), v.end());
+        }
+
+        std::ostream& os;
+        bool initialized;
+};
+
+template <typename SystemType> class VectorObserver
+{
+    public:
+        VectorObserver() : v(std::vector<std::pair<double,double> >()) {}
+        void observe(const SystemType& sys, const double t)
+        {
+            v.push_back(std::make_pair(t, sys.state[0]));
+        }
+
+        std::vector<std::pair<double,double> > get() const
+        {
+            return v;
+        }
+
+    private:
+        std::vector<std::pair<double,double> > v;
+};
+
+
+
+
+#endif /* OBSERVERS_HPP_ */
