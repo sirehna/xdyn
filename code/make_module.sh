fcdfe51b53707739fd7bce0424099919bfa96ff7
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-25 14:50:06 +0100

    Fixed new module template

--- code/make_module.sh
+++ code/make_module.sh
@@ -21,17 +21,15 @@ echo "add_subdirectory(unit_tests)" >> $1/CMakeLists.txt
 echo "# ------8<---[LINES TO MODIFY WHEN CHANGING MODULE]----->8-----" >> $1/unit_tests/CMakeLists.txt
 echo "set(MODULE_UNDER_TEST $1)" >> $1/unit_tests/CMakeLists.txt
 echo "project (\${MODULE_UNDER_TEST}_tests)" >> $1/unit_tests/CMakeLists.txt
-echo "FILE(GLOB SRC src/DataSourceTest.cpp" >> $1/unit_tests/CMakeLists.txt
-echo "              src/SignalContainerTest.cpp" >> $1/unit_tests/CMakeLists.txt
-echo "              src/TypeCoercionTest.cpp)" >> $1/unit_tests/CMakeLists.txt
+echo "FILE(GLOB SRC src/Test.cpp" >> $1/unit_tests/CMakeLists.txt
+echo "              )" >> $1/unit_tests/CMakeLists.txt
 echo "# ------8<---------------------------------------------->8-----" >> $1/unit_tests/CMakeLists.txt
 echo "" >> $1/unit_tests/CMakeLists.txt
 echo "# Include directories" >> $1/unit_tests/CMakeLists.txt
-echo "include_directories(SYSTEM inc)" >> $1/unit_tests/CMakeLists.txt
-#echo "include_directories(\${\${MODULE_UNDER_TEST}_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
-#echo "include_directories(\${random_data_generator_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
-#echo "include_directories(\${test_macros_SOURCE_DIR})" >> $1/unit_tests/CMakeLists.txt
-#echo "include_directories(SYSTEM \${GTEST_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
-#echo "include_directories(SYSTEM \${GMOCK_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
-#echo "" >> $1/unit_tests/CMakeLists.txt
-#echo "add_library (\${PROJECT_NAME} OBJECT \${SRC})" >> $1/unit_tests/CMakeLists.txt
+echo "include_directories(inc)" >> $1/unit_tests/CMakeLists.txt
+echo "include_directories(\${\${MODULE_UNDER_TEST}_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
+echo "include_directories(\${random_data_generator_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
+echo "include_directories(\${test_macros_SOURCE_DIR})" >> $1/unit_tests/CMakeLists.txt
+echo "include_directories(SYSTEM \${GTEST_INCLUDE_DIRS})" >> $1/unit_tests/CMakeLists.txt
+echo "" >> $1/unit_tests/CMakeLists.txt
+echo "add_library (\${PROJECT_NAME} OBJECT \${SRC})" >> $1/unit_tests/CMakeLists.txt
