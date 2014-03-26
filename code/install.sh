399313f89630d2f552c7b7a12f2b1007a2cbb174
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-03-26 11:41:28 +0100

    Update build and install name directory wrt 32/64
    Comment also some compilation options

--- code/install.sh
+++ code/install.sh
@@ -7,6 +7,15 @@ else
     buildType=$1
 fi
 
+if [[ `gcc -dumpmachine` == x86_64* ]]
+then
+    echo "Target 64 bits"
+    suffix=64
+else
+    echo "Target 32 bits"
+    suffix=32
+fi
+
 buildType="$(tr '[:lower:]' '[:upper:]' <<< ${buildType:0:1})$(tr '[:upper:]' '[:lower:]' <<< ${buildType:1})"
 
 if [ `uname -s` = "Linux" ];
@@ -16,9 +25,9 @@ else
     cmakeGenerator="MSYS Makefiles"
 fi
 
-mkdir -p ../build${buildType}
-cd ../build${buildType}
-cmake ../code -G"${cmakeGenerator}" -DCMAKE_BUILD_TYPE=${buildType}
+mkdir -p ../build${buildType}${suffix}
+cd ../build${buildType}${suffix}
+cmake ../code -G"${cmakeGenerator}" -DCMAKE_BUILD_TYPE=${buildType} -DINSTALL_PREFIX=../install${buildType}${suffix}
 make install
 make test
 
