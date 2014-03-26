61894cc52a9637de782d1aaf27ede946cb512e83
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-03-26 16:35:24 +0100

    Update the GMock options (PThread and PosixRE)

--- code/cmake/CMakeGMock.cmake
+++ code/cmake/CMakeGMock.cmake
@@ -1,12 +1,14 @@
-#IF(WIN32)
-    ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=0")
-    ADD_DEFINITIONS("-Dgtest_disable_pthreads=0")
-    SET(gtest_disable_pthreads ON  CACHE BOOL "")
-#ELSE()
-#    ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=1")
-#    ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
-#    SET(gtest_disable_pthreads OFF CACHE BOOL "")
-#ENDIF()
+ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=0")
+IF(ARCH64_BITS AND UNIX AND NOT WIN32) # If on Linux
+    MESSAGE(STATUS "1")
+    #ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
+    #SET(gtest_disable_pthreads ON CACHE BOOL "")
+ELSE()
+    MESSAGE(STATUS "2")
+    ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
+    SET(gtest_disable_pthreads ON CACHE BOOL "")
+ENDIF()
+
 ADD_SUBDIRECTORY(gmock)
 
 SET(GMOCK_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/gmock/include
