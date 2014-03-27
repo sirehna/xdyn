e3fdbf750a3ffdd0268baa7c24b07b3f754907c5
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-27 08:34:53 +0100

    Fixed gtest flags under win 32

--- code/cmake/CMakeGMock.cmake
+++ code/cmake/CMakeGMock.cmake
@@ -1,11 +1,7 @@
 ADD_DEFINITIONS("-DGTEST_HAS_POSIX_RE=0")
-IF(ARCH64_BITS AND UNIX AND NOT WIN32) # If on Linux
-    MESSAGE(STATUS "1")
-    #ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
-    #SET(gtest_disable_pthreads ON CACHE BOOL "")
+IF(UNIX AND NOT WIN32) # If on Linux
+    SET(gtest_disable_pthreads OFF CACHE BOOL "")
 ELSE()
-    MESSAGE(STATUS "2")
-    ADD_DEFINITIONS("-Dgtest_disable_pthreads=1")
     SET(gtest_disable_pthreads ON CACHE BOOL "")
 ENDIF()
 
