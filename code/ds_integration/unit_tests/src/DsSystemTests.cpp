82b320380f2b6c45b8525420a274e217db96cd68
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-27 14:37:39 +0100

    Added tests to check integrate throws if DataSource is incomplete

--- code/ds_integration/unit_tests/src/DsSystemTests.cpp
+++ code/ds_integration/unit_tests/src/DsSystemTests.cpp
@@ -51,3 +51,31 @@ TEST_F(DsSystemTests, example)
 //! [DsSystemTests expected output]
 //! [DsSystemTests expected output]
 }
+
+TEST_F(DsSystemTests, throws_if_initial_time_step_is_not_defined)
+{
+    DataSource ds;
+    SET(ds, falling_ball::zdotdot, -9.81);
+    SET(ds, falling_ball::zdot, 0);
+    SET(ds, falling_ball::z, 0);
+    SET(ds, simulator_base::stepper, solver::EULER);
+    DEFINE_DERIVATIVE(falling_ball::z, falling_ball::zdot, ds);
+    DEFINE_DERIVATIVE(falling_ball::zdot, falling_ball::zdotdot, ds);
+    DsCsvObserver observer(std::cout);
+
+    ASSERT_THROW(integrate(ds, 0, 10, observer),DataSourceException);
+}
+
+TEST_F(DsSystemTests, throws_if_stepper_is_not_defined)
+{
+    DataSource ds;
+    SET(ds, falling_ball::zdotdot, -9.81);
+    SET(ds, falling_ball::zdot, 0);
+    SET(ds, falling_ball::z, 0);
+    SET(ds, simulator_base::initial_time_step, 1);
+    DEFINE_DERIVATIVE(falling_ball::z, falling_ball::zdot, ds);
+    DEFINE_DERIVATIVE(falling_ball::zdot, falling_ball::zdotdot, ds);
+    DsCsvObserver observer(std::cout);
+
+    ASSERT_THROW(integrate(ds, 0, 10, observer),DataSourceException);
+}
