70c1fb27f2f42fe915c77efa979ccaf2b59c36ec
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-03-31 14:56:06 +0200

    Added a 'non-observer' for the default integrate function

--- code/ds_integration/src/DsSolve.cpp
+++ code/ds_integration/src/DsSolve.cpp
@@ -9,6 +9,8 @@
 #include <boost/numeric/odeint/stepper/runge_kutta4.hpp>
 #include <boost/numeric/odeint/stepper/runge_kutta_cash_karp54.hpp>
 
+#include <iostream>
+
 #include "DsSolve.hpp"
 #include "solve.hpp"
 #include "DsSystemMacros.hpp"
@@ -19,6 +21,16 @@ typedef std::vector<double> StateType;
 typedef boost::numeric::odeint::euler<StateType> EulerStepper;
 typedef boost::numeric::odeint::runge_kutta4<StateType> RK4Stepper;
 typedef boost::numeric::odeint::runge_kutta_cash_karp54<StateType> RKCK;
+
+class NoObservation : public DsObserver
+{
+    public:
+        NoObservation() : DsObserver(std::cout) {}
+        void observe(const DSSystem& , const double )
+        {
+
+        }
+};
 
 struct Pendulum
 {
