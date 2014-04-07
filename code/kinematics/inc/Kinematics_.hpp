b317a80346d7212cc06964cd551ec2d72b18935f
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-07 15:13:00 +0200

    Renaming kinematics -> Kinematics

---
+++ code/kinematics/inc/Kinematics_.hpp
@@ -0,0 +1,14 @@
+/*
+ * kinematics.hpp
+ *
+ *  Created on: 1 avr. 2014
+ *      Author: maroff
+ */
+
+#ifndef KINEMATICS_HPP_
+#define KINEMATICS_HPP_
+
+#include "Velocity.hpp"
+#include "Frame.hpp"
+
+#endif /* KINEMATICS_HPP_ */
