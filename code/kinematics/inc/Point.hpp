e4a6575ca2c7c90f98d6a09524de5dda55631ccc
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-07 15:28:44 +0200

    Stubbed Point

---
+++ code/kinematics/inc/Point.hpp
@@ -0,0 +1,35 @@
+/*
+ * Point.hpp
+ *
+ *  Created on: 7 avr. 2014
+ *      Author: maroff
+ */
+
+#ifndef POINT_HPP_
+#define POINT_HPP_
+
+#include <string>
+
+/** \author cec
+ *  \brief 
+ *  \details 
+ *  \ingroup kinematics
+ *  \section ex1 Example
+ *  \snippet kinematics/unit_tests/src/PointTest.cpp PointTest example
+ *  \section ex2 Expected output
+ *  \snippet kinematics/unit_tests/src/PointTest.cpp PointTest expected output
+ */
+class Point
+{
+    public:
+        Point(const double x, const double y, const double z, const std::string& frame);
+        double x;
+        double y;
+        double z;
+        std::string frame;
+
+    private:
+        Point();
+};
+
+#endif /* POINT_HPP_ */
---
+++ code/kinematics/src/Point.cpp
@@ -0,0 +1,6 @@
+#include "Point.hpp"
+
+Point::Point(const double x_, const double y_, const double z_, const std::string& frame_) : x(x_), y(y_), z(z_), frame(frame_)
+{
+
+}
