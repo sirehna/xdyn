9eb8c7dbb3189b5039ae5afe4e826cf17d3d129b
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-07 15:20:43 +0200

    Trying to fix fantom file pb in git

---
+++ code/kinematics/inc/Kinematics.hpp
@@ -0,0 +1,25 @@
+/*
+ * Kinematics.hpp
+ *
+ *  Created on: 7 avr. 2014
+ *      Author: maroff
+ */
+
+#ifndef KINEMATICS_HPP_
+#define KINEMATICS_HPP_
+
+/** \author cec
+ *  \brief Interface to robwork
+ *  \details 
+ *  \ingroup kinematics
+ *  \section ex1 Example
+ *  \snippet kinematics/unit_tests/src/KinematicsTest.cpp KinematicsTest example
+ *  \section ex2 Expected output
+ *  \snippet kinematics/unit_tests/src/KinematicsTest.cpp KinematicsTest expected output
+ */
+class Kinematics
+{
+
+};
+
+#endif /* KINEMATICS_HPP_ */
