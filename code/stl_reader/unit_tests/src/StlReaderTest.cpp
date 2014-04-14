6fa938ab0cd0819271399d09cdf0edda30b02e49
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-04-14 16:35:52 +0200

    Stubbed read_stl

--- code/stl_reader/unit_tests/src/StlReaderTest.cpp
+++ code/stl_reader/unit_tests/src/StlReaderTest.cpp
@@ -1,6 +1,8 @@
+#include "StlReader.hpp"
+#include "STL_data.hpp"
 #include "StlReaderTest.hpp"
 
 TEST_F(StlReaderTest, example)
 {
-
+	const VectorOfPoint3dTriplet res = read_stl(stl_single_facet());
 }
