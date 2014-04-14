3ef7fd3bf97134c0de2cecca0a94e6939763fb06
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-04-14 17:06:39 +0200

    Added a stl dataset for 3 facets

--- code/stl_reader/unit_tests/src/StlReaderTest.cpp
+++ code/stl_reader/unit_tests/src/StlReaderTest.cpp
@@ -8,7 +8,6 @@ TEST_F(StlReaderTest, number_of_facets_should_be_correct_for_a_single_facet_file
 	const VectorOfPoint3dTriplet facets = read_stl(stl_single_facet());
 	ASSERT_EQ(1, facets.size());
 }
-
 TEST_F(StlReaderTest, number_of_facets_should_be_correct_for_a_three_facet_file)
 {
 	const VectorOfPoint3dTriplet facets = read_stl(stl_three_facets());
