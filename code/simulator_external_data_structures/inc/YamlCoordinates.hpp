ffa6a28f786b32e3583050970dd6eab38d785e47
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-16 13:02:06 +0200

    Created YamlCoordinates structure

---
+++ code/simulator_external_data_structures/inc/YamlCoordinates.hpp
@@ -0,0 +1,19 @@
+/*
+ * YamlCoordinates.hpp
+ *
+ *  Created on: 16 avr. 2014
+ *      Author: cady
+ */
+
+#ifndef YAMLCOORDINATES_HPP_
+#define YAMLCOORDINATES_HPP_
+
+struct YamlCoordinates
+{
+    YamlCoordinates();
+    double x;
+    double y;
+    double z;
+};
+
+#endif /* YAMLCOORDINATES_HPP_ */
---
+++ code/simulator_external_data_structures/src/YamlCoordinates.cpp
@@ -0,0 +1,13 @@
+/*
+ * YamlCoordinates.cpp
+ *
+ *  Created on: 16 avr. 2014
+ *      Author: cady
+ */
+
+#include "YamlCoordinates.hpp"
+
+YamlCoordinates::YamlCoordinates() : x(0), y(0), z(0)
+{
+
+}
