a61299b8d08363adafec12510a26e9b55e5ee5d5
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-04-16 16:27:29 +0200

    Added a unit cube test and split the unit test data

---
+++ code/mesh/unit_tests/inc/TriMeshTestData.hpp
@@ -0,0 +1,10 @@
+#ifndef TRIMESHTESTDATA_HPP
+#define TRIMESHTESTDATA_HPP
+
+#include "GeometricTypes3d.hpp"
+
+Point3dTriplet generate_a_single_triangle();
+Point3dTriplet generate_a_degenerated_triangle();
+VectorOfPoint3dTriplet generate_a_unit_cube();
+
+#endif
