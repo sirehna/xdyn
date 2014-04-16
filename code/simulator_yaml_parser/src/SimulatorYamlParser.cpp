3d007cf69a34bcf8f13d52e1419d0048641be21f
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-16 11:49:08 +0200

    SimulatorYamlParser throws if yaml string is empty

--- code/simulator_yaml_parser/src/SimulatorYamlParser.cpp
+++ code/simulator_yaml_parser/src/SimulatorYamlParser.cpp
@@ -7,9 +7,15 @@
 
 #include "SimulatorYamlParser.hpp"
 #include "yaml.h"
+#include "SimulatorYamlParserException.hpp"
 
 SimulatorYamlParser::SimulatorYamlParser(const std::string& data) : YamlParser(data), node(new YAML::Node())
 {
+    convert_stream_to_yaml_node(contents, *node);
+    if (node->size() == 0)
+    {
+        THROW(__PRETTY_FUNCTION__, SimulatorYamlParserException, "Something is wrong with the YAML data: no YAML nodes were detected by the YAML parser.");
+    }
 }
 
 YamlRotation SimulatorYamlParser::get_rotation()
