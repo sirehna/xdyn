bfe9f855e2802180c437002bc97d963c24625836
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-04-17 15:25:31 +0200

    Merge operations

--- code/stl_reader/unit_tests/src/StlReaderTest.cpp
+++ code/stl_reader/unit_tests/src/StlReaderTest.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "StlReader.hpp"
 #include "StlReaderException.hpp"
 #include "STL_data.hpp"
@@ -70,11 +69,4 @@ TEST_F(StlReaderTest, should_throw_an_exception_when_end_file_is_not_reached)
 {
 	const std::string data("solid MYSOLID\nfacet normal 0.4 0.4 0.2\nouterloop\n");
 	ASSERT_THROW(read_stl(data),StlReaderException);
-=======
-#include "StlReaderTest.hpp"
-
-TEST_F(StlReaderTest, example)
-{
-
->>>>>>> 76267ed94b4d397077d5e77b61863f3dde24d7fd
 }
--- code/test_data_generator/src/STL_data.cpp
+++ code/test_data_generator/src/STL_data.cpp
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-=======
-/*
- * STL_data.cpp
- *
- *  Created on: 14 avr. 2014
- *      Author: jacquenot
- */
-
-
-
->>>>>>> 76267ed94b4d397077d5e77b61863f3dde24d7fd
 #include <sstream>
 #include "STL_data.hpp"
 
@@ -28,7 +16,6 @@ std::string stl_single_facet()
     return data.str();
 }
 
-<<<<<<< HEAD
 std::string stl_three_facets()
 {
     std::stringstream data;
@@ -59,8 +46,6 @@ std::string stl_three_facets()
     return data.str();
 }
 
-=======
->>>>>>> 76267ed94b4d397077d5e77b61863f3dde24d7fd
 std::string stl_cube()
 {
     std::stringstream data;
