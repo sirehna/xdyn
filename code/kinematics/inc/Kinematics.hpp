68f0cddfd954e7e4764cb6754a858a5c61aa405b
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-23 11:36:02 +0200

    [cleanup_kinematics] Re-created Kinematics class
    Started from zero.
    Stubbed add(Transform).

--- code/kinematics/inc/Kinematics.hpp
+++ code/kinematics/inc/Kinematics.hpp
@@ -9,13 +9,21 @@
 #define KINEMATICS_HPP_
 
 #include <string>
-#include "rotation_matrix_builders.hpp"
-#include "Point.hpp"
 #include <tr1/memory>
 
+namespace kinematics
+{
+    class Transform;
+}
+
 /** \author cec
- *  \brief Interface to robwork
- *  \details
+ *  \date 23 avr. 2014, 10:34:05
+ *  \brief Lazily computes the optimal transform between two reference frames.
+ *  \details When user requests a transform between two frames:
+ *           - Looks for it in the underlying internal DataSource object
+ *           - If the corresponding transform wasn't found, see if it's the inverse of a known transform
+ *           - If it isn't, see if it can be computed using a PathComputer object.
+ *           - Otherwise throw a KinematicsException.
  *  \ingroup kinematics
  *  \section ex1 Example
  *  \snippet kinematics/unit_tests/src/KinematicsTest.cpp KinematicsTest example
@@ -25,13 +33,16 @@
 class Kinematics
 {
     public:
-        Kinematics();
-        void add_frame(const std::string& frame_of_reference, const Point& P, const RotationMatrix& R);
-        Point project(const Point& P, const std::string& destination_frame);
+        /** \author cec
+          *  \date 23 avr. 2014, 10:45:40
+          *  \author cec
+          *  \brief Adds a transform between two reference frames to *this
+          *  \snippet kinematics/unit_tests/src/KinematicsTest.cpp KinematicsTest add_example
+          */
+        void add(const kinematics::Transform& transform_to_add //!< Transform to add
+                );
 
     private:
-        class Impl;
-        std::tr1::shared_ptr<Impl> pimpl;
 };
 
 #endif /* KINEMATICS_HPP_ */
--- code/kinematics/src/Kinematics.cpp
+++ code/kinematics/src/Kinematics.cpp
@@ -1,48 +1,8 @@
 #include "Kinematics.hpp"
 #include "KinematicsException.hpp"
 #include "DataSource.hpp"
-#include "test_macros.hpp"
 
-class Kinematics::Impl
-{
-    public:
-        Impl() :ds(DataSource()) {}
-
-        ~Impl(){}
-        Impl& operator=(const Impl& rhs)
-        {
-            if (this!=&rhs)
-            {
-                ds = rhs.ds;
-            }
-            return *this;
-        }
-
-        Impl(const Impl& rhs) : ds(rhs.ds)
-        {
-        }
-
-        void add_frame(const std::string& , const Point& , const RotationMatrix& )
-        {
-
-        }
-
-
-    private:
-        DataSource ds;
-};
-
-Kinematics::Kinematics() : pimpl(std::tr1::shared_ptr<Impl>(new Impl()))
+void Kinematics::add(const kinematics::Transform& )
 {
 
-}
-
-void Kinematics::add_frame(const std::string& name, const Point& P, const RotationMatrix& R)
-{
-    pimpl->add_frame(name, P, R);
-}
-
-Point Kinematics::project(const Point& , const std::string& frame)
-{
-    return Point(frame, 0, 0, 0);
 }
