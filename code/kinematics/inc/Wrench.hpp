01f0307343750faded73c2a0870dfaca04d2b8fb
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-24 12:41:27 +0200

    [wrench] Added a Wrench class

---
+++ code/kinematics/inc/Wrench.hpp
@@ -0,0 +1,38 @@
+/*
+ * Wrench.hpp
+ *
+ *  Created on: 24 avr. 2014
+ *      Author: cady
+ */
+
+#ifndef WRENCH_HPP_
+#define WRENCH_HPP_
+
+#include <string>
+
+/** \author cec
+ *  \date 24 avr. 2014, 12:35:30
+ *  \brief Stores a force & a torque, projected in a given reference frame.
+ *  \ingroup kinematics
+ *  \section ex1 Example
+ *  \snippet kinematics/unit_tests/src/WrenchTest.cpp WrenchTest example
+ *  \section ex2 Expected output
+ *  \snippet kinematics/unit_tests/src/WrenchTest.cpp WrenchTest expected output
+ */
+class Wrench
+{
+    public:
+        Wrench(const std::string& frame);
+        double X;
+        double Y;
+        double Z;
+        double K;
+        double M;
+        double N;
+
+    private:
+        Wrench(); // Disabled because we must specify a frame for the coordinates
+        std::string frame;
+};
+
+#endif /* WRENCH_HPP_ */
---
+++ code/kinematics/src/Wrench.cpp
@@ -0,0 +1,12 @@
+/*
+ * Wrench.cpp
+ *
+ *  Created on: 24 avr. 2014
+ *      Author: cady
+ */
+
+#include "Wrench.hpp"
+
+Wrench::Wrench(const std::string& frame_) : X(0), Y(0), Z(0), K(0), M(0), N(0), frame(frame_)
+{
+}
