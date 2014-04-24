cb9a1804388d5b5af0936676b3725e068650333d
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-24 08:16:24 +0200

    Fixed script permissions under Linux

--- code/linux_build.sh
+++ code/linux_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir build
-./foreach.sh externalDependencies.txt svn_export.sh $2 $3
+./foreach.sh externalDependencies.txt svn_export.sh $1 $2
 cd internal_solver_validation
-./foreach.sh externalDependencies.txt svn_export.sh $2 $3
+./foreach.sh externalDependencies.txt svn_export.sh $1 $2
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 cd ..
