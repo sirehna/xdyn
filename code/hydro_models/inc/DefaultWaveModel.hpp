e434fac586f11da5e9b4003454c4fde9dec194d8
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-24 12:29:29 +0200

    [default wave model] Added class description to DefaultWaveModel & WaveModelInterface

--- code/hydro_models/inc/DefaultWaveModel.hpp
+++ code/hydro_models/inc/DefaultWaveModel.hpp
@@ -10,6 +10,20 @@
 
 #include "WaveModelInterface.hpp"
 
+/** \author cec
+ *  \date 24 avr. 2014, 10:15:24
+ *  \brief Flat sea surface.
+ *  \details The relative height is zwave-z, so a negative
+ *           value implies the point is above the water and a
+ *           positive value implies it's underneath. This convention
+ *           was chosen because it should simplify the computation of the
+ *           hydrostatic forces (no sign change).
+ *  \ingroup hydro_models
+ *  \section ex1 Example
+ *  \snippet hydro_models/unit_tests/src/DefaultWaveModelTest.cpp DefaultWaveModelTest example
+ *  \section ex2 Expected output
+ *  \snippet hydro_models/unit_tests/src/DefaultWaveModelTest.cpp DefaultWaveModelTest expected output
+ */
 class DefaultWaveModel : public WaveModelInterface
 {
     public:
--- code/hydro_models/inc/WaveModelInterface.hpp
+++ code/hydro_models/inc/WaveModelInterface.hpp
@@ -10,6 +10,16 @@
 
 class Point;
 
+/** \author cec
+ *  \date 24 avr. 2014, 10:28:25
+ *  \brief Interface to wave models
+ *  \details It is this base class' responsibility to change the reference frame of the input point.
+ *  \ingroup hydro_models
+ *  \section ex1 Example
+ *  \snippet hydro_models/unit_tests/src/WaveModelInterfaceTest.cpp WaveModelInterfaceTest example
+ *  \section ex2 Expected output
+ *  \snippet hydro_models/unit_tests/src/WaveModelInterfaceTest.cpp WaveModelInterfaceTest expected output
+ */
 class WaveModelInterface
 {
     public:
