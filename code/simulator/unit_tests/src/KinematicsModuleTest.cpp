85aa435fcc57775ea35b201be6ea7dc86159f568
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-25 15:19:13 +0200

    [kin_module] Stubbed KinematicsModuleTest

---
+++ code/simulator/unit_tests/src/KinematicsModuleTest.cpp
@@ -0,0 +1,35 @@
+/*
+ * KinematicsModuleTest.cpp
+ *
+ *  Created on: Apr 25, 2014
+ *      Author: cady
+ */
+
+#include "KinematicsModuleTest.hpp"
+
+KinematicsModuleTest::KinematicsModuleTest() : a(DataGenerator(18914214))
+{
+}
+
+KinematicsModuleTest::~KinematicsModuleTest()
+{
+}
+
+void KinematicsModuleTest::SetUp()
+{
+}
+
+void KinematicsModuleTest::TearDown()
+{
+}
+
+TEST_F(KinematicsModuleTest, example)
+{
+//! [KinematicsModuleTest example]
+//! [KinematicsModuleTest example]
+//! [KinematicsModuleTest expected output]
+//! [KinematicsModuleTest expected output]
+}
+
+
+
