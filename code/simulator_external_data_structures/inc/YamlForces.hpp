21fc0b9f700b6c81175d77ee0c22a3342aff95d8
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-28 17:22:18 +0200

    [gravity] Added external data structures for forces.
    The YAML section of the force models is not parsed with the rest: it's the
    builder's job to call the right parser, based on the value of the 'model'
    string.

---
+++ code/simulator_external_data_structures/inc/YamlForces.hpp
@@ -0,0 +1,18 @@
+/*
+ * Forces.hpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: cady
+ */
+
+#ifndef YAMLFORCES_HPP_
+#define YAMLFORCES_HPP_
+
+struct GravityParameters
+{
+    GravityParameters();
+    double g;
+};
+
+
+#endif /* YAMLFORCES_HPP_ */
---
+++ code/simulator_external_data_structures/src/YamlForces.cpp
@@ -0,0 +1,13 @@
+/*
+ * Forces.cpp
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: cady
+ */
+
+#include "YamlForces.hpp"
+
+
+GravityParameters::GravityParameters() : g(0)
+{
+}
