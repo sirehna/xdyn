c23f6dfb27605ba45afcbcd93d5b5de4f42f496e
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-04-29 16:39:44 +0200

    [hydrostatic] Renamed polygon->points in mesh_manipulations

--- code/mesh/src/mesh_manipulations.cpp
+++ code/mesh/src/mesh_manipulations.cpp
@@ -32,40 +32,40 @@ double area(const VectorOfPoints& points)
     return a;
 }
 
-Eigen::Vector3d barycenter(const VectorOfPoints& polygon)
+Eigen::Vector3d barycenter(const VectorOfPoints& points)
 {
     Eigen::Vector3d xyz;
-    xyz = (polygon[0]+polygon[1]+polygon[2])/3.0;
+    xyz = (points[0]+points[1]+points[2])/3.0;
     return xyz;
 }
 
-Eigen::Vector3d unit_normal(const VectorOfPoints& polygon)
+Eigen::Vector3d unit_normal(const VectorOfPoints& points)
 {
-    const Eigen::Vector3d n = normal(polygon);
+    const Eigen::Vector3d n = normal(points);
     const double norm = n.norm();
     if (norm<1000*std::numeric_limits<double>::epsilon())
     {
         std::stringstream ss;
         ss << "Input is degenerated: cannot compute unit normal vector. The polygon is:" << std::endl;
-        for (size_t i = 0 ; i < polygon.size() ; ++i)
+        for (size_t i = 0 ; i < points.size() ; ++i)
         {
-          ss << "p[" << i << "] = " << polygon[i].transpose() << std::endl;
+          ss << "p[" << i << "] = " << points[i].transpose() << std::endl;
         }
         THROW(__PRETTY_FUNCTION__, MeshException, ss.str());
     }
     return n/norm;
 }
 
-Eigen::Vector3d normal(const VectorOfPoints& polygon)
+Eigen::Vector3d normal(const VectorOfPoints& points)
 {
-    if (polygon.size() < 3)
+    if (points.size() < 3)
     {
         std::stringstream ss;
         ss << "Need at least three points to define a surface: cannot compute normal vector. Input has "
-           << polygon.size() << " points.";
+           << points.size() << " points.";
        THROW(__PRETTY_FUNCTION__, MeshException, ss.str());
     }
-    const Eigen::Vector3d n1(polygon[1]-polygon[0]);
-    const Eigen::Vector3d n2(polygon[2]-polygon[0]);
+    const Eigen::Vector3d n1(points[1]-points[0]);
+    const Eigen::Vector3d n2(points[2]-points[0]);
     return n1.cross(n2);
 }
