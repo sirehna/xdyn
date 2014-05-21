41df59d4cd6b1d8ac20f54b156cb8cc45778736a
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-05-21 11:46:40 +0200

    [hydrostatic] Checked we cannot build mesh if facets have exactly one or two points

--- code/mesh/unit_tests/src/MeshBuilderTest.cpp
+++ code/mesh/unit_tests/src/MeshBuilderTest.cpp
@@ -109,3 +109,9 @@ TEST_F(MeshBuilderTest, two_triangles)
     ASSERT_EQ(3,m.facets.at(1).index.at(1));
     ASSERT_EQ(1,m.facets.at(1).index.at(2));
 }
+
+TEST_F(MeshBuilderTest, cannot_build_mesh_if_facets_have_one_or_two_points)
+{
+    ASSERT_THROW(MeshBuilder(n_gone(1)).build(),MeshException);
+    ASSERT_THROW(MeshBuilder(n_gone(2)).build(),MeshException);
+}
