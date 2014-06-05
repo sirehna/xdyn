45b72883beeec0eb01b09546239938a46973a132
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-06-05 13:04:56 +0200

    [6DOF] State derivatives are now computed

--- code/simulator/src/StateDerivativesModule.cpp
+++ code/simulator/src/StateDerivativesModule.cpp
@@ -10,6 +10,9 @@
 #include "DataSource.hpp"
 #include "pairwise_sum.hpp"
 #include "coriolis_and_centripetal.hpp"
+#include "Kinematics.hpp"
+#include "Transform.hpp"
+#include <Eigen/Geometry> // For Eigen::Quaternion
 
 StateDerivativesModule::StateDerivativesModule(DataSource* const data_source, const std::string& body, const std::vector<std::string>& forces_) : SimulatorModuleInterface(data_source, "sum of forces", body), forces(forces_)
 {
@@ -38,13 +41,14 @@ void StateDerivativesModule::update() const
     const Matrix6x6 Mrb = ds->get<Matrix6x6>(customize("solid body inertia"));
     const Matrix6x6 Minv = ds->get<Matrix6x6>(customize("inverse of the total inertia"));
     const Point P = ds->get<Point>(customize("G"));
-    const Eigen::Vector3d uvw(ds->get<double>(customize("u")),
-                              ds->get<double>(customize("v")),
-                              ds->get<double>(customize("w")));
+    const std::tr1::shared_ptr<Kinematics> k = ds->get<std::tr1::shared_ptr<Kinematics> >("kinematics");
+    const Eigen::Vector3d uvw_in_body_frame(ds->get<double>(customize("u")),
+                                            ds->get<double>(customize("v")),
+                                            ds->get<double>(customize("w")));
     const Eigen::Vector3d pqr(ds->get<double>(customize("p")),
                               ds->get<double>(customize("q")),
                               ds->get<double>(customize("r")));
-    std::vector<Wrench> f(1, coriolis_and_centripetal(P,Mrb,uvw, pqr));
+    std::vector<Wrench> f(1, (-1.)*coriolis_and_centripetal(P,Mrb,uvw_in_body_frame, pqr));
     ds->set<Wrench>(customize("coriolis and centripetal forces"), f.front());
     if (not(ds->read_only()))
     {
@@ -53,4 +57,27 @@ void StateDerivativesModule::update() const
     }
     if (not(ds->read_only())) sum = sum::pairwise(f);
     ds->set(customize("sum of forces"), sum);
+    Eigen::Matrix<double,6,1> dstates_dt = ds->read_only() ? Eigen::Matrix<double,6,1>::Constant(0) : Eigen::Matrix<double,6,1>(Minv*sum.to_vector());
+    const RotationMatrix R = ds->read_only() ? Eigen::Matrix<double, 3, 3>::Identity() : k->get(body, "NED").get_rot();
+    const Eigen::Vector3d uvw_in_ned_frame(R*Point(body,uvw_in_body_frame).v);
+    ds->set<double>(customize("dx_dt"), (double)uvw_in_ned_frame(0));
+    ds->set<double>(customize("dy_dt"), (double)uvw_in_ned_frame(1));
+    ds->set<double>(customize("dz_dt"), (double)uvw_in_ned_frame(2));
+    ds->set<double>(customize("du_dt"), (double)dstates_dt(0));
+    ds->set<double>(customize("dv_dt"), (double)dstates_dt(1));
+    ds->set<double>(customize("dw_dt"), (double)dstates_dt(2));
+    ds->set<double>(customize("dp_dt"), (double)dstates_dt(3));
+    ds->set<double>(customize("dq_dt"), (double)dstates_dt(4));
+    ds->set<double>(customize("dr_dt"), (double)dstates_dt(5));
+    const Eigen::Quaternion<double> q1(ds->get<double>(customize("qr")),
+                                       ds->get<double>(customize("qi")),
+                                       ds->get<double>(customize("qj")),
+                                       ds->get<double>(customize("qk"))
+                        );
+    const Eigen::Quaternion<double> q2(0,pqr(0),pqr(1),pqr(2));
+    const Eigen::Quaternion<double> dq_dt(Eigen::Quaternion<double>(q1*q2));
+    ds->set<double>(customize("dqr_dt"), 0.5*(double)dq_dt.w());
+    ds->set<double>(customize("dqi_dt"), 0.5*(double)dq_dt.x());
+    ds->set<double>(customize("dqj_dt"), 0.5*(double)dq_dt.y());
+    ds->set<double>(customize("dqk_dt"), 0.5*(double)dq_dt.z());
 }
--- code/simulator/unit_tests/src/StateDerivativesModuleTest.cpp
+++ code/simulator/unit_tests/src/StateDerivativesModuleTest.cpp
@@ -9,6 +9,11 @@
 #include "StateDerivativesModule.hpp"
 #include "DataSource.hpp"
 #include "Wrench.hpp"
+#include "Kinematics.hpp"
+#include "rotation_matrix_builders.hpp"
+#include "Transform.hpp"
+
+#define PI (4.*atan(1.))
 
 StateDerivativesModuleTest::StateDerivativesModuleTest() : a(DataGenerator(9898))
 {
@@ -34,28 +39,58 @@ TEST_F(StateDerivativesModuleTest, example)
     DataSource ds;
     const StateDerivativesModule module(&ds, body, forces);
     ds.add(module);
+    std::tr1::shared_ptr<Kinematics> k(new Kinematics());
+    ds.set<std::tr1::shared_ptr<Kinematics> >("kinematics", k);
+    const RotationMatrix R = kinematics::rot(1,0,0, PI/3.);
+
+    k->add(kinematics::Transform(Point("NED",4,-98,100),R,"foo"));
+
     ds.set<Wrench>("F1(foo)", Wrench(Point("foo"), Eigen::Vector3d(1, 22, 333), Eigen::Vector3d(-1, -22, -333)));
     ds.set<Wrench>("F2(foo)", Wrench(Point("foo"), Eigen::Vector3d(7, -11, 91), Eigen::Vector3d(-7, 11, -91)));
     ds.set<Eigen::Matrix<double,6,6> >("solid body inertia(foo)", Eigen::Matrix<double,6,6>::Identity());
     ds.set<Eigen::Matrix<double,6,6> >("total inertia(foo)", Eigen::Matrix<double,6,6>());
     ds.set<Eigen::Matrix<double,6,6> >("inverse of the total inertia(foo)", Eigen::Matrix<double,6,6>::Identity());
     ds.set<Point>("G(foo)", Point("foo"));
-    ds.set<double>("u(foo)", 0);
-    ds.set<double>("v(foo)", 0);
-    ds.set<double>("w(foo)", 0);
-    ds.set<double>("p(foo)", 0);
-    ds.set<double>("q(foo)", 0);
-    ds.set<double>("r(foo)", 0);
+    ds.set<double>("u(foo)", 79);
+    ds.set<double>("v(foo)", 842);
+    ds.set<double>("w(foo)", -72);
+    ds.set<double>("p(foo)", 116);
+    ds.set<double>("q(foo)", 3);
+    ds.set<double>("r(foo)", 5);
+    ds.set<double>("qr(foo)", 1);
+    ds.set<double>("qi(foo)", 2);
+    ds.set<double>("qj(foo)", 3);
+    ds.set<double>("qk(foo)", 4);
 //! [StateDerivativesModule example]
 //! [StateDerivativesModule expected output]
     const Wrench F = ds.get<Wrench>("sum of forces(foo)");
+    const Wrench Fc = ds.get<Wrench>("coriolis and centripetal forces(foo)");
+    ASSERT_DOUBLE_EQ(-4426, Fc.X);
+    ASSERT_DOUBLE_EQ(8747, Fc.Y);
+    ASSERT_DOUBLE_EQ(97435, Fc.Z);
+    ASSERT_DOUBLE_EQ(0, Fc.K);
+    ASSERT_DOUBLE_EQ(0, Fc.M);
+    ASSERT_DOUBLE_EQ(0, Fc.N);
     ASSERT_EQ("foo", F.get_frame());
-    ASSERT_DOUBLE_EQ(8, F.X);
-    ASSERT_DOUBLE_EQ(11, F.Y);
-    ASSERT_DOUBLE_EQ(424, F.Z);
+    ASSERT_DOUBLE_EQ(8-4426, F.X);
+    ASSERT_DOUBLE_EQ(11+8747, F.Y);
+    ASSERT_DOUBLE_EQ(424+97435, F.Z);
     ASSERT_DOUBLE_EQ(-8, F.K);
     ASSERT_DOUBLE_EQ(-11, F.M);
     ASSERT_DOUBLE_EQ(-424, F.N);
+    ASSERT_DOUBLE_EQ(79, ds.get<double>("dx_dt(foo)"));
+    ASSERT_DOUBLE_EQ(358.6461709275205294, ds.get<double>("dy_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-765.19338998649732275, ds.get<double>("dz_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-4418, ds.get<double>("du_dt(foo)"));
+    ASSERT_DOUBLE_EQ(8758, ds.get<double>("dv_dt(foo)"));
+    ASSERT_DOUBLE_EQ(97859, ds.get<double>("dw_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-8, ds.get<double>("dp_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-11, ds.get<double>("dq_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-424, ds.get<double>("dr_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-130.5, ds.get<double>("dqr_dt(foo)"));
+    ASSERT_DOUBLE_EQ(59.5, ds.get<double>("dqi_dt(foo)"));
+    ASSERT_DOUBLE_EQ(228.5, ds.get<double>("dqj_dt(foo)"));
+    ASSERT_DOUBLE_EQ(-168.5, ds.get<double>("dqk_dt(foo)"));
 //! [StateDerivativesModule expected output]
 }
 
