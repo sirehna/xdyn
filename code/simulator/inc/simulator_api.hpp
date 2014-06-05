5ee8e712bed9f611f08b6b03c85457900cab8df5
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-06-05 16:44:23 +0200

    [new_api] Added a new prototype for make_ds
    Takes a VectorOfVectorOfPoints as input
    Added the include_directories to use VectorOfVectorOfPoints
    Added eigen include_directories needed by simulator_external_data_structures_INCLUDE_DIRS

--- code/simulator/inc/simulator_api.hpp
+++ code/simulator/inc/simulator_api.hpp
@@ -1,8 +1,15 @@
 #ifndef SIMULATORAPI_HPP_
 #define SIMULATORAPI_HPP_
 
+#include <map>
+#include <string>
+
+#include "GeometricTypes3d.hpp"
 #include "DataSource.hpp"
 
 DataSource make_ds(const std::string& data);
+
+DataSource make_ds(const std::string& data,
+                   const std::map<std::string, VectorOfVectorOfPoints>& input_meshes);
 
 #endif
--- code/simulator/src/simulator_api.cpp
+++ code/simulator/src/simulator_api.cpp
@@ -9,3 +9,11 @@ DataSource make_ds(const std::string& data)
     DataSourceBuilder builder(check_input_yaml(parser.parse()));
     return builder.build_ds();
 }
+
+DataSource make_ds(const std::string& data,
+                   const std::map<std::string, VectorOfVectorOfPoints>& input_meshes)
+{
+    SimulatorYamlParser parser(data);
+    DataSourceBuilder builder(check_input_yaml(parser.parse()), input_meshes);
+    return builder.build_ds();
+}
