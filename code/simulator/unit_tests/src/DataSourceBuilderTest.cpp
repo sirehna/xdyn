0f104e8797c0ade35afb75b94905b7f45bcfe923
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-06-06 09:21:37 +0200

    [new_api] Refactored unit test constructor DataSourceBuilderTest so as not to write stl file on fly

--- code/simulator/unit_tests/src/DataSourceBuilderTest.cpp
+++ code/simulator/unit_tests/src/DataSourceBuilderTest.cpp
@@ -18,7 +18,6 @@
 #include "WaveModelInterface.hpp"
 #include "Kinematics.hpp"
 #include "Transform.hpp"
-#include "make_stl_file_on_the_fly.hpp"
 #include "StlReader.hpp"
 
 #include <Eigen/Geometry>
@@ -27,9 +26,9 @@ typedef Eigen::Matrix<double,6,6> Matrix6x6;
 
 DataSourceBuilderTest::DataSourceBuilderTest() : a(DataGenerator(12)), ds(DataSource()), mesh_data(read_stl(test_data::three_facets()))
 {
-    make_stl_file(test_data::three_facets(), "anthineas.stl");
+    const std::map<std::string, VectorOfVectorOfPoints> m = { {"body 1", mesh_data} };
     const SimulatorYamlParser parser(test_data::full_example());
-    DataSourceBuilder builder(parser.parse());
+    DataSourceBuilder builder(parser.parse(), m);
     ds = builder.build_ds();
 }
 
