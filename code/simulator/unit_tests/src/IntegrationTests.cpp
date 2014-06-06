16792c901df156d0cfcd7ecf04b38ad2001c70bd
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-06-06 11:44:13 +0200

    [falling_ball] Simulator does not crash for falling ball
    We're not actually checking the results yet: just that the
    simulation actually runs.

--- code/simulator/unit_tests/src/IntegrationTests.cpp
+++ code/simulator/unit_tests/src/IntegrationTests.cpp
@@ -10,6 +10,9 @@
 #include "DsMapObserver.hpp"
 #include "SimulatorYamlParser.hpp"
 #include "check_input_yaml.hpp"
+#include "simulator_api.hpp"
+#include "DsSystem.hpp"
+#include "DsSolve.hpp"
 
 IntegrationTests::IntegrationTests() : a(DataGenerator(1212))
 {
@@ -30,4 +33,11 @@ void IntegrationTests::TearDown()
 TEST_F(IntegrationTests, yaml_data_for_falling_ball_should_be_valid)
 {
     check_input_yaml(SimulatorYamlParser(test_data::falling_ball_example()).parse());
+}
+
+TEST_F(IntegrationTests, simulator_does_not_crash_for_falling_ball)
+{
+    DataSource ds = make_ds(test_data::falling_ball_example(),1,"euler");
+    DsMapObserver observer;
+    integrate(ds, 0, 10, observer);
 }
