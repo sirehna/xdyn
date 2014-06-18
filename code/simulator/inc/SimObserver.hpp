a67e1fe3836fdff5c707464940b1de92d5a310fb
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-06-18 09:27:03 +0200

    [refactoring] Stubbed SimObserver

---
+++ code/simulator/inc/SimObserver.hpp
@@ -0,0 +1,43 @@
+/*
+ * SimObserver.hpp
+ *
+ *  Created on: Jun 17, 2014
+ *      Author: cady
+ */
+
+#ifndef SIMOBSERVER_HPP_
+#define SIMOBSERVER_HPP_
+
+#include <string>
+
+class Sim;
+
+/** \author cec
+ *  \date Jun 17, 2014, 8:23:16 AM
+ *  \brief
+ *  \details
+ *  \ingroup simulator
+ *  \section ex1 Example
+ *  \snippet simulator/unit_tests/src/SimObserverTest.cpp SimObserverTest example
+ *  \section ex2 Expected output
+ *  \snippet simulator/unit_tests/src/SimObserverTest.cpp SimObserverTest expected output
+ */
+class SimObserver
+{
+    public:
+        SimObserver();
+        void observe(const Sim& sys, const double t);
+        SimObserver& watch_angle(const std::string& frame,
+                                 const std::string& relative_to_frame,
+                                 const std::string& projected_in_frame,
+                                 const std::string& axis
+                                 );
+        SimObserver& watch_point(const std::string& point,
+                                 const std::string& relative_to_frame,
+                                 const std::string& projected_in_frame,
+                                 const std::string& axis
+                                 );
+    private:
+};
+
+#endif /* SIMOBSERVER_HPP_ */
---
+++ code/simulator/src/SimObserver.cpp
@@ -0,0 +1,16 @@
+/*
+ * SimObserver.cpp
+ *
+ *  Created on: Jun 17, 2014
+ *      Author: cady
+ */
+
+#include "SimObserver.hpp"
+
+SimObserver::SimObserver()
+{
+}
+
+void SimObserver::observe(const Sim& , const double )
+{
+}
