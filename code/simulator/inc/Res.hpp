ed663504a71cc0d7573cf69adb82a5d6908f3940
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-07-01 09:31:28 +0200

    [outputs] Added Res structure to store simulation results

---
+++ code/simulator/inc/Res.hpp
@@ -0,0 +1,31 @@
+/*
+ * Res.hpp
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: cady
+ */
+
+#ifndef RES_HPP_
+#define RES_HPP_
+
+#include "StateMacros.hpp"
+
+/** \author cec
+ *  \date Jul 1, 2014, 9:28:53 AM
+ *  \brief Stores simulation results (states & instant)
+ *  \details This structure will be outputted at each simulation step.
+ *  \ingroup simulator
+ *  \section ex1 Example
+ *  \snippet simulator/unit_tests/src/ResTest.cpp ResTest example
+ *  \section ex2 Expected output
+ *  \snippet simulator/unit_tests/src/ResTest.cpp ResTest expected output
+ */
+struct Res
+{
+    Res();
+    double t; //<! Instant to which the states correspond
+    StateType x; //!< Values of the states of the system
+
+};
+
+#endif /* RES_HPP_ */
