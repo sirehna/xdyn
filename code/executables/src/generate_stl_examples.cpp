26927efe80a7383ec7be4a7b47c49231c57576dd
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-07-04 17:51:56 +0200

    [immerged_polygon_bugs] Added a big STL file
    This file produced a crash so we decided to keep it for non-regression.

--- code/executables/src/generate_stl_examples.cpp
+++ code/executables/src/generate_stl_examples.cpp
@@ -14,11 +14,13 @@ int main(int argc, const char* argv[])
     const std::string path = argc>1 ? std::string(argv[1])+"/" : "";
     std::ofstream file1(path+"single_facet.stl"),
                   file2(path+"three_facets.stl"),
-                  file3(path+"cube.stl");
+                  file3(path+"cube.stl"),
+                  file4(path+"big_cube.stl");
 
     file1 << test_data::single_facet();
     file2 << test_data::three_facets();
     file3 << test_data::cube();
+    file4 << test_data::big_cube();
 
     return 0;
 }
