34362e20588e331975c5b8fe952337b1da6f990a
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-08-21 10:37:32 +0200

    [anthineas_test] Can read binary STL from encoded string (base91x)

--- code/mesh/unit_tests/src/mesh_manipulationsTest.cpp
+++ code/mesh/unit_tests/src/mesh_manipulationsTest.cpp
@@ -5,11 +5,13 @@
  *      Author: cady
  */
 
+#include "base91x.hpp"
 #include "mesh_manipulationsTest.hpp"
 #include "GeometricTypes3d.hpp"
 #include "mesh_manipulations.hpp"
 #include "StlReader.hpp"
 #include "TriMeshTestData.hpp"
+#include "STL_data.hpp"
 
 #include "test_macros.hpp"
 
@@ -106,6 +108,25 @@ TEST_F(mesh_manipulationsTest, can_read_and_write_binary_stl)
             ASSERT_DOUBLE_EQ((double)mesh[i][j](0), (double)parsed_mesh[i][j](0)) << " (for i = " << i << " and j = " << j << ", x-coordinate)";
             ASSERT_DOUBLE_EQ((double)mesh[i][j](1), (double)parsed_mesh[i][j](1)) << " (for i = " << i << " and j = " << j << ", y-coordinate)";
             ASSERT_DOUBLE_EQ((double)mesh[i][j](2), (double)parsed_mesh[i][j](2)) << " (for i = " << i << " and j = " << j << ", z-coordinate)";
+        }
+    }
+}
+
+TEST_F(mesh_manipulationsTest, can_read_binary_stl_from_string)
+{
+    const VectorOfVectorOfPoints from_ascii = read_stl(test_data::cube());
+    const std::string s = ";C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$A;C2W$AMAAAAAAAAAAAAAAA4}BAC'EBAA1hAAT|DAAA#AAA1hAAT|DAAA#AAAHLAAT|DAAAAAAAAAAAAAk_HAAASEAAcsBA+\\PAAA+CAA4}AAv(fAAA.QAAT|BABt#AAAAAAAAAAAAAC'JCAAOWAA+\\XAAAAAAAP(AA+\\XAAAAAAAP(AA+\\HAAAAAAAAAAAAAAAAAAATEAAcsAAv(vAAAAAAAcsBAv(PAAAAAAAcsAAv(PAAAAAAAAA4}FAAAAAAAAAAA4}CABtEBAAAAAAT|FAC'EBAAPLAAk_LAAA#AAAPLAAAA+\\vAAAAAAAAAAA+\\XAAASEAAAAAA+\\XAAAjBAA8sAAv(vAAA+CAAAAAAAABt#AAAAAAAAAAABtfAAA.QAA4}BAC'fAAA.QAAAAAABtfAAAxFAAAAAAAAC'#AAAuWAAAAAAC'fAAA.QAA4}BAC'fAAAxFAAAAAABtfAAAxFAA4}BAAAAAAAAAf(AAAAAAAAJCAAP(AAAAAAAA#AAA1hAAAAAAC'fAAA.QAA4}BAAAAAAAAAf(AAAAAAAAJCAAP(AAAAAAAA#AAA1hAAT|DAAAJCAAP(AAk_HAAAAAXLAA4}BAAAAAAA.QAAT|BABt#AAAHLAAk_DAC'#AAAHLAAk_DAAAAAAAAAAAAAk_HAAAAAAAP(AA+\\HAAAkBAAcsAAv(PAAAAAAAcsBAv(PAAAAAAA";
+    const VectorOfVectorOfPoints from_binary = read_binary_stl(base<91>::decode(s));
+
+    ASSERT_EQ(from_ascii.size(),from_binary.size());
+    for (size_t i = 0 ; i < from_ascii.size() ; ++i)
+    {
+        ASSERT_EQ(from_ascii[i].size(),from_binary[i].size());
+        for (size_t j = 0 ; j < from_ascii[i].size() ; ++j)
+        {
+            ASSERT_DOUBLE_EQ((double)from_ascii[i][j](0),(double)from_binary[i][j](0));
+            ASSERT_DOUBLE_EQ((double)from_ascii[i][j](1),(double)from_binary[i][j](1));
+            ASSERT_DOUBLE_EQ((double)from_ascii[i][j](2),(double)from_binary[i][j](2));
         }
     }
 }
