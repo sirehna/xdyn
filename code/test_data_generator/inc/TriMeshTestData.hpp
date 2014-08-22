a18d8e83c0f68c3f1180b17a2ad89bc76d260238
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-08-22 16:36:09 +0200

    [dev] Added more mesh test data (for clockwise)

--- code/test_data_generator/inc/TriMeshTestData.hpp
+++ code/test_data_generator/inc/TriMeshTestData.hpp
@@ -7,12 +7,15 @@ VectorOfPoints one_triangle();
 VectorOfPoints one_triangle_clockwise();
 VectorOfPoints degenerated_triangle();
 VectorOfVectorOfPoints unit_cube();
+VectorOfVectorOfPoints unit_cube_with_incorrect_orientation();
+VectorOfVectorOfPoints unit_cube_clockwise();
 VectorOfVectorOfPoints two_triangles();
 VectorOfVectorOfPoints two_triangles_immerged();
 VectorOfVectorOfPoints trapezium();
 VectorOfVectorOfPoints n_gone(const size_t n);
 VectorOfVectorOfPoints cube(const double a, const double x0, const double y0, const double z0);
 VectorOfVectorOfPoints tetrahedron(const double a, const double x0, const double y0, const double z0);
+VectorOfVectorOfPoints tetrahedron_clockwise(const double a, const double x0, const double y0, const double z0);
 VectorOfVectorOfPoints generated_stl();
 VectorOfVectorOfPoints anthineas();
 
--- code/test_data_generator/src/TriMeshTestData.cpp
+++ code/test_data_generator/src/TriMeshTestData.cpp
@@ -64,6 +64,109 @@ VectorOfPoints degenerated_triangle()
 
 VectorOfVectorOfPoints unit_cube()
 {
+    VectorOfVectorOfPoints V;
+    VectorOfPoints P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12;
+    EPoint p1, p2, p3;
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P1.push_back(p1);
+    P1.push_back(p2);
+    P1.push_back(p3);
+    V.push_back(P1);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P2.push_back(p1);
+    P2.push_back(p2);
+    P2.push_back(p3);
+    V.push_back(P2);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P3.push_back(p1);
+    P3.push_back(p2);
+    P3.push_back(p3);
+    V.push_back(P3);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P4.push_back(p1);
+    P4.push_back(p2);
+    P4.push_back(p3);
+    V.push_back(P4);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P5.push_back(p1);
+    P5.push_back(p2);
+    P5.push_back(p3);
+    V.push_back(P5);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P6.push_back(p1);
+    P6.push_back(p2);
+    P6.push_back(p3);
+    V.push_back(P6);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P7.push_back(p1);
+    P7.push_back(p2);
+    P7.push_back(p3);
+    V.push_back(P7);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P8.push_back(p1);
+    P8.push_back(p2);
+    P8.push_back(p3);
+    V.push_back(P8);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=-0.5; p3(2)=+0.5;
+    P9.push_back(p1);
+    P9.push_back(p2);
+    P9.push_back(p3);
+    V.push_back(P9);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=-0.5; p3(2)=+0.5;
+    P10.push_back(p1);
+    P10.push_back(p2);
+    P10.push_back(p3);
+    V.push_back(P10);
+
+    p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P11.push_back(p1);
+    P11.push_back(p2);
+    P11.push_back(p3);
+    V.push_back(P11);
+
+    p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P12.push_back(p1);
+    P12.push_back(p2);
+    P12.push_back(p3);
+    V.push_back(P12);
+    return V;
+}
+
+VectorOfVectorOfPoints unit_cube_clockwise()
+{
 	VectorOfVectorOfPoints V;
 	VectorOfPoints P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12;
 	EPoint p1, p2, p3;
@@ -71,98 +174,201 @@ VectorOfVectorOfPoints unit_cube()
 	p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
 	p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
 	P1.push_back(p1);
-    P1.push_back(p2);
     P1.push_back(p3);
+    P1.push_back(p2);
 	V.push_back(P1);
 
 	p1(0)=-0.5; p1(1)=-0.5; p1(2)=+0.5;
 	p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
 	p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
 	P2.push_back(p1);
-    P2.push_back(p2);
     P2.push_back(p3);
+    P2.push_back(p2);
     V.push_back(P2);
 
 	p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=-0.5; p2(1)=-0.5; p2(2)=-0.5;
 	p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P3.push_back(p1);
-    P3.push_back(p2);
     P3.push_back(p3);
+    P3.push_back(p2);
     V.push_back(P3);
 
 	p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=-0.5; p2(1)=+0.5; p2(2)=-0.5;
 	p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P4.push_back(p1);
-    P4.push_back(p2);
     P4.push_back(p3);
+    P4.push_back(p2);
     V.push_back(P4);
 
 	p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=-0.5; p2(1)=-0.5; p2(2)=+0.5;
 	p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
 	P5.push_back(p1);
-    P5.push_back(p2);
     P5.push_back(p3);
+    P5.push_back(p2);
     V.push_back(P5);
 
 	p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=-0.5; p2(1)=+0.5; p2(2)=+0.5;
 	p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P6.push_back(p1);
-    P6.push_back(p2);
     P6.push_back(p3);
+    P6.push_back(p2);
     V.push_back(P6);
 
 	p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
 	p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
 	p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P7.push_back(p1);
-    P7.push_back(p2);
     P7.push_back(p3);
+    P7.push_back(p2);
     V.push_back(P7);
 
 	p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
 	p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
 	p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
 	P8.push_back(p1);
-    P8.push_back(p2);
     P8.push_back(p3);
+    P8.push_back(p2);
     V.push_back(P8);
 
 	p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
 	p3(0)=+0.5; p3(1)=-0.5; p3(2)=+0.5;
 	P9.push_back(p1);
-    P9.push_back(p2);
     P9.push_back(p3);
+    P9.push_back(p2);
     V.push_back(P9);
 
 	p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
 	p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
 	p3(0)=-0.5; p3(1)=-0.5; p3(2)=+0.5;
 	P10.push_back(p1);
-    P10.push_back(p2);
     P10.push_back(p3);
+    P10.push_back(p2);
     V.push_back(P10);
 
 	p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
 	p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
 	p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P11.push_back(p1);
-    P11.push_back(p2);
     P11.push_back(p3);
+    P11.push_back(p2);
     V.push_back(P11);
 
 	p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
 	p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
 	p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
 	P12.push_back(p1);
-    P12.push_back(p2);
     P12.push_back(p3);
+    P12.push_back(p2);
     V.push_back(P12);
 	return V;
+}
+
+VectorOfVectorOfPoints unit_cube_with_incorrect_orientation()
+{
+    VectorOfVectorOfPoints V;
+    VectorOfPoints P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12;
+    EPoint p1, p2, p3;
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P1.push_back(p1);
+    P1.push_back(p3);
+    P1.push_back(p2);
+    V.push_back(P1);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P2.push_back(p1);
+    P2.push_back(p2);
+    P2.push_back(p3);
+    V.push_back(P2);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P3.push_back(p1);
+    P3.push_back(p2);
+    P3.push_back(p3);
+    V.push_back(P3);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P4.push_back(p1);
+    P4.push_back(p2);
+    P4.push_back(p3);
+    V.push_back(P4);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P5.push_back(p1);
+    P5.push_back(p3);
+    P5.push_back(p2);
+    V.push_back(P5);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=-0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P6.push_back(p1);
+    P6.push_back(p3);
+    P6.push_back(p2);
+    V.push_back(P6);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P7.push_back(p1);
+    P7.push_back(p3);
+    P7.push_back(p2);
+    V.push_back(P7);
+
+    p1(0)=+0.5; p1(1)=-0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=+0.5;
+    P8.push_back(p1);
+    P8.push_back(p3);
+    P8.push_back(p2);
+    V.push_back(P8);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=-0.5;
+    p3(0)=+0.5; p3(1)=-0.5; p3(2)=+0.5;
+    P9.push_back(p1);
+    P9.push_back(p3);
+    P9.push_back(p2);
+    V.push_back(P9);
+
+    p1(0)=-0.5; p1(1)=-0.5; p1(2)=-0.5;
+    p2(0)=+0.5; p2(1)=-0.5; p2(2)=+0.5;
+    p3(0)=-0.5; p3(1)=-0.5; p3(2)=+0.5;
+    P10.push_back(p1);
+    P10.push_back(p3);
+    P10.push_back(p2);
+    V.push_back(P10);
+
+    p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=+0.5;
+    p3(0)=+0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P11.push_back(p1);
+    P11.push_back(p3);
+    P11.push_back(p2);
+    V.push_back(P11);
+
+    p1(0)=-0.5; p1(1)=+0.5; p1(2)=+0.5;
+    p2(0)=+0.5; p2(1)=+0.5; p2(2)=-0.5;
+    p3(0)=-0.5; p3(1)=+0.5; p3(2)=-0.5;
+    P12.push_back(p1);
+    P12.push_back(p3);
+    P12.push_back(p2);
+    V.push_back(P12);
+    return V;
 }
 
 VectorOfVectorOfPoints two_triangles()
