063b560358085a923dfb3ac5db3a72b4fb31ff0d
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2014-08-29 11:56:46 +0200

    [dev] Added environment variables for CMake to search gcovr

--- code/cmake/FindGcovr.cmake
+++ code/cmake/FindGcovr.cmake
@@ -6,10 +6,11 @@
 # Uses:
 #
 # GCOVR_ROOT - root to search for the script
+# ENV{GCOVR_ROOT} - root to search for the script
 
 INCLUDE(FindPackageHandleStandardArgs)
 
-FIND_PROGRAM(GCOVR_EXECUTABLE gcovr HINTS ${GCOVR_ROOT} "${GCOVR_ROOT}/bin")
+FIND_PROGRAM(GCOVR_EXECUTABLE gcovr HINTS ${GCOVR_ROOT} "${GCOVR_ROOT}/bin" $ENV{GCOVR_ROOT} "$ENV{GCOVR_ROOT}/bin")
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(gcovr DEFAULT_MSG GCOVR_EXECUTABLE)
 
