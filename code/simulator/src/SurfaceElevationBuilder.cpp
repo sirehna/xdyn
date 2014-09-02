bf2d5c0e9132d84b94d1a3b709499d54224d2211
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-02 13:48:09 +0200

    [dev] Fixed wave mesh output
    Correct matrix size when xmin==xmax or ymin==ymax

--- code/simulator/src/SurfaceElevationBuilder.cpp
+++ code/simulator/src/SurfaceElevationBuilder.cpp
@@ -17,11 +17,13 @@ TR1(shared_ptr)<PointMatrix> SurfaceElevationBuilderInterface::make_wave_mesh(co
 {
     const double dx = (output.xmax-output.xmin)/(double)(output.nx > 1 ? output.nx-1 : 1);
     const double dy = (output.ymax-output.ymin)/(double)(output.ny > 1 ? output.ny-1 : 1);
-    PointMatrix M(output.frame_of_reference, output.nx*output.ny);
+    const size_t nx = (dx==0 and output.nx) ? 1 : output.nx;
+    const size_t ny = (dy==0 and output.ny) ? 1 : output.ny;
+    PointMatrix M(output.frame_of_reference, nx*ny);
     size_t k = 0;
-    for (size_t j = 0 ; j < output.ny ; ++j)
+    for (size_t j = 0 ; j < ny ; ++j)
     {
-        for (size_t i = 0 ; i < output.nx ; ++i)
+        for (size_t i = 0 ; i < nx ; ++i)
         {
             M.m(0,k) = output.xmin + (double)i*dx;
             M.m(1,k) = output.ymin + (double)j*dy;
