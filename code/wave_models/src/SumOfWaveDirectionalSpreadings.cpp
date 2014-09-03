634948a8bd872ce057633c62a9d4b11c3512dd8f
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-03 16:27:17 +0200

    [froude_krylov] Implemented SumOfWaveDirectionalSpreadings

---
+++ code/wave_models/src/SumOfWaveDirectionalSpreadings.cpp
@@ -0,0 +1,74 @@
+/*
+ * SumOfWaveDirectionalSpreadings.cpp
+ *
+ *  Created on: Sep 3, 2014
+ *      Author: cady
+ */
+
+#include "SumOfWaveDirectionalSpreadings.hpp"
+
+/*
+ * SumOfWaveSpectralDensities.cpp
+ *
+ *  Created on: Sep 3, 2014
+ *      Author: cady
+ */
+
+#include <list>
+#include <set>
+
+#include "SumOfWaveSpectralDensities.hpp"
+
+SumOfWaveDirectionalSpreadings::SumOfWaveDirectionalSpreadings(const WaveDirectionalSpreading& w) : terms(std::vector<TR1(shared_ptr)<WaveDirectionalSpreading> >(1, TR1(shared_ptr)<WaveDirectionalSpreading>(w.clone())))
+{
+}
+
+SumOfWaveDirectionalSpreadings::SumOfWaveDirectionalSpreadings(const WaveDirectionalSpreading& w1, const WaveDirectionalSpreading& w2) : terms(std::vector<TR1(shared_ptr)<WaveDirectionalSpreading> >())
+{
+    terms.push_back(TR1(shared_ptr)<WaveDirectionalSpreading>(w1.clone()));
+    terms.push_back(TR1(shared_ptr)<WaveDirectionalSpreading>(w2.clone()));
+}
+
+SumOfWaveDirectionalSpreadings::SumOfWaveDirectionalSpreadings(const std::vector<WaveDirectionalSpreading>& ws) : terms(std::vector<TR1(shared_ptr)<WaveDirectionalSpreading> >())
+{
+    for (auto t = ws.begin() ; t != ws.end() ; ++t)
+    {
+        terms.push_back(TR1(shared_ptr)<WaveDirectionalSpreading>(t->clone()));
+    }
+}
+
+
+double SumOfWaveDirectionalSpreadings::operator()(const double omega //!< Angular frequency (\f$2\pi f\f$) in rad/s of the significant wave height
+                                  ) const
+{
+    double ret = 0;
+    for (auto t = terms.begin() ; t != terms.end() ; ++t)
+    {
+        ret += (*t)->operator()(omega);
+    }
+    return ret;
+}
+
+WaveDirectionalSpreading* SumOfWaveDirectionalSpreadings::clone() const
+{
+    return new SumOfWaveDirectionalSpreadings(*this);
+}
+
+/**  \brief Returns n angular frequencies between omega_min (included)
+  *         and omega_max (also included)
+  *  \snippet wave_models/unit_tests/src/WaveSpectralDensityTest.cpp WaveSpectralDensityTest get_omega0_example
+  */
+std::vector<double> SumOfWaveDirectionalSpreadings::get_directions(const size_t n) const
+{
+    std::set<double> S;
+    for (auto t = terms.begin() ; t != terms.end() ; ++t)
+    {
+        std::vector<double> f = (*t)->get_directions(n);
+        S.insert(f.begin(), f.end());
+    }
+    std::list<double> L(S.begin(), S.end());
+    L.sort();
+    return std::vector<double>(L.begin(), L.end());
+}
+
+
--- code/wave_models/unit_tests/src/AiryTest.cpp
+++ code/wave_models/unit_tests/src/AiryTest.cpp
@@ -12,6 +12,7 @@
 #include "DiracSpectralDensity.hpp"
 #include "DiracDirectionalSpreading.hpp"
 #include "SumOfWaveSpectralDensities.hpp"
+#include "SumOfWaveDirectionalSpreadings.hpp"
 
 #include "discretize.hpp"
 
@@ -76,6 +77,30 @@ TEST_F(AiryTest, two_frequencies_single_direction_at_one_point)
     const double omega_max = 10;
     const size_t nfreq = 50;
     const DiscreteDirectionalWaveSpectrum A = discretize(DiracSpectralDensity(omega0, Hs)+DiracSpectralDensity(omega0, Hs), DiracDirectionalSpreading(psi0), omega_min, omega_max, nfreq);
+    int random_seed = 0;
+    const Airy wave(A, random_seed);
+
+    const double x = a.random<double>();
+    const double y = a.random<double>();
+    const double phi = 3.4482969340598712549;
+    const double k = 4.*PI*PI/Tp/Tp/9.81;
+    for (double t = 0 ; t < 3*Tp ; t+=0.1)
+    {
+        ASSERT_NEAR(2*sqrt(Hs)*cos(k*(x*cos(psi0)+y*sin(psi0))-2*PI/Tp*t +phi), wave.elevation(x,y,t), 1E-6);
+    }
+}
+
+TEST_F(AiryTest, one_frequency_two_directions_at_one_point)
+{
+    //! [AiryTest example]
+    const double psi0 = PI/4;
+    const double Hs = 3;
+    const double Tp = 10;
+    const double omega0 = 2*PI/Tp;
+    const double omega_min = 0.01;
+    const double omega_max = 10;
+    const size_t nfreq = 50;
+    const DiscreteDirectionalWaveSpectrum A = discretize(DiracSpectralDensity(omega0, Hs), DiracDirectionalSpreading(psi0)+DiracDirectionalSpreading(psi0), omega_min, omega_max, nfreq);
     int random_seed = 0;
     const Airy wave(A, random_seed);
 
