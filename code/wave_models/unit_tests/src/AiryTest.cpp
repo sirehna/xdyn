35470aef43ffda55746e2bf433742a789c52ddff
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-09-03 15:45:07 +0200

    [froude_krylov] Added test 'two_frequencies_single_direction_at_one_point'

--- code/wave_models/unit_tests/src/AiryTest.cpp
+++ code/wave_models/unit_tests/src/AiryTest.cpp
@@ -11,6 +11,8 @@
 #include "Cos2sDirectionalSpreading.hpp"
 #include "DiracSpectralDensity.hpp"
 #include "DiracDirectionalSpreading.hpp"
+#include "SumOfWaveSpectralDensities.hpp"
+
 #include "discretize.hpp"
 
 #define _USE_MATH_DEFINE
@@ -63,6 +65,29 @@ TEST_F(AiryTest, single_frequency_single_direction_at_one_point)
     //! [AiryTest expected output]
 }
 
+TEST_F(AiryTest, two_frequencies_single_direction_at_one_point)
+{
+    //! [AiryTest example]
+    const double psi0 = PI/4;
+    const double Hs = 3;
+    const double Tp = 10;
+    const double omega0 = 2*PI/Tp;
+    const double omega_min = 0.01;
+    const double omega_max = 10;
+    const size_t nfreq = 50;
+    const DiscreteDirectionalWaveSpectrum A = discretize(DiracSpectralDensity(omega0, Hs)+DiracSpectralDensity(omega0, Hs), DiracDirectionalSpreading(psi0), omega_min, omega_max, nfreq);
+    int random_seed = 0;
+    const Airy wave(A, random_seed);
+
+    const double x = a.random<double>();
+    const double y = a.random<double>();
+    const double phi = 3.4482969340598712549;
+    const double k = 4.*PI*PI/Tp/Tp/9.81;
+    for (double t = 0 ; t < 3*Tp ; t+=0.1)
+    {
+        ASSERT_NEAR(2*sqrt(Hs)*cos(k*(x*cos(psi0)+y*sin(psi0))-2*PI/Tp*t +phi), wave.elevation(x,y,t), 1E-6);
+    }
+}
 
 TEST_F(AiryTest, bug)
 {
