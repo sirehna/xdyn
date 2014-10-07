d0665fe27f86bd6a07fc6a33432c9da3aea88420
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-10-07 10:31:39 +0200

    [froude_krylov] Refactored test_hs

--- code/executables/src/test_hs.cpp
+++ code/executables/src/test_hs.cpp
@@ -7,7 +7,7 @@
 
 #include "Body.hpp"
 #include "DefaultSurfaceElevation.hpp"
-#include "FastHydrostaticForceModel.hpp"
+#include "FroudeKrylovForceModel.hpp"
 #include "TriMeshTestData.hpp"
 #include "MeshIntersector.hpp"
 #include "YamlRotation.hpp"
@@ -15,10 +15,19 @@
 #include "GeometricTypes3d.hpp"
 #include <ssc/kinematics.hpp>
 #include "generate_anthineas.hpp"
+#include "DiracSpectralDensity.hpp"
+#include "DiracDirectionalSpreading.hpp"
+#include "discretize.hpp"
+#include "Airy.hpp"
 #include <ssc/macros.hpp>
+#include "SurfaceElevationFromWaves.hpp"
+
 #define BODY "body 1"
 
-#define N 2000000
+#define N 2000
+#define _USE_MATH_DEFINE
+#include <cmath>
+#define PI M_PI
 
 Body get_body(const std::string& name);
 Body get_body(const std::string& name, const VectorOfVectorOfPoints& points);
@@ -39,7 +48,23 @@ Body get_body(const std::string& name, const VectorOfVectorOfPoints& points)
     return BodyBuilder(rot).build(name, points);
 }
 
-int main()
+TR1(shared_ptr)<WaveModel> get_wave_model();
+TR1(shared_ptr)<WaveModel> get_wave_model()
+{
+    const double psi0 = PI/4;
+    const double Hs = 3;
+    const double Tp = 10;
+    const double omega0 = 2*PI/Tp;
+    const double omega_min = 0.1;
+    const double omega_max = 5;
+    const size_t nfreq = 10;
+    const DiscreteDirectionalWaveSpectrum A = discretize(DiracSpectralDensity(omega0, Hs), DiracDirectionalSpreading(psi0), omega_min, omega_max, nfreq);
+    int random_seed = 0;
+    return TR1(shared_ptr)<WaveModel>(new Airy(A, random_seed));
+}
+
+EnvironmentAndFrames get_env();
+EnvironmentAndFrames get_env()
 {
     EnvironmentAndFrames env;
     env.g = 9.81;
@@ -49,28 +74,27 @@ int main()
     env.k->add(ssc::kinematics::Transform(ssc::kinematics::Point("NED"), "mesh(" BODY ")"));
     env.k->add(ssc::kinematics::Transform(ssc::kinematics::Point("NED"), BODY));
     TR1(shared_ptr)<ssc::kinematics::PointMatrix> mesh;
-    env.w = SurfaceElevationPtr(new DefaultSurfaceElevation(0, mesh));
+    //env.w = SurfaceElevationPtr(new DefaultSurfaceElevation(0, mesh));
+    env.w = SurfaceElevationPtr(new SurfaceElevationFromWaves(get_wave_model()));
+    return env;
+}
 
-    auto points = two_triangles();
-    for (size_t i = 0 ; i < 2 ; ++i)
-    {
-        for (size_t j = 0 ; j < 3 ; ++j)
-        {
-            double x = points[i][j][0];
-            points[i][j][0] = points[i][j][2];
-            points[i][j][2] = points[i][j][1];
-            points[i][j][1] = x;
-        }
-    }
-
+void test(const ForceModel& F, const EnvironmentAndFrames& env, const size_t n);
+void test(const ForceModel& F, const EnvironmentAndFrames& env, size_t n)
+{
     Body body = get_body(BODY, anthineas());
-    body.G = G;
-
-    FastHydrostaticForceModel F(env);
     const double t = 0;
     const std::vector<double> vz = env.w->get_relative_wave_height(body.M,env.k,t);
     body.intersector->update_intersection_with_free_surface(vz);
     ssc::kinematics::Wrench Fhs;
-    for (size_t i = 0 ; i < N ; ++i) Fhs = F(body, t);
+    for (size_t i = 0 ; i < n ; ++i) F(body, t);
+}
+
+int main(int argc, char* argv[])
+{
+    const size_t n = argc>1 ? atoi(argv[1]) : N;
+    auto env = get_env();
+    test(FroudeKrylovForceModel(env), env, n);
+    //test(FastHydrostaticForceModel(env), env, N);
     return 0;
 }
