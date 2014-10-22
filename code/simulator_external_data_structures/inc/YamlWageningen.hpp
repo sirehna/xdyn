21403c59b5bfa97821a33e8318a02a56698b8ffa
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-10-22 15:32:08 +0200

    [controlled_forces] Created external data structure for Wageningen

---
+++ code/simulator_external_data_structures/inc/YamlWageningen.hpp
@@ -0,0 +1,26 @@
+/*
+ * YamlWageningen.hpp
+ *
+ *  Created on: Oct 22, 2014
+ *      Author: cady
+ */
+
+#ifndef YAMLWAGENINGEN_HPP_
+#define YAMLWAGENINGEN_HPP_
+
+#include "YamlPosition.hpp"
+
+struct YamlWageningen
+{
+    YamlWageningen();
+    std::string name;
+    YamlPosition position_of_propeller_frame;
+    double wake_coefficient;
+    double relative_rotative_efficiency;
+    double thrust_deduction_factor;
+    bool rotating_clockwise;
+    size_t number_of_blades;
+    double blade_area_ratio;
+};
+
+#endif /* YAMLWAGENINGEN_HPP_ */
