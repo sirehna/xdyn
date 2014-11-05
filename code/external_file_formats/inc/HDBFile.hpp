758904fa9f1e0d7b57cdf5212f5933906371d74d
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-05 17:34:30 +0100

    [hdb_parser] Stubbed external data structure for HDB files

---
+++ code/external_file_formats/inc/HDBFile.hpp
@@ -0,0 +1,19 @@
+/*
+ * HdbFile.hpp
+ *
+ *  Created on: Nov 5, 2014
+ *      Author: cady
+ */
+
+#ifndef HDBFILE_HPP_
+#define HDBFILE_HPP_
+
+#include <string>
+
+struct HDBFile
+{
+    HDBFile();
+    std::string soft;
+};
+
+#endif /* HDBFILE_HPP_ */
