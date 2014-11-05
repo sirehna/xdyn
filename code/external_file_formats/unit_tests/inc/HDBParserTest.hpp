f007a66d11dbc1e8e29bdca1f562e170595b19f9
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-05 17:11:11 +0100

    [hdb_parser] Stubbed HDBParserTest

---
+++ code/external_file_formats/unit_tests/inc/HDBParserTest.hpp
@@ -0,0 +1,24 @@
+/*
+ * HDBParserTest.hpp
+ *
+ *  Created on: Nov 5, 2014
+ *      Author: cady
+ */
+
+#ifndef HDBPARSERTEST_HPP_
+#define HDBPARSERTEST_HPP_
+
+#include "gtest/gtest.h"
+#include <ssc/random_data_generator/DataGenerator.hpp>
+
+class HDBParserTest : public ::testing::Test
+{
+    protected:
+        HDBParserTest();
+        virtual ~HDBParserTest();
+        virtual void SetUp();
+        virtual void TearDown();
+        ssc::random_data_generator::DataGenerator a;
+};
+
+#endif  /* HDBPARSERTEST_HPP_ */
---
+++ code/external_file_formats/unit_tests/src/HDBParserTest.cpp
@@ -0,0 +1,35 @@
+/*
+ * HDBParserTest.cpp
+ *
+ *  Created on: Nov 5, 2014
+ *      Author: cady
+ */
+
+#include "HDBParserTest.hpp"
+
+HDBParserTest::HDBParserTest() : a(ssc::random_data_generator::DataGenerator(212))
+{
+}
+
+HDBParserTest::~HDBParserTest()
+{
+}
+
+void HDBParserTest::SetUp()
+{
+}
+
+void HDBParserTest::TearDown()
+{
+}
+
+TEST_F(HDBParserTest, example)
+{
+//! [HDBParserTest example]
+//! [HDBParserTest example]
+//! [HDBParserTest expected output]
+//! [HDBParserTest expected output]
+}
+
+
+
