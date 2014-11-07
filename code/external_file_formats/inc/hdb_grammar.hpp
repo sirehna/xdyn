d535d9d7adbdc3beea892b6677c82623da89407e
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-07 13:15:29 +0100

    [hdb_parser] Put HDB grammar in one file

---
+++ code/external_file_formats/inc/hdb_grammar.hpp
@@ -0,0 +1,43 @@
+/*
+ * hdb_grammar.hpp
+ *
+ *  Created on: Nov 7, 2014
+ *      Author: cady
+ */
+
+#ifndef HDB_GRAMMAR_HPP_
+#define HDB_GRAMMAR_HPP_
+
+#include "boost_spirit_hdb_adapters.hpp"
+
+
+template <typename Iterator>
+struct hdb_grammar : qi::grammar<Iterator, hdb::AST(), ascii::space_type>
+{
+    hdb_grammar() : hdb_grammar::base_type(ast)
+    {
+        ast                      %= string_key | value_key | section | section_with_id | list_of_sections | list_of_sections_with_id;
+        str = char_("a-zA-Z") >> *char_("a-zA-Z_0-9");
+        header                   %= lexeme['[' >> +(char_ - '[' - ']') >> ']'];
+        string_key               %= header >> str >> *char_;
+        value_key                %= header >> double_;
+        values                   %= double_ >> *(' ' >> double_);
+        section                  %= header >> +(eol >> values);
+        list_of_sections         %= header >> +(eol >> section);
+        section_with_id          %= header >> double_ >> +(eol >> values);
+        list_of_sections_with_id %= header >> +(eol >> section_with_id);
+    }
+
+    qi::rule<Iterator, hdb::AST(), ascii::space_type>                  ast;
+    qi::rule<Iterator, hdb::Header(), ascii::space_type>               header;
+    qi::rule<Iterator, std::string(), ascii::space_type>               str;
+    qi::rule<Iterator, hdb::Key<double>(), ascii::space_type>          value_key;
+    qi::rule<Iterator, hdb::Key<std::string>(), ascii::space_type>     string_key;
+    qi::rule<Iterator, hdb::ListOfSections(), ascii::space_type>       list_of_sections;
+    qi::rule<Iterator, hdb::ListOfSectionsWithId(), ascii::space_type> list_of_sections_with_id;
+    qi::rule<Iterator, hdb::Section(), ascii::space_type>              section;
+    qi::rule<Iterator, hdb::SectionWithId(), ascii::space_type>        section_with_id;
+    qi::rule<Iterator, hdb::Values(), ascii::space_type>               values;
+};
+
+#endif /* HDB_GRAMMAR_HPP_ */
