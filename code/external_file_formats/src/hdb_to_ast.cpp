bdbfaeb4b9ecda5684b72b70626e803cf61d7424
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-07 16:03:31 +0100

    [hdb_parser] Simplified hdb_to_ast

--- code/external_file_formats/src/hdb_to_ast.cpp
+++ code/external_file_formats/src/hdb_to_ast.cpp
@@ -9,36 +9,16 @@
 #include "hdb_grammar.hpp"
 
 #include <ssc/macros.hpp>
-//
-//distributor parsed;
-//        qi::rule<std::string::const_iterator, std::string()> string_rule =
-//                    +~qi::char_(',');
-//        qi::rule<std::string::const_iterator, distributor()> rule =
-//                    (qi::int_ | string_rule)%qi::lit(',');
-//        std::string test="abc,1,2,def,ghi,3,jkl";
-//        std::string::const_iterator iter=test.begin(), end=test.end();
-//        bool result = qi::parse(iter,end,rule,parsed);
-
 
 hdb::AST hdb::parse(const std::string& contents)
 {
-    typedef hdb_grammar<std::string::const_iterator> grammar;
-    grammar g; // Our grammar
+    hdb::grammar g; // Our grammar
     hdb::AST ast; // Our tree
 
     using boost::spirit::ascii::space;
     std::string::const_iterator begin = contents.begin(), end = contents.end();
-    bool success = qi::phrase_parse(begin, end, g.ast, space, ast);
-//    COUT(success);
-//
-//    if(success && begin==end)
-//    {
-//        std::cout << "Success." << std::endl;
-//    }
-//    else
-//    {
-//        std::cout << "Failure." << std::endl;
-//        //std::cout << "Unparsed: " << std::string(begin,end) << std::endl;
-//    }
+
+    qi::phrase_parse(begin, end, g.ast, space, ast);
+
     return ast;
 }
