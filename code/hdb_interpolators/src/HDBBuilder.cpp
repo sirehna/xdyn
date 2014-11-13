e743a96e9eca533fe6a68e26e6e33f7f522b31e7
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-13 17:28:59 +0100

    [hdb_parser] Used compilation firewall for HDBBuilder

--- code/hdb_interpolators/src/HDBBuilder.cpp
+++ code/hdb_interpolators/src/HDBBuilder.cpp
@@ -7,52 +7,64 @@
 
 #include <sstream>
 
+#include "hdb_parser_internal_data_structures.hpp"
 #include "HDBBuilder.hpp"
 #include "HDBBuilderException.hpp"
 #include "hdb_to_ast.hpp"
 
-HDBBuilder::HDBBuilder(const std::string& data) : tree(hdb::parse(data))
+class HDBBuilder::Impl
 {
-}
-
-void HDBBuilder::fill(TimestampedMatrices& ret, const size_t i, const hdb::ListOfValues& M) const
-{
-    if (ret.empty()) ret.resize(M.size());
-    if (M.size() != ret.size())
-    {
-        std::stringstream ss;
-        ss << "Something is wrong with the HDB file: detected "
-           << ret.size()
-           << " periods in Added_mass_Radiation_Damping, but line "
-           << i << " has " << M.size() << " periods";
-        THROW(__PRETTY_FUNCTION__, HDBBuilderException,  ss.str());
-    }
-    for (size_t k = 0 ; k < M.size() ; ++k)
-    {
-        ret[k].first = M.at(k).front();
-        for (size_t j = 0 ; j < 6 ; ++j)
+    public:
+        Impl(const std::string& data) : tree(hdb::parse(data))
         {
-            ret[k].second[i][j] = M.at(k).at(j+1);
         }
-    }
+
+        void fill(TimestampedMatrices& ret, const size_t i, const hdb::ListOfValues& M) const
+        {
+            if (ret.empty()) ret.resize(M.size());
+            if (M.size() != ret.size())
+            {
+                std::stringstream ss;
+                ss << "Something is wrong with the HDB file: detected "
+                   << ret.size()
+                   << " periods in Added_mass_Radiation_Damping, but line "
+                   << i << " has " << M.size() << " periods";
+                THROW(__PRETTY_FUNCTION__, HDBBuilderException,  ss.str());
+            }
+            for (size_t k = 0 ; k < M.size() ; ++k)
+            {
+                ret[k].first = M.at(k).front();
+                for (size_t j = 0 ; j < 6 ; ++j)
+                {
+                    ret[k].second[i][j] = M.at(k).at(j+1);
+                }
+            }
+        }
+
+        hdb::AST tree;
+};
+
+
+HDBBuilder::HDBBuilder(const std::string& data) : pimpl(new Impl(data))
+{
 }
 
 TimestampedMatrices HDBBuilder::get_added_mass() const
 {
     TimestampedMatrices ret;
     std::vector<bool> found_line(6,false);
-    for (auto M = tree.lists_of_matrix_sections.begin() ; M != tree.lists_of_matrix_sections.end() ; ++M)
+    for (auto M = pimpl->tree.lists_of_matrix_sections.begin() ; M != pimpl->tree.lists_of_matrix_sections.end() ; ++M)
     {
         if (M->header == "Added_mass_Radiation_Damping")
         {
             for (auto that_section = M->sections.begin() ; that_section != M->sections.end() ; ++that_section)
             {
-                if (that_section->header == "ADDED_MASS_LINE_1") fill(ret, 0, that_section->values); found_line[0] = true;
-                if (that_section->header == "ADDED_MASS_LINE_2") fill(ret, 1, that_section->values); found_line[1] = true;
-                if (that_section->header == "ADDED_MASS_LINE_3") fill(ret, 2, that_section->values); found_line[2] = true;
-                if (that_section->header == "ADDED_MASS_LINE_4") fill(ret, 3, that_section->values); found_line[3] = true;
-                if (that_section->header == "ADDED_MASS_LINE_5") fill(ret, 4, that_section->values); found_line[4] = true;
-                if (that_section->header == "ADDED_MASS_LINE_6") fill(ret, 5, that_section->values); found_line[5] = true;
+                if (that_section->header == "ADDED_MASS_LINE_1") pimpl->fill(ret, 0, that_section->values); found_line[0] = true;
+                if (that_section->header == "ADDED_MASS_LINE_2") pimpl->fill(ret, 1, that_section->values); found_line[1] = true;
+                if (that_section->header == "ADDED_MASS_LINE_3") pimpl->fill(ret, 2, that_section->values); found_line[2] = true;
+                if (that_section->header == "ADDED_MASS_LINE_4") pimpl->fill(ret, 3, that_section->values); found_line[3] = true;
+                if (that_section->header == "ADDED_MASS_LINE_5") pimpl->fill(ret, 4, that_section->values); found_line[4] = true;
+                if (that_section->header == "ADDED_MASS_LINE_6") pimpl->fill(ret, 5, that_section->values); found_line[5] = true;
             }
         }
     }
