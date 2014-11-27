3884ab801e5aeb58666dce049a3143c55dba62ac
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-27 17:27:48 +0100

    [radiation_damping] Implemented DampingMatrixInterpolator::build_interpolator

--- code/hdb_interpolators/inc/DampingMatrixInterpolator.hpp
+++ code/hdb_interpolators/inc/DampingMatrixInterpolator.hpp
@@ -8,6 +8,15 @@
 #ifndef DAMPINGMATRIXINTERPOLATOR_HPP_
 #define DAMPINGMATRIXINTERPOLATOR_HPP_
 
+#include <ssc/macros/tr1_macros.hpp>
+#include TR1INC(memory)
+
+#include <ssc/interpolation.hpp>
+
+typedef TR1(shared_ptr)<ssc::interpolation::Interpolator> InterpolatorPtr;
+
+#include "YamlRadiationDamping.hpp"
+
 /** \brief Creates interpolation functions for the damping matrix
  *  \details Used to create the retardation functions
  *  \addtogroup hdb_interpolators
@@ -21,8 +30,7 @@ class DampingMatrixInterpolator
 {
     public:
         DampingMatrixInterpolator();
-
-    private:
+        InterpolatorPtr build_interpolator(const std::vector<double>& x, const std::vector<double>& y, const TypeOfInterpolation& type) const;
 };
 
 #endif /* DAMPINGMATRIXINTERPOLATOR_HPP_ */
--- code/hdb_interpolators/unit_tests/src/DampingMatrixInterpolatorTest.cpp
+++ code/hdb_interpolators/unit_tests/src/DampingMatrixInterpolatorTest.cpp
@@ -7,6 +7,9 @@
 
 
 #include "DampingMatrixInterpolatorTest.hpp"
+#include "DampingMatrixInterpolator.hpp"
+
+#include <ssc/macros.hpp>
 
 DampingMatrixInterpolatorTest::DampingMatrixInterpolatorTest() : a(ssc::random_data_generator::DataGenerator(212))
 {
@@ -23,14 +26,35 @@ void DampingMatrixInterpolatorTest::SetUp()
 void DampingMatrixInterpolatorTest::TearDown()
 {
 }
-
-TEST_F(DampingMatrixInterpolatorTest, example)
+double f(const double omega);
+double f(const double omega)
 {
-//! [DampingMatrixInterpolatorTest example]
-//! [DampingMatrixInterpolatorTest example]
-//! [DampingMatrixInterpolatorTest expected output]
-//! [DampingMatrixInterpolatorTest expected output]
+    return 0.5*(0.1/(0.01+(0.5-omega)*(0.5-omega))+0.1/(0.01+(0.5+omega)*(0.5+omega)));
 }
 
+TEST_F(DampingMatrixInterpolatorTest, all_types_of_interpolator_can_retrieve_original_values)
+{
+    std::vector<double> omega;
+    std::vector<double> B;
+    const double omega_min = 0;
+    const double omega_max = 4;
+    size_t N = 100;
+    for (size_t i = 0 ; i < N ; ++i)
+    {
+        omega.push_back(omega_min + (omega_max-omega_min)*((double)i)/((double)(N-1)));
+        B.push_back(f(omega.back()));
+    }
 
+    DampingMatrixInterpolator factory;
+    auto f1 = factory.build_interpolator(omega, B, TypeOfInterpolation::LINEAR);
+    auto f2 = factory.build_interpolator(omega, B, TypeOfInterpolation::PIECEWISE_CONSTANT);
+    auto f3 = factory.build_interpolator(omega, B, TypeOfInterpolation::SPLINES);
+
+    for (size_t i = 0 ; i < N ; ++i)
+    {
+        ASSERT_NEAR(B.at(i), f1->f(omega.at(i)), 1E-10) << "i = " << i;
+        ASSERT_NEAR(B.at(i), f2->f(omega.at(i)), 1E-10) << "i = " << i;
+        ASSERT_NEAR(B.at(i), f3->f(omega.at(i)), 1E-10) << "i = " << i;
+    }
+}
 
