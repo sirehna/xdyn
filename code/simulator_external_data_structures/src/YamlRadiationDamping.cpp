dbc7fb0409cc68e987398a7ceb9e4766d8d9fa7c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-11-27 13:04:54 +0100

    [radiation_damping] Created external data structure YamlRadiationDamping
    Added C++0x flags for simulator_yaml_parser

---
+++ code/simulator_external_data_structures/src/YamlRadiationDamping.cpp
@@ -0,0 +1,16 @@
+/*
+ * YamlRadiationDamping.cpp
+ *
+ *  Created on: Nov 27, 2014
+ *      Author: cady
+ */
+
+#include "YamlRadiationDamping.hpp"
+
+YamlRadiationDamping::YamlRadiationDamping() : hdb_filename(),
+                                               interpolation(),
+                                               quadrature(),
+                                               quadrature_tolerance(0),
+                                               nb_of_points_in_convolution(0)
+{
+}
