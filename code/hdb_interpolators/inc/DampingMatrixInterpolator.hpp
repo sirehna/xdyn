29444815474f47995e41d0bfd85468aeb9295c47
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-02 13:08:30 +0100

    [radiation_damping] Made DampingMatrixInterpolator::integrate private

--- code/hdb_interpolators/inc/DampingMatrixInterpolator.hpp
+++ code/hdb_interpolators/inc/DampingMatrixInterpolator.hpp
@@ -28,10 +28,11 @@ class DampingMatrixInterpolator
         DampingMatrixInterpolator(const TypeOfInterpolation& type_of_interpolation, const TypeOfQuadrature& type_of_quadrature);
         std::function<double(double)> build_interpolator(const std::vector<double>& x, const std::vector<double>& y) const;
         std::function<double(double)> make_retardation_function(const std::function<double(double)>& Br, const double omega_min, const double omega_max, const size_t n) const;
-        double integrate(const std::function<double(double)>& Br, const double tau, const double omega_min, const double omega_max) const;
 
     private:
         DampingMatrixInterpolator();
+        double integrate(const std::function<double(double)>& Br, const double tau, const double omega_min, const double omega_max) const;
+
         TypeOfInterpolation type_of_interpolation;
         TypeOfQuadrature type_of_quadrature;
 };
