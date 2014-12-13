1126b42ba8d232d0bd8d13e93d11d8abe233d36c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-13 22:11:02 +0100

    [radiation_damping] Can interpolate analytical Br properly
    Only useful for tests. Not so easy: had to split integration interval in two &
    use exponential & reversed-exponential intervals.

--- code/hdb_interpolators/unit_tests/src/RadiationDampingBuilderTest.cpp
+++ code/hdb_interpolators/unit_tests/src/RadiationDampingBuilderTest.cpp
@@ -330,3 +330,24 @@ TEST_F(RadiationDampingBuilderTest, retardation_function_should_closely_match_an
         ASSERT_NEAR(exp(-0.1*tau)*cos(0.5*tau), K(tau), 0.018) << "tau = " << tau;
     }
 }
+
+TEST_F(RadiationDampingBuilderTest, can_interpolate_Br)
+{
+    std::vector<double> vBr;
+    size_t N = 260;
+    const double omega_min = 0.01;
+    const double omega_max = 10;
+    // LINEAR does not work. Need reverse + direct exponential intervals arround omega_med. Need 260 points.
+    // Have to be careful not to miss the peak
+    RadiationDampingBuilder builder(TypeOfInterpolation::SPLINES, TypeOfQuadrature::SIMPSON);//SIMPSON);
+    const double r = 0.5;
+    const double omega_med = builder.find_r_bound(test_data::analytical_Br,omega_min,omega_max, r);
+    auto omegas = builder.build_exponential_intervals(omega_min, omega_med, (size_t)std::floor(N*r+0.5));
+    const auto omegas2 = builder.build_exponential_intervals(omega_med, omega_max, (size_t)std::floor(N*(1-r)+0.5));
+    omegas.insert(omegas.end(), omegas2.begin()+1,omegas2.end());
+    for (auto omega:omegas) vBr.push_back(test_data::analytical_Br(omega));
+    const auto Br_ = builder.build_interpolator(omegas,vBr);
+
+    const auto fine_omegas = builder.build_regular_intervals(omega_min, omega_max, 2*N);
+    for (auto omega:fine_omegas) ASSERT_NEAR(test_data::analytical_Br(omega), Br_(omega), 1E-4) << "omega = " << omega;
+}
