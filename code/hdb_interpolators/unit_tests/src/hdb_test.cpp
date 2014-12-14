363e0efd160b68d8885cf730d612053ef66c9404
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-14 08:23:40 +0100

    [radiation_damping] Optimized interpolated Br
    Simpson quadrature seems to outperform the others in this particular case.

--- code/hdb_interpolators/unit_tests/src/hdb_test.cpp
+++ code/hdb_interpolators/unit_tests/src/hdb_test.cpp
@@ -19,10 +19,10 @@ std::function<double(double)> get_interpolated_Br()
     const double omega_max = 40;
     // LINEAR does not work. Need reverse + direct exponential intervals arround omega_med. Need 260 points.
     // Have to be careful not to miss the peak
-    RadiationDampingBuilder builder(TypeOfInterpolation::SPLINES, TypeOfQuadrature::SIMPSON);//SIMPSON);
+    RadiationDampingBuilder builder(TypeOfInterpolation::SPLINES, TypeOfQuadrature::SIMPSON);
     const double r = 0.5;
     const double omega_med = builder.find_r_bound(test_data::analytical_Br,omega_min,omega_max, r);
-    auto omegas = builder.build_exponential_intervals(omega_min, omega_med, (size_t)std::floor(N*r+0.5));
+    auto omegas = builder.build_exponential_intervals_reversed(omega_min, omega_med, (size_t)std::floor(N*r+0.5));
     const auto omegas2 = builder.build_exponential_intervals(omega_med, omega_max, (size_t)std::floor(N*(1-r)+0.5));
     omegas.insert(omegas.end(), omegas2.begin()+1,omegas2.end());
     for (auto omega:omegas) vBr.push_back(test_data::analytical_Br(omega));
