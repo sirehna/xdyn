206bb8aca9a9ee4eedd499f763518a7098da1dc7
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2014-12-14 07:42:51 +0100

    [radiation_damping] Created functions to compute K & B from analytical

---
+++ code/hdb_interpolators/unit_tests/inc/hdb_test.hpp
@@ -0,0 +1,15 @@
+/*
+ * hdb_test.hpp
+ *
+ *  Created on: Dec 14, 2014
+ *      Author: cady
+ */
+
+#ifndef HDB_TEST_HPP_
+#define HDB_TEST_HPP_
+
+#include <functional>
+
+std::function<double(double)> get_interpolated_Br();
+
+#endif /* HDB_TEST_HPP_ */
---
+++ code/hdb_interpolators/unit_tests/src/hdb_test.cpp
@@ -0,0 +1,32 @@
+/*
+ * hdb_test.cpp
+ *
+ *  Created on: Dec 14, 2014
+ *      Author: cady
+ */
+
+#include <cmath>
+
+#include "hdb_data.hpp"
+#include "hdb_test.hpp"
+#include "RadiationDampingBuilder.hpp"
+
+std::function<double(double)> get_interpolated_Br()
+{
+    std::vector<double> vBr;
+    size_t N = 460;
+    const double omega_min = 0.01;
+    const double omega_max = 40;
+    // LINEAR does not work. Need reverse + direct exponential intervals arround omega_med. Need 260 points.
+    // Have to be careful not to miss the peak
+    RadiationDampingBuilder builder(TypeOfInterpolation::SPLINES, TypeOfQuadrature::SIMPSON);//SIMPSON);
+    const double r = 0.5;
+    const double omega_med = builder.find_r_bound(test_data::analytical_Br,omega_min,omega_max, r);
+    auto omegas = builder.build_exponential_intervals(omega_min, omega_med, (size_t)std::floor(N*r+0.5));
+    const auto omegas2 = builder.build_exponential_intervals(omega_med, omega_max, (size_t)std::floor(N*(1-r)+0.5));
+    omegas.insert(omegas.end(), omegas2.begin()+1,omegas2.end());
+    for (auto omega:omegas) vBr.push_back(test_data::analytical_Br(omega));
+    return builder.build_interpolator(omegas,vBr);
+}
+
+
