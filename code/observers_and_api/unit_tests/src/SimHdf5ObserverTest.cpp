d4bd817d731dfafe50a48fcd36ec351e8b4177c1
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-01-07 20:42:13 +0100

    [export_houle] Added unit test can_simulate_system_with_no_wave_output for HDF5

--- code/observers_and_api/unit_tests/src/SimHdf5ObserverTest.cpp
+++ code/observers_and_api/unit_tests/src/SimHdf5ObserverTest.cpp
@@ -11,6 +11,7 @@
 #include "StateMacros.hpp" // For StateType
 #include <ssc/solver.hpp>
 #include "yaml_data.hpp"
+#include "TriMeshTestData.hpp"
 
 SimHdf5ObserverTest::SimHdf5ObserverTest() : a(ssc::random_data_generator::DataGenerator(546545))
 {
@@ -40,5 +41,19 @@ TEST_F(SimHdf5ObserverTest, can_simulate_system_with_no_environment)
         ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, 0, tend, dt, observer);
     }
     EXPECT_EQ(0,remove(fileName.c_str()));
+}
+
+TEST_F(SimHdf5ObserverTest, can_simulate_system_with_no_wave_output)
+{
+    const double dt = 1E-1;
+    const double tend = 10;
+    const std::string fileName("can_simulate_system_with_no_wave_output.h5");
+    const std::string datasetName("simu0");
+    auto sys = get_system(test_data::anthineas_fast_hydrostatic_test(), unit_cube());
+    {
+        SimHdf5Observer observer(fileName, datasetName, sys);
+        ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, 0, tend, dt, observer);
+    }
+    //EXPECT_EQ(0,remove(fileName.c_str()));
 }
 
