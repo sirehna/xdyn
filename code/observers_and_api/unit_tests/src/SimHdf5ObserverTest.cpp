f0a72a73ce8cf772a3e8d996eaee3406381fcb33
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-01-08 20:17:28 +0100

    [export_houle] Added 3 unit tests for hdf5 export, based on CSV export unit tests

--- code/observers_and_api/unit_tests/src/SimHdf5ObserverTest.cpp
+++ code/observers_and_api/unit_tests/src/SimHdf5ObserverTest.cpp
@@ -54,7 +54,7 @@ TEST_F(SimHdf5ObserverTest, can_simulate_system_with_no_wave_output)
         SimHdf5Observer observer(fileName, datasetName, sys);
         ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, 0, tend, dt, observer);
     }
-    //EXPECT_EQ(0,remove(fileName.c_str()));
+    EXPECT_EQ(0,remove(fileName.c_str()));
 }
 
 TEST_F(SimHdf5ObserverTest, can_retrieve_the_output_wave_mesh_size)
@@ -64,3 +64,43 @@ TEST_F(SimHdf5ObserverTest, can_retrieve_the_output_wave_mesh_size)
     ASSERT_EQ(2,ms.second);
 }
 
+TEST_F(SimHdf5ObserverTest, should_be_able_to_create_an_observer_for_states_efforts_and_waves)
+{
+    const std::string fileName("should_be_able_to_create_an_observer_for_states_efforts_and_waves.h5");
+    const std::string datasetName("simu0");
+    auto sys = get_system(test_data::cube_in_waves(), unit_cube());
+    ASSERT_EQ(5,sys.get_waves_mesh_size().first);
+    ASSERT_EQ(2,sys.get_waves_mesh_size().second);
+    {
+        SimHdf5Observer observer(fileName, datasetName, sys);
+    }
+    EXPECT_EQ(0,remove(fileName.c_str()));
+}
+
+TEST_F(SimHdf5ObserverTest, can_simulate_both_environment_and_system_for_two_time_steps)
+{
+    const double dt = 1E-1;
+    const double tend = dt;
+    const std::string fileName("can_simulate_both_environment_and_system_for_two_time_steps.h5");
+    const std::string datasetName("simu0");
+    auto sys = get_system(test_data::cube_in_waves(), unit_cube());
+    {
+        SimHdf5Observer observer(fileName, datasetName, sys);
+        ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, 0, tend, dt, observer);
+    }
+    //EXPECT_EQ(0,remove(fileName.c_str()));
+}
+
+TEST_F(SimHdf5ObserverTest, can_simulate_both_environment_and_system_for_several_time_steps)
+{
+    const double dt = 1E-1;
+    const double tend = 10*dt;
+    const std::string fileName("can_simulate_both_environment_and_system_for_several_time_steps.h5");
+    const std::string datasetName("simu0");
+    auto sys = get_system(test_data::cube_in_waves(), unit_cube());
+    {
+        SimHdf5Observer observer(fileName, datasetName, sys);
+        ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, 0, tend, dt, observer);
+    }
+    //EXPECT_EQ(0,remove(fileName.c_str()));
+}
