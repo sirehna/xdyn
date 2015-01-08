80671481d6b95b74e5d0be03eb53a4b5b4adfc4a
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-01-08 20:18:19 +0100

    [export_houle] Added unit test for class SimHdf5WaveObserverBuilderTest

--- code/observers_and_api/unit_tests/src/SimHdf5WaveObserverBuilderTest.cpp
+++ code/observers_and_api/unit_tests/src/SimHdf5WaveObserverBuilderTest.cpp
@@ -29,3 +29,17 @@ TEST_F(SimHdf5WaveObserverTest, SimHdf5WaveObserver_can_be_created_from_a_H5File
     }
     EXPECT_EQ(0,remove(fileName.c_str()));
 }
+
+TEST_F(SimHdf5WaveObserverTest, SimHdf5WaveObserver_can_be_created_from_a_H5File2)
+{
+    const std::string fileName("SimHdf5WaveObserver_can_be_created_from_a_filename.h5");
+    const std::string datasetName("simu0/waveElevation");
+    const size_t nx(10);
+    const size_t ny(30);
+    {
+        const SimHdf5WaveObserverBuilder s(H5::H5File(fileName,H5F_ACC_TRUNC),datasetName,nx,ny);
+        ASSERT_EQ(nx,s.get_nx());
+        ASSERT_EQ(ny,s.get_ny());
+    }
+    EXPECT_EQ(0,remove(fileName.c_str()));
+}
