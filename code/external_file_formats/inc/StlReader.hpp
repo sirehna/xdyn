8c9c5f5530d0c9bb8e607ab335711d48fda30745
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-01-13 09:22:59 +0100

    [export_houle] Added include guard for StlReader.hpp file

--- code/external_file_formats/inc/StlReader.hpp
+++ code/external_file_formats/inc/StlReader.hpp
@@ -1,3 +1,6 @@
+#ifndef STL_READER_HPP_
+#define STL_READER_HPP_
+
 #include <string>
 #include "GeometricTypes3d.hpp"
 
@@ -41,3 +44,5 @@ VectorOfVectorOfPoints read_binary_stl(std::istream& stream);
 VectorOfVectorOfPoints read_binary_stl(const std::string& input);
 std::string escape_backslashes(const std::string& s);
 std::string replace(char c, const std::string& replacement, const std::string& s);
+
+#endif
