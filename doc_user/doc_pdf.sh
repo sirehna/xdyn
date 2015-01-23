e507d440b73ca1d174b0004c0925f25cb92a57e1
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-01-23 16:30:17 +0100

    [fixups] sed -i does not exist under Windows

--- doc_user/doc_pdf.sh
+++ doc_user/doc_pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cat introduction.md interfaces.md solver.md reperes_et_conventions.md modeles_environnementaux.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
-sed -i 's/svg/png/g' concatenated_doc.md
-pandoc -s --highlight-style pygments -o concatenated_doc.tex concatenated_doc.md
+cat concatenated_doc.md | sed -e 's/svg/png/g' > concatenated_doc2.md
+pandoc -s --highlight-style pygments -o concatenated_doc.tex concatenated_doc2.md
 pdflatex concatenated_doc.tex
 mv concatenated_doc.pdf doc.pdf
 rm *.tex
