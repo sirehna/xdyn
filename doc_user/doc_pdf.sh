0e69e5ee1ed599fab220360e25f64592fc62f8e9
sirehna <sirehna@pc-gj>
2015-01-26 15:05:09 +0100

    dev Made use of nonstopmode for pdflatex to avoid brutal stops

--- doc_user/doc_pdf.sh
+++ doc_user/doc_pdf.sh
@@ -2,7 +2,7 @@
 cat introduction.md interfaces.md solver.md reperes_et_conventions.md modeles_environnementaux.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
 cat concatenated_doc.md | sed -e 's/svg/png/g' > concatenated_doc2.md
 pandoc -s --highlight-style pygments -o concatenated_doc.tex concatenated_doc2.md
-pdflatex concatenated_doc.tex
+pdflatex -interaction=nonstopmode concatenated_doc.tex > log.txt 2> err.txt
 mv concatenated_doc.pdf doc.pdf
 rm *.tex
 rm concatenated_doc.md
