0562941980f0a3f878ac4b8b7ef39d3ba3464884
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-02-02 09:46:45 +0100

    [rudder] Added Dowygen comments

--- code/force_models/inc/RudderForceModel.hpp
+++ code/force_models/inc/RudderForceModel.hpp
@@ -23,27 +23,72 @@ class RudderForceModel : public ControllableForceModel
         struct RudderModel
         {
             virtual ~RudderModel(){}
-            double get_alpha() const;
+
+            /**  \brief Calculates the angle between the propeller's wake & the rudder
+              *  \details When positive, the wake is coming towards the rudder's port side
+              *  \returns Angle of attack (in radian)
+              *  \snippet force_models/unit_tests/src/RudderForceModelTest.cpp RudderForceModelTest get_alpha_example
+              */
+            double get_angle_of_attack() const;
+
+            /**  \brief Calculates the norm of the lift force acting on the rudder
+              *  \returns Lift force (in Newton)
+              */
             double get_lift() const;
+
+            /**  \brief Calculates the norm of the drag force acting on the rudder
+              *  \returns Drag force (in Newton)
+              */
             double get_drag() const;
+
+            /**  \brief Calculates the drag coefficient (non-dimensional)
+              */
             double get_Cd() const;
+
+            /**  \brief Calculates the lift coefficient (non-dimensional)
+              */
             double get_Cl() const;
+
+            /**  \brief Wrench created by the rudder on the ship
+             *   \details Expressed in the rudder's reference frame
+              */
             ssc::kinematics::Vector6d get_wrench() const;
 
+            /**  \brief Angle of the fluid in the ship's reference frame
+             *   \details If the fluid is propagating along -X, the angle is 0.
+              */
             virtual double get_wake_angle() const = 0;
-            virtual double get_wake_speed() const = 0;
+
+            /**  \brief Norm of the speed of the ship relative to the fluid
+             *   \details In m/s
+              */
+            virtual double get_relative_ship_speed() const = 0;
         };
 
         struct RudderInWake : RudderModel
         {
+            /**  \brief Angle of the fluid in the ship's reference frame
+             *   \details If the fluid is propagating along -X, the angle is 0.
+              */
             double get_wake_angle() const;
-            double get_wake_speed() const;
+
+            /**  \brief Norm of the speed of the ship relative to the fluid
+             *   \details In m/s
+              */
+            double get_relative_ship_speed() const;
         };
 
         struct RudderOutsideWake : RudderModel
         {
+            /**  \brief Angle of the fluid in the ship's reference frame
+             *   \details If the fluid is propagating along -X, the angle is 0.
+              */
             double get_wake_angle() const;
-            double get_wake_speed() const;
+
+            /**  \brief Norm of the speed of the ship relative to the fluid
+             *   \details In m/s
+              */
+            double get_relative_ship_speed() const;
         };
 
     private:
