ba83e34a8607860fdf138a52025b7b1a13dab00e
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-02-09 22:14:27 +0100

    [GZ] Sim only contains gravity & (exact) hydrostatic

--- code/gz_curves/src/make_sim_for_GZ.cpp
+++ code/gz_curves/src/make_sim_for_GZ.cpp
@@ -5,11 +5,23 @@
  *      Author: cady
  */
 
+#include "check_input_yaml.hpp"
 #include "make_sim_for_GZ.hpp"
 #include "simulator_api.hpp"
+#include "SimulatorYamlParser.hpp"
 
 Sim GZ::make_sim(const std::string& yaml, const std::string& stl)
 {
-    ssc::data_source::DataSource ds;
-    return get_system(yaml, stl, 0, ds);
+    ssc::data_source::DataSource command_listener;
+    auto input = SimulatorYamlParser(yaml).parse();
+    YamlBody body = input.bodies.front();
+    body.controlled_forces.clear();
+    body.external_forces.clear();
+    YamlModel gravity, hydrostatic;
+    gravity.model = "gravity";
+    hydrostatic.model = "non-linear hydrostatic (exact)";
+    body.external_forces.push_back(gravity);
+    body.external_forces.push_back(hydrostatic);
+    input.bodies[0] = body;
+    return get_system(check_input_yaml(input), stl, 0, command_listener);
 }
--- code/gz_curves/unit_tests/src/ResultantForceComputerTest.cpp
+++ code/gz_curves/unit_tests/src/ResultantForceComputerTest.cpp
@@ -5,6 +5,8 @@
  *      Author: cady
  */
 
+#include <set>
+
 #include "make_sim_for_GZ.hpp"
 #include "ResultantForceComputerTest.hpp"
 #include "ResultantForceComputer.hpp"
@@ -29,6 +31,23 @@ void ResultantForceComputerTest::SetUp()
 
 void ResultantForceComputerTest::TearDown()
 {
+}
+
+TEST_F(ResultantForceComputerTest, sim_only_contains_gravity_and_hydrostatic_forces)
+{
+    const auto s = GZ::make_sim(test_data::stable_cube_example(), test_data::cube());
+
+    std::map<std::string,std::vector<ForcePtr> > forces = s.get_forces();
+    std::set<std::string> force_models;
+    for (auto force:forces["cube"])
+    {
+        force_models.insert(force->get_name());
+    }
+    ASSERT_EQ(2, force_models.size());
+    const bool has_gravity = force_models.find("gravity") != force_models.end();
+    const bool has_hydrostatic = force_models.find("non-linear hydrostatic (exact)") != force_models.end();
+    ASSERT_TRUE(has_gravity);
+    ASSERT_TRUE(has_hydrostatic);
 }
 
 TEST_F(ResultantForceComputerTest, can_compute_resultant_force_for_a_cube)
