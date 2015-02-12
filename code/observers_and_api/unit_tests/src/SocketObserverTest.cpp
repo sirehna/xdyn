9aa57bde605a1b199cdf93b67646f09185f73278
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-02-12 11:31:29 +0100

    [html] Added a test to use a timer asynchronously

--- code/observers_and_api/unit_tests/src/SocketObserverTest.cpp
+++ code/observers_and_api/unit_tests/src/SocketObserverTest.cpp
@@ -6,10 +6,22 @@ SocketObserverTest::SocketObserverTest() : a(ssc::random_data_generator::DataGen
 {
 }
 
-TEST_F(SocketObserverTest, should_be_able_to_use_boost_asio)
+TEST_F(SocketObserverTest, should_be_able_to_use_boost_asio_timer_synchronously)
 {
     boost::asio::io_service io;
     boost::asio::deadline_timer t(io, boost::posix_time::milliseconds(1));
     t.wait();
 }
 
+void dummyFunction(const boost::system::error_code& /*e*/);
+void dummyFunction(const boost::system::error_code& /*e*/)
+{
+}
+
+TEST_F(SocketObserverTest, should_be_able_to_use_boost_asio_timer_asynchronously)
+{
+    boost::asio::io_service io;
+    boost::asio::deadline_timer t(io, boost::posix_time::milliseconds(1));
+    t.async_wait(&dummyFunction);
+    io.run();
+}
