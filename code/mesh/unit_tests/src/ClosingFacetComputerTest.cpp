5f223a0297e140bf39763a85ae7ac708b105ee83
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-03-02 11:32:25 +0100

    [bug] Created test mesh #0

--- code/mesh/unit_tests/src/ClosingFacetComputerTest.cpp
+++ code/mesh/unit_tests/src/ClosingFacetComputerTest.cpp
@@ -156,6 +156,18 @@ std::ostream& operator<<(std::ostream& os, const TestMesh& mesh)
     return os;
 }
 
+TestMesh case_0();
+TestMesh case_0()
+{
+    TestMesh ret;
+    ret.all_nodes = Eigen::MatrixXd::Zero(3,16);
+    ret.all_nodes << 0, 3, 6, 5, 6, 3, 0, 1,   3, 4, 4.5, 4,   3, 2, 1.5, 2,
+                     0, 1, 0, 3, 6, 5, 6, 3, 1.5, 2,   3, 4, 4.5, 4,   3, 2,
+                     0, 0, 0, 0, 0, 0, 0, 0,   0, 0,   0, 0,   0, 0,   0, 0;
+    ret.edges = {{13,15},{15,9},{9,11},{11,13}};
+    return ret;
+}
+
 TestMesh case_1();
 TestMesh case_1()
 {
