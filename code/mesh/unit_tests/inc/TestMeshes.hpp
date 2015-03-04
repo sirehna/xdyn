795c1131e893251881ed1566f6ffd53b79546c2b
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-03-04 16:29:03 +0100

    [bug] Put 2D test meshes in a separate file

---
+++ code/mesh/unit_tests/inc/TestMeshes.hpp
@@ -0,0 +1,30 @@
+/*
+ * TestMeshes.hpp
+ *
+ *  Created on: Mar 4, 2015
+ *      Author: cady
+ */
+
+#ifndef TESTMESHES_HPP_
+#define TESTMESHES_HPP_
+
+#include "2DMeshDisplay.hpp"
+
+TestMesh case_0();
+TestMesh case_1();
+TestMesh case_2();
+TestMesh case_3();
+TestMesh case_4();
+TestMesh case_5();
+TestMesh case_6();
+TestMesh case_7();
+TestMesh case_8();
+TestMesh case_9();
+TestMesh case_10();
+TestMesh case_11();
+TestMesh case_12();
+TestMesh case_13();
+TestMesh case_14();
+
+
+#endif /* TESTMESHES_HPP_ */
