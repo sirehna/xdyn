fce3848451864e2b1abd70a5b8026598e334cdbd
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-04-02 16:01:47 +0200

    [html] Added Json serializer

---
+++ code/observers_and_api/inc/JsonObserver.hpp
@@ -0,0 +1,35 @@
+#ifndef JSONOBSERVER_HPP_
+#define JSONOBSERVER_HPP_
+
+#include <ostream>
+#include <fstream>
+
+#include "Observer.hpp"
+
+#include <map>
+#include <string>
+typedef std::map<std::string, std::map<std::string, double> > JsonMap;
+
+class JsonObserver : public Observer
+{
+    public:
+        JsonObserver(const std::string& filename, const std::vector<std::string>& d);
+        ~JsonObserver();
+
+    private:
+        void flush_after_initialization();
+        void flush_after_write();
+        void flush_value();
+
+        bool output_to_file;
+        std::ostream& os;
+        JsonMap jsonMap;
+
+        using Observer::get_serializer;
+        using Observer::get_initializer;
+
+        std::function<void()> get_serializer(const double val, const DataAddressing& address);
+        std::function<void()> get_initializer(const double val, const DataAddressing& address);
+};
+
+#endif
---
+++ code/observers_and_api/src/JsonObserver.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include "JsonObserver.hpp"
+
+#include <utility>
+typedef std::pair<std::string,std::string> JsonMapKeyVar;
+JsonMapKeyVar extractKeyVarFromString(const std::string& s);
+JsonMapKeyVar extractKeyVarFromString(const std::string& s)
+{
+    JsonMapKeyVar j;
+    auto kS = s.find('(');
+    auto kE = s.find_last_of(')');
+    if ((kS != std::string::npos) and (kE != std::string::npos))
+    {
+        j.first = s.substr(kS+1,kE-kS-1);
+        j.second = s.substr(0,kS);
+    }
+    return j;
+}
+
+JsonObserver::JsonObserver(
+        const std::string& filename, const std::vector<std::string>& d) :
+        Observer(d),
+        output_to_file(not(filename.empty())),
+        os(output_to_file ? *(new std::ofstream(filename)) : std::cout),
+        jsonMap()
+{
+    std::cout<<filename<<std::endl;
+}
+
+JsonObserver::~JsonObserver()
+{
+    if (output_to_file) delete(&os);
+}
+
+std::function<void()> JsonObserver::get_serializer(const double val, const DataAddressing& d)
+{
+    std::cout<<"'"<<d.name<<"':"<<val<<std::endl<<std::flush;
+    return [this,d,val]()
+                      {
+                        JsonMapKeyVar j = extractKeyVarFromString(d.name);
+                        if ((not j.first.empty()) and (not j.second.empty()))
+                        {
+                           jsonMap[j.first][j.second]=val;
+                        }
+                      };
+}
+
+std::function<void()> JsonObserver::get_initializer(const double, const DataAddressing&)
+{
+    return [this](){};
+}
+
+void JsonObserver::flush_after_initialization()
+{
+}
+
+void flushMap(std::ostream& os, const std::map<std::string,double>& stuff_to_write);
+void flushMap(std::ostream& os, const std::map<std::string,double>& stuff_to_write)
+{
+    const size_t n = stuff_to_write.size();
+    size_t i = 0;
+    os << "{";
+    for (auto const& stuff:stuff_to_write)
+    {
+        os << "'"<<stuff.first<<"':"<<stuff.second;
+        if (i<(n-1)) os << ",";
+        ++i;
+    }
+    os << "}";
+}
+
+void JsonObserver::flush_after_write()
+{
+    os << "[";
+    for (auto const& object:jsonMap)
+    {
+        os << "{";
+        os << "'name':'"<<object.first<<"',";
+        if (object.first.find(',')==std::string::npos)
+        {
+            os << "'type':'attitude',";
+        }
+        else
+        {
+            os << "'type':'wrench',";
+        }
+        os << "'var':";
+        flushMap(os, object.second);
+        os << "}";
+    }
+    os << "]"<<std::endl;
+}
+
+/*
+void Observer::serialize_everything()
+{
+    const size_t n = stuff_to_write.size();
+    size_t i = 0;
+    for (auto stuff:stuff_to_write)
+    {
+        auto serialize_stuff = serialize.find(stuff);
+        if (serialize_stuff == serialize.end())
+        {
+            THROW(__PRETTY_FUNCTION__, ObserverException, std::string("Simulation does not compute '") + stuff + "'");
+        }
+        serialize_stuff->second();
+        if (i<(n-1)) flush_value();
+        ++i;
+    }
+    flush_after_write();
+}
+ */
+
+void JsonObserver::flush_value()
+{
+}
