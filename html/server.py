18001a9803c0b1b8ff13ac144afbd345a287b973
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-05-11 09:16:24 +0200

    [html] Refactored server.py & automatically open browser

--- html/server.py
+++ html/server.py
@@ -13,54 +13,58 @@ import tornado.web
 import tornado.websocket
 import tornado.ioloop
 import os
-import datetime
-import time
-import json
-import math
-
-clients = []
-global t0
-global ser
-t0 = time.time()
-
+import webbrowser
         
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("websocket_test.html")
  
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def initialize(self):
+        self.clients = []
+
     def on_message(self, message):  
       print("message received: " + message)
-      for client in clients:
+      for client in self.clients:
         client.write_message(message)
 
     def open(self):
-      clients.append(self)
-      self.write_message(u"Connected")
+      self.clients.append(self)
+      self.write_message("Connected")
       print("open")
       
     def on_close(self):
-      clients.remove(self)
+      self.clients.remove(self)
       print("close")
 
-handlers = [
-    (r"/main", MainHandler),
-    (r"/", WebSocketHandler),
-]
-settings = {
-#            static_path=os.path.join(os.path.dirname(__file__), "js"),
-     #"static_path": os.path.join(os.path.dirname(__file__), "js"),
-     "static_path": os.path.join(os.path.dirname(__file__), "static"),
-}
 
-application = tornado.web.Application(handlers, **settings)
- 
+
+class SimulatorGUI:
+    def __init__(self, address, port):
+        suffix = "main"
+        self.port = port
+        handlers = [
+            (r"/"+suffix, MainHandler),
+            (r"/",        WebSocketHandler),
+        ]
+        settings = {
+             "static_path": os.path.join(os.path.dirname(__file__), "static"),
+        }
+        self.application = tornado.web.Application(handlers, **settings)
+        self.url = address + ":" + str(port) + "/" + suffix
+
+    def open_html_page_in_browser(self, url):
+        # Open in a new tab, if possible
+        webbrowser.open(url,new=2)
+
+    def run(self):
+        self.application.listen(self.port)
+        self.mainLoop = tornado.ioloop.IOLoop.instance()
+        self.open_html_page_in_browser(self.url)
+        self.mainLoop.start()
+        
+
 if __name__ == "__main__":
-    application.listen(9002)
-    mainLoop = tornado.ioloop.IOLoop.instance()
-    #scheduler = tornado.ioloop.PeriodicCallback(timer, 100, io_loop = mainLoop)
-    #scheduler = tornado.ioloop.PeriodicCallback(checkSerial, 1, io_loop = mainLoop)
-    #scheduler.start()
-    mainLoop.start()
-    
+    gui = SimulatorGUI("http://127.0.0.1", 9002)
+    gui.run()
 
