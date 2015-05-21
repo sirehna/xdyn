221eafebf003b31314e320b91a0c81b284e3b9bd
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-05-21 14:19:13 +0200

    [html] Added a try/catch instruction in the main program

--- code/executables/src/simulator.cpp
+++ code/executables/src/simulator.cpp
@@ -37,34 +37,43 @@ ListOfObservers get_observers(const std::string& yaml, const InputData& input_da
 int main(int argc, char** argv)
 {
     InputData input_data;
-    const int error = get_input_data(argc, argv, input_data);
-    if (not(error))
+    int error = get_input_data(argc, argv, input_data);
+    try
     {
-        const ssc::text_file_reader::TextFileReader yaml_reader(input_data.yaml_filenames);
-        ssc::data_source::DataSource command_listener;
-        if (not(input_data.command_file.empty()))
+        if (not(error))
         {
-            command_listener = listen_to_file(ssc::text_file_reader::TextFileReader(std::vector<std::string>(1,input_data.command_file)).get_contents());
+            const ssc::text_file_reader::TextFileReader yaml_reader(input_data.yaml_filenames);
+            ssc::data_source::DataSource command_listener;
+            if (not(input_data.command_file.empty()))
+            {
+                command_listener = listen_to_file(ssc::text_file_reader::TextFileReader(std::vector<std::string>(1,input_data.command_file)).get_contents());
+            }
+            const auto yaml = yaml_reader.get_contents();
+            auto observer = get_observers(yaml, input_data);
+            auto sys = get_system(yaml,input_data.tstart,command_listener);
+            if (input_data.solver=="euler")
+            {
+                ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
+            }
+            else if (input_data.solver=="rk4")
+            {
+                ssc::solver::quicksolve<ssc::solver::RK4Stepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
+            }
+            else if (input_data.solver=="rkck")
+            {
+                ssc::solver::quicksolve<ssc::solver::RKCK>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
+            }
+            else
+            {
+                ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
+            }
         }
-        const auto yaml = yaml_reader.get_contents();
-        auto observer = get_observers(yaml, input_data);
-        auto sys = get_system(yaml,input_data.tstart,command_listener);
-        if (input_data.solver=="euler")
-        {
-            ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
-        }
-        else if (input_data.solver=="rk4")
-        {
-            ssc::solver::quicksolve<ssc::solver::RK4Stepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
-        }
-        else if (input_data.solver=="rkck")
-        {
-            ssc::solver::quicksolve<ssc::solver::RKCK>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
-        }
-        else
-        {
-            ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
-        }
+    }
+    catch(const ssc::exception_handling::Exception& e)
+    {
+        std::cerr <<"Error detected:"<<std::endl;
+        std::cerr << e.what();
+        error = 1;
     }
     return error;
 }
