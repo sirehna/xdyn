113f4059abb37c4c384d077bdf8dcc22bb6fcda9
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-05-22 09:03:21 +0200

    [html] Splitted solve function in two functions

--- code/executables/src/simulator.cpp
+++ code/executables/src/simulator.cpp
@@ -18,18 +18,9 @@
 
 CHECK_SSC_VERSION(0x95e96c28e3b2eedd)
 
-void solve(const InputData& input_data);
-void solve(const InputData& input_data)
+void solve(const InputData& input_data, Sim& sys, ListOfObservers& observer);
+void solve(const InputData& input_data, Sim& sys, ListOfObservers& observer)
 {
-    const ssc::text_file_reader::TextFileReader yaml_reader(input_data.yaml_filenames);
-    ssc::data_source::DataSource command_listener;
-    if (not(input_data.command_file.empty()))
-    {
-        command_listener = listen_to_file(ssc::text_file_reader::TextFileReader(std::vector<std::string>(1,input_data.command_file)).get_contents());
-    }
-    const auto yaml = yaml_reader.get_contents();
-    auto sys = get_system(yaml,input_data.tstart,command_listener);
-    auto observer = get_observers(yaml, input_data);
     if (input_data.solver=="euler")
     {
         ssc::solver::quicksolve<ssc::solver::EulerStepper>(sys, input_data.tstart, input_data.tend, input_data.initial_timestep, observer);
@@ -48,6 +39,21 @@ void solve(const InputData& input_data)
     }
 }
 
+void run_simulation(const InputData& input_data);
+void run_simulation(const InputData& input_data)
+{
+    const ssc::text_file_reader::TextFileReader yaml_reader(input_data.yaml_filenames);
+    ssc::data_source::DataSource command_listener;
+    if (not(input_data.command_file.empty()))
+    {
+        command_listener = listen_to_file(ssc::text_file_reader::TextFileReader(std::vector<std::string>(1,input_data.command_file)).get_contents());
+    }
+    const auto yaml = yaml_reader.get_contents();
+    auto sys = get_system(yaml,input_data.tstart,command_listener);
+    auto observer = get_observers(yaml, input_data);
+    solve(input_data, sys, observer);
+}
+
 int main(int argc, char** argv)
 {
     InputData input_data;
@@ -59,7 +65,7 @@ int main(int argc, char** argv)
     }
     try
     {
-        solve(input_data);
+        run_simulation(input_data);
     }
     catch(const ssc::exception_handling::Exception& e)
     {
