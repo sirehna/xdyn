93d6400bec27ef9fc485f2ea50b15f8fb4903cb4
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-06-05 16:30:29 +0200

    [visu_html] Updated error message in case of version lower than Python 3.4

--- html/setup.py
+++ html/setup.py
@@ -4,7 +4,10 @@ import sys
 from cx_Freeze import setup, Executable
 
 if sys.version_info.major<3:
-    sys.exit("Python server should be run with Python 3")
+    sys.exit("Python server should be run with at least Python 3.4")
+
+if sys.version_info.minor<4:
+    sys.exit("Python server should be run with at least Python 3.4")
 
 setup(
     name = "SimulatorServer",
