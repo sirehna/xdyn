c3391e4b7df997395fb212b903a4c6b017e7a19a
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-06-27 14:48:25 +0200

    [Kt(J)] Stubbed KtKqForceModel class

---
+++ code/force_models/inc/KtKqForceModel.hpp
@@ -0,0 +1,27 @@
+/*
+ * KtKqForceModel.hpp
+ *
+ *  Created on: Jun 27, 2015
+ *      Author: cady
+ */
+
+#ifndef KTKQFORCEMODEL_HPP_
+#define KTKQFORCEMODEL_HPP_
+
+#include "WageningenControlledForceModel.hpp"
+
+/** \details This class was created to
+ *  \addtogroup module
+ *  \ingroup module
+ *  \section ex1 Example
+ *  \snippet module/unit_tests/src/KtKqForceModelTest.cpp KtKqForceModelTest example
+ *  \section ex2 Expected output
+ *  \snippet module/unit_tests/src/KtKqForceModelTest.cpp KtKqForceModelTest expected output
+ */
+class KtKqForceModel : public WageningenControlledForceModel
+{
+    public:
+
+    private:
+};
+#endif /* KTKQFORCEMODEL_HPP_ */
---
+++ code/force_models/src/KtKqForceModel.cpp
@@ -0,0 +1,9 @@
+/*
+ * KtKqForceModel.cpp
+ *
+ *  Created on: Jun 27, 2015
+ *      Author: cady
+ */
+
+#include "KtKqForceModel.hpp"
+
