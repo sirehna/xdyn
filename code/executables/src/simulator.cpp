04c52e337a6d9e14012f4b7c54c6878f44de30b6
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-07-06 17:31:43 +0200

    [dev] Dealt the cases where one runs program with no argument and with -h option

--- code/executables/src/simulator.cpp
+++ code/executables/src/simulator.cpp
@@ -8,7 +8,7 @@
 #include <ssc/check_ssc_version.hpp>
 CHECK_SSC_VERSION(0xcb9745b34670edcb)
 
-#include <ssc/exception_handling.hpp>
+#include <iostream>
 #include "InputData.hpp"
 #include "utilities_for_InputData.hpp"
 #include "simulator_run.hpp"
@@ -16,12 +16,14 @@ CHECK_SSC_VERSION(0xcb9745b34670edcb)
 int main(int argc, char** argv)
 {
     InputData input_data;
+    if (argc==1) return display_help(argv[0], input_data);
     int error = get_input_data(argc, argv, input_data);
     if (error)
     {
         std::cerr <<"A problem occurred while parsing inputs"<<std::endl;
         return error;
     }
+    if (input_data.empty()) return EXIT_SUCCESS;
     run_simulation(input_data);
     return error;
 }
