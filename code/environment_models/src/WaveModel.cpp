7c86bdcac1e7fb6763c1cc280647443b876cb41c
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2015-07-08 09:56:10 +0200

    [dev] Removed unneeded boost include in WaveModel.cpp

--- code/environment_models/src/WaveModel.cpp
+++ code/environment_models/src/WaveModel.cpp
@@ -4,14 +4,12 @@
  *  Created on: Aug 1, 2014
  *      Author: cady
  */
-#include <boost/foreach.hpp>
 
 #include "WaveModel.hpp"
 
 WaveModel::WaveModel(const DiscreteDirectionalWaveSpectrum& spectrum_) : spectrum(spectrum_)
 {
 }
-
 
 WaveModel::~WaveModel()
 {
