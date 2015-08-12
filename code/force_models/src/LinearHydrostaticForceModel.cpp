23b204dcd744527346e488ebca1126e96492754c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-08-12 17:53:14 +0200

    Can parse linear hydrostatic YAML

--- code/force_models/src/LinearHydrostaticForceModel.cpp
+++ code/force_models/src/LinearHydrostaticForceModel.cpp
@@ -9,6 +9,8 @@
 #include "LinearHydrostaticForceModel.hpp"
 #include "SurfaceElevationInterface.hpp"
 #include "Body.hpp"
+#include "yaml.h"
+#include <ssc/yaml_parser.hpp>
 
 #include <ssc/kinematics.hpp>
 
@@ -30,6 +32,30 @@ LinearHydrostaticForceModel::Input::Input() :
             x4(),
             y4()
 {
+}
+
+LinearHydrostaticForceModel::Input LinearHydrostaticForceModel::parse(const std::string& yaml)
+{
+    std::stringstream stream(yaml);
+    YAML::Parser parser(stream);
+    YAML::Node node;
+    parser.GetNextDocument(node);
+    LinearHydrostaticForceModel::Input ret;
+    ssc::yaml_parser::parse_uv(node["z eq"], ret.z_eq);
+    ssc::yaml_parser::parse_uv(node["phi eq"], ret.phi_eq);
+    ssc::yaml_parser::parse_uv(node["theta eq"], ret.theta_eq);
+    node["K row 1"] >> ret.K1;
+    node["K row 2"] >> ret.K2;
+    node["K row 3"] >> ret.K3;
+    ssc::yaml_parser::parse_uv(node["x1"], ret.x1);
+    ssc::yaml_parser::parse_uv(node["x2"], ret.x2);
+    ssc::yaml_parser::parse_uv(node["x3"], ret.x3);
+    ssc::yaml_parser::parse_uv(node["x4"], ret.x4);
+    ssc::yaml_parser::parse_uv(node["y1"], ret.y1);
+    ssc::yaml_parser::parse_uv(node["y2"], ret.y2);
+    ssc::yaml_parser::parse_uv(node["y3"], ret.y3);
+    ssc::yaml_parser::parse_uv(node["y4"], ret.y4);
+    return ret;
 }
 
 LinearHydrostaticForceModel::LinearHydrostaticForceModel(const std::string& body_name_, const EnvironmentAndFrames& env) : ForceModel(model_name, body_name_)
--- code/force_models/unit_tests/src/LinearHydrostaticForceModelTest.cpp
+++ code/force_models/unit_tests/src/LinearHydrostaticForceModelTest.cpp
@@ -5,8 +5,16 @@
  *      Author: cady
  */
 
-
+#include "gmock/gmock.h"
+#include "LinearHydrostaticForceModel.hpp"
 #include "LinearHydrostaticForceModelTest.hpp"
+#include "yaml_data.hpp"
+
+#define _USE_MATH_DEFINE
+#include <cmath>
+#define PI M_PI
+
+using namespace testing; // So we can use 'ElementsAre' unqualified
 
 LinearHydrostaticForceModelTest::LinearHydrostaticForceModelTest() : a(ssc::random_data_generator::DataGenerator(8615615618))
 {
@@ -24,12 +32,23 @@ void LinearHydrostaticForceModelTest::TearDown()
 {
 }
 
-TEST_F(LinearHydrostaticForceModelTest, example)
+TEST_F(LinearHydrostaticForceModelTest, parser_test)
 {
-//! [LinearHydrostaticForceModelTest example]
-//! [LinearHydrostaticForceModelTest example]
-//! [LinearHydrostaticForceModelTest expected output]
-//! [LinearHydrostaticForceModelTest expected output]
+    const auto input = LinearHydrostaticForceModel::parse(test_data::linear_hydrostatics());
+    ASSERT_DOUBLE_EQ(-2, input.z_eq);
+    ASSERT_DOUBLE_EQ(1*PI/180., input.theta_eq);
+    ASSERT_DOUBLE_EQ(-3*PI/180., input.phi_eq);
+    ASSERT_THAT(input.K1, ElementsAre(1,0,0));
+    ASSERT_THAT(input.K2, ElementsAre(0,1,0));
+    ASSERT_THAT(input.K3, ElementsAre(0,0,1));
+    ASSERT_DOUBLE_EQ(10, input.x1);
+    ASSERT_DOUBLE_EQ(10, input.x2);
+    ASSERT_DOUBLE_EQ(-10, input.x3);
+    ASSERT_DOUBLE_EQ(-10, input.x4);
+    ASSERT_DOUBLE_EQ(-10, input.y1);
+    ASSERT_DOUBLE_EQ(10, input.y2);
+    ASSERT_DOUBLE_EQ(-10, input.y3);
+    ASSERT_DOUBLE_EQ(10, input.y4);
 }
 
 
