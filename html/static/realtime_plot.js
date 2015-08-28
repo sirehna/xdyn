aa5d4981c3b503308dadee0b6d2bf6e74ffa4565
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2015-08-28 08:19:10 +0200

    Can plot selected curves only

--- html/static/realtime_plot.js
+++ html/static/realtime_plot.js
@@ -43,16 +43,48 @@ function get_plotter()
         return (typeof t != 'undefined');
     }
 
+    var legend_container = $("#legend");
+    function insert_checkboxes()
+    {
+        $.each(full_plot_data, function(key, val) {
+        legend_container.append("<br/>" +
+                                "<input type='checkbox' name='" + key + "' checked='checked' id='id" + key + "'></input>" +
+                                "<label for='id" + key + "'>" + val.label + "</label>");
+        });
+        legend_container.find("input").click(plot_selected_curves);
+    }
+
+
+    function plot_selected_curves()
+    {
+
+        var selected_data_to_plot = [];
+
+        legend_container.find("input:checked").each(function ()
+                                                    {
+                                                        var key = $(this).attr("name");
+                                                        if (key && full_plot_data[key]) selected_data_to_plot.push(full_plot_data[key]);
+                                                    });
+
+        if (selected_data_to_plot.length > 0)
+        {
+            $.plot($("#graph"), selected_data_to_plot);
+        }
+    }
+
+
+   
+    var t = NaN;
     var f = function plot_yaml(yaml_data, ship_name, variable_to_plot)
     {
+        var first_run = isNaN(t);
         t = yaml_data['t'];
         y = yaml_data[variable_to_plot + '(' + ship_name + ')']
         if (data_was_received_from_websocket_at_this_instant(t))
         {
                 full_plot_data = append(yaml_data)
-                var selected_data_to_plot = full_plot_data['z(Anthineas)'];
-                selected_data_to_plot.color = 1;
-                $.plot($("#graph"), [selected_data_to_plot]);
+                if (first_run) insert_checkboxes();
+                plot_selected_curves();
         }
         if (yaml_data.hasOwnProperty('waves'))
         {
