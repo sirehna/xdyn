45848987b532ef2cc368597ae08c75f0f28f25e5
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-02-23 15:24:40 +0100

    Updated python script integration_tests

--- code/integration_tests.py
+++ code/integration_tests.py
@@ -1,10 +1,9 @@
 #!/bin/python
-import os
-
-prefix = ''     if os.name=='nt' else './'
-suffix = '.exe' if os.name=='nt' else ''
 
 def launch(executable, arguments):
+    import os
+    prefix = ''     if os.name=='nt' else './'
+    suffix = '.exe' if os.name=='nt' else ''
     return os.system(prefix + executable + suffix + ' ' + arguments)
 
 print(launch('sim', 'tutorial_03_waves.yml --dt 1 --tend 1 -w tutorial_03_results.h5'))
