6d1ddde792b261306d51a408e82ab9eed0383aa5
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-02-23 18:12:02 +0100

    Replaced spaces with tabs in stl_readerTest.cpp

--- code/external_file_formats/unit_tests/src/stl_readerTest.cpp
+++ code/external_file_formats/unit_tests/src/stl_readerTest.cpp
@@ -5,68 +5,68 @@
 
 TEST_F(StlReaderTest, number_of_facets_should_be_correct_for_a_single_facet_file)
 {
-	const VectorOfVectorOfPoints facets = read_stl(test_data::single_facet());
-	ASSERT_EQ(1, facets.size());
+    const VectorOfVectorOfPoints facets = read_stl(test_data::single_facet());
+    ASSERT_EQ(1, facets.size());
 }
 
 TEST_F(StlReaderTest, number_of_facets_should_be_correct_for_a_three_facet_file)
 {
-	const VectorOfVectorOfPoints facets = read_stl(test_data::three_facets());
-	ASSERT_EQ(3, facets.size());
+    const VectorOfVectorOfPoints facets = read_stl(test_data::three_facets());
+    ASSERT_EQ(3, facets.size());
 }
 
 TEST_F(StlReaderTest, should_be_able_to_read_a_single_facet_properly)
 {
-	const VectorOfVectorOfPoints facets = read_stl(test_data::single_facet());
-	ASSERT_EQ(1, facets.size());
-	ASSERT_DOUBLE_EQ(1.0, facets.at(0)[0][0]);
-	ASSERT_DOUBLE_EQ(2.1, facets.at(0)[0][1]);
-	ASSERT_DOUBLE_EQ(3.2, facets.at(0)[0][2]);
-	ASSERT_DOUBLE_EQ(2.1, facets.at(0)[1][0]);
-	ASSERT_DOUBLE_EQ(3.7, facets.at(0)[1][1]);
-	ASSERT_DOUBLE_EQ(4.5, facets.at(0)[1][2]);
-	ASSERT_DOUBLE_EQ(3.1, facets.at(0)[2][0]);
-	ASSERT_DOUBLE_EQ(4.5, facets.at(0)[2][1]);
-	ASSERT_DOUBLE_EQ(6.7, facets.at(0)[2][2]);
+    const VectorOfVectorOfPoints facets = read_stl(test_data::single_facet());
+    ASSERT_EQ(1, facets.size());
+    ASSERT_DOUBLE_EQ(1.0, facets.at(0)[0][0]);
+    ASSERT_DOUBLE_EQ(2.1, facets.at(0)[0][1]);
+    ASSERT_DOUBLE_EQ(3.2, facets.at(0)[0][2]);
+    ASSERT_DOUBLE_EQ(2.1, facets.at(0)[1][0]);
+    ASSERT_DOUBLE_EQ(3.7, facets.at(0)[1][1]);
+    ASSERT_DOUBLE_EQ(4.5, facets.at(0)[1][2]);
+    ASSERT_DOUBLE_EQ(3.1, facets.at(0)[2][0]);
+    ASSERT_DOUBLE_EQ(4.5, facets.at(0)[2][1]);
+    ASSERT_DOUBLE_EQ(6.7, facets.at(0)[2][2]);
 }
 
 TEST_F(StlReaderTest, should_be_able_to_read_several_facets_properly)
 {
-	const VectorOfVectorOfPoints facets = read_stl(test_data::three_facets());
-	ASSERT_EQ(3, facets.size());
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[0][0]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[0][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[0][2]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[1][0]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[1][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[1][2]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[2][0]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[2][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[2][2]);
+    const VectorOfVectorOfPoints facets = read_stl(test_data::three_facets());
+    ASSERT_EQ(3, facets.size());
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[0][0]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[0][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[0][2]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[1][0]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(0)[1][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[1][2]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[2][0]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(0)[2][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(0)[2][2]);
 
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[0][0]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[0][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[0][2]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[1][0]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[1][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[1][2]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[2][0]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[2][1]);
-	ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[2][2]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[0][0]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[0][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[0][2]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[1][0]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[1][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[1][2]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(1)[2][0]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(1)[2][1]);
+    ASSERT_DOUBLE_EQ(  1.0, facets.at(1)[2][2]);
 
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(2)[0][0]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[0][1]);
-	ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[0][2]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[1][0]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[1][1]);
-	ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[1][2]);
-	ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[2][0]);
-	ASSERT_DOUBLE_EQ( 0.50, facets.at(2)[2][1]);
-	ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[2][2]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(2)[0][0]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[0][1]);
+    ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[0][2]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[1][0]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[1][1]);
+    ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[1][2]);
+    ASSERT_DOUBLE_EQ(-0.50, facets.at(2)[2][0]);
+    ASSERT_DOUBLE_EQ( 0.50, facets.at(2)[2][1]);
+    ASSERT_DOUBLE_EQ( 0.00, facets.at(2)[2][2]);
 }
 
 TEST_F(StlReaderTest, should_throw_an_exception_if_endsolid_keyword_not_found_before_end_of_file_is_reached)
 {
-	const std::string data("solid MYSOLID\nfacet normal 0.4 0.4 0.2\nouterloop\n");
-	ASSERT_THROW(read_stl(data),MeshException);
+    const std::string data("solid MYSOLID\nfacet normal 0.4 0.4 0.2\nouterloop\n");
+    ASSERT_THROW(read_stl(data), MeshException);
 }
