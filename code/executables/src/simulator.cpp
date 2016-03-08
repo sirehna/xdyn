8b32cea25a8f5492769c08dfe26266438f4b8ea6
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2016-03-08 12:24:45 +0100

    [c++] Fixed crash when unable to parse command line
    Just catch the exception.

--- code/executables/src/simulator.cpp
+++ code/executables/src/simulator.cpp
@@ -5,6 +5,8 @@
  *      Author: cady
  */
 
+#include <exception>
+
 #include <ssc/check_ssc_version.hpp>
 CHECK_SSC_VERSION(6,0)
 
@@ -17,13 +19,22 @@ int main(int argc, char** argv)
 {
     InputData input_data;
     if (argc==1) return display_help(argv[0], input_data);
-    const int error = get_input_data(argc, argv, input_data);
-    if (error)
+    int error = 0;
+    try
     {
-        std::cerr <<"A problem occurred while parsing inputs"<<std::endl;
-        return error;
+        error = get_input_data(argc, argv, input_data);
+        if (error)
+        {
+            std::cerr <<"A problem occurred while parsing inputs" << std::endl;
+            return error;
+        }
+        if (input_data.empty()) return EXIT_SUCCESS;
+        run_simulation(input_data);
     }
-    if (input_data.empty()) return EXIT_SUCCESS;
-    run_simulation(input_data);
+    catch (const std::exception& e) 
+    {
+        std::cerr << "An internal error has occured: " << e.what() << std::endl;
+        error = -1;
+    }
     return error;
 }
