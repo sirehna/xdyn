956645bfef1be42d2004ef107a75a2a9f80d8922
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-03-10 12:10:39 +0100

    [c++] Removed undefined code snippet reference

--- code/external_file_formats/inc/stl_writer.hpp
+++ code/external_file_formats/inc/stl_writer.hpp
@@ -11,7 +11,6 @@ std::string write_stl(const VectorOfVectorOfPoints& mesh);
 ::std::ostream& operator<<(::std::ostream& os, const VectorOfVectorOfPoints& mesh);
 
 /**  \brief Writes a mesh to output stream, in binary STL format
-  *  \snippet mesh/unit_tests/src/mesh_manipulationsTest.cpp mesh_manipulationsTest read_binary_stl_example
   */
 void write_binary_stl(const VectorOfVectorOfPoints& stl, std::ostream& os);
 
