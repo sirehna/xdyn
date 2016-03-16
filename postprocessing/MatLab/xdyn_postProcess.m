29ba7206410ef8b85e61f3889fc0ae36c9d2ab78
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-03-16 09:23:28 +0100

    [postproMatlab] Created a loading function from hdf5 files

--- postprocessing/MatLab/xdyn_postProcess.m
+++ postprocessing/MatLab/xdyn_postProcess.m
@@ -32,57 +32,7 @@ end
 if isstruct(filename)
     simu = filename;
 else
-    if ischar(filename)
-        filename = {filename,'/outputs','/inputs'};
-    end
-    tbx_assert(length(filename)==3);
-    outputsGroupName = filename{2};
-    inputsGroupName = filename{3};
-    filename = filename{1};
-    if exist(filename,'file')~=2
-        disp('Input file does not exist');
-        return
-    end
-    outputsGroupName = ensureStringHasHeadingPattern(outputsGroupName, '/');
-    inputsGroupName = ensureStringHasHeadingPattern(inputsGroupName, '/');
-
-    info = h5info(filename);
-    simu = struct;
-    simu.info = h5readatt(filename,'/','Description');
-    listOfGroups = info.Groups;
-    cmp = strcmp(outputsGroupName, {listOfGroups.Name});
-    if ~any(cmp)
-        disp('Data group name does not exit');
-        return;
-    end
-    outputs = info.Groups(cmp).Groups;
-    for i=1:numel(outputs)
-        name = outputs(i).Name;
-        if strcmp(name, [outputsGroupName,'/states'])
-            simu.t = extractTime(filename, [outputsGroupName,'/t']);
-            simu.states = extractStates(filename, name, [outputsGroupName,'/t']);
-        elseif strcmp(name, [outputsGroupName,'/waves'])
-            simu.waves = tbx_wave_importWaveElevationFromHdf5(filename, name);
-        end
-    end
-
-    cmp = strcmp(inputsGroupName, {listOfGroups.Name});
-    if any(cmp)
-        inputs = info.Groups(cmp).Groups;
-        simu.meshes=struct;
-        for i=1:numel(inputs)
-            name = inputs(i).Name;
-            m = [inputsGroupName,'/meshes'];
-            if strcmp(name, m)
-                hdf5Name = inputs(i).Groups.Name;
-                meshName = getNameFromHdf5Hierarchy(hdf5Name);
-                simu.meshes.(meshName) = struct;
-                simu.meshes.(meshName).points = h5read(filename,[hdf5Name '/points']);
-                simu.meshes.(meshName).nPoints = size(simu.meshes.(meshName).points,2);
-                simu.meshes.(meshName).faces = h5read(filename,[hdf5Name '/faces'])';
-            end
-        end
-    end
+    simu = xdyn_loadResultsFromHdf5File(filename);
 end
 
 if plotResult
@@ -127,6 +77,59 @@ if plotResult
     end
 end
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function simu = xdyn_loadResultsFromHdf5File(filename)
+if ischar(filename)
+    filename = {filename,'/outputs','/inputs'};
+end
+tbx_assert(length(filename)==3);
+outputsGroupName = filename{2};
+inputsGroupName = filename{3};
+filename = filename{1};
+if exist(filename,'file')~=2
+    disp('Input file does not exist');
+    return
+end
+outputsGroupName = ensureStringHasHeadingPattern(outputsGroupName, '/');
+inputsGroupName = ensureStringHasHeadingPattern(inputsGroupName, '/');
+
+info = h5info(filename);
+simu = struct;
+simu.info = h5readatt(filename,'/','Description');
+listOfGroups = info.Groups;
+cmp = strcmp(outputsGroupName, {listOfGroups.Name});
+if ~any(cmp)
+    disp('Data group name does not exit');
+    return;
+end
+outputs = info.Groups(cmp).Groups;
+for i=1:numel(outputs)
+    name = outputs(i).Name;
+    if strcmp(name, [outputsGroupName,'/states'])
+        simu.t = extractTime(filename, [outputsGroupName,'/t']);
+        simu.states = extractStates(filename, name, [outputsGroupName,'/t']);
+    elseif strcmp(name, [outputsGroupName,'/waves'])
+        simu.waves = tbx_wave_importWaveElevationFromHdf5(filename, name);
+    end
+end
+
+cmp = strcmp(inputsGroupName, {listOfGroups.Name});
+if any(cmp)
+    inputs = info.Groups(cmp).Groups;
+    simu.meshes=struct;
+    for i=1:numel(inputs)
+        name = inputs(i).Name;
+        m = [inputsGroupName,'/meshes'];
+        if strcmp(name, m)
+            hdf5Name = inputs(i).Groups.Name;
+            meshName = getNameFromHdf5Hierarchy(hdf5Name);
+            simu.meshes.(meshName) = struct;
+            simu.meshes.(meshName).points = h5read(filename,[hdf5Name '/points']);
+            simu.meshes.(meshName).nPoints = size(simu.meshes.(meshName).points,2);
+            simu.meshes.(meshName).faces = h5read(filename,[hdf5Name '/faces'])';
+        end
+    end
+end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function t = extractTime(filename, time)
 t = h5read(filename, time);
