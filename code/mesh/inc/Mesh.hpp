35c209caef461f7729ac814adbe56995bc2cf9c2
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-07-05 17:41:49 +0200

    [c++] Transfered data structure from Mesh to GeometricTypes3d

--- code/mesh/inc/Mesh.hpp
+++ code/mesh/inc/Mesh.hpp
@@ -1,35 +1,11 @@
 #ifndef MESH_HPP
 #define MESH_HPP
 
-#include <array>
 #include <vector>
 #include "GeometricTypes3d.hpp"
 
 #include <ssc/macros.hpp>
 #include TR1INC(memory)
-
-typedef std::array<std::vector<size_t>,2> ArrayOfEdges;
-
-/**
- * \details Holds the indices of the vertices in the mesh rather than duplicating the coordinates
- * \ingroup mesh
- */
-struct Facet
-{
-    Facet():vertex_index(),unit_normal(Eigen::MatrixXd::Zero(3,1)),centre_of_gravity(Eigen::MatrixXd::Zero(3,1)),area(0) {}
-    Facet(const std::vector<size_t> &vertex_index_,const Eigen::Vector3d &unit_normal_,const Eigen::Vector3d &barycenter_,double area_)
-    :vertex_index(vertex_index_)
-    ,unit_normal(unit_normal_)
-    ,centre_of_gravity(barycenter_)
-    ,area(area_) {}
-    std::vector<size_t> vertex_index;
-    Eigen::Vector3d unit_normal;
-    Eigen::Vector3d centre_of_gravity;
-    double area;
-};
-
-typedef std::vector<Facet> VectorOfFacet;
-
 
 /**
  * \author gj
