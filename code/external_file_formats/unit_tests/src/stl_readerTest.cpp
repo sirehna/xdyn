15cf5d7734fd5919f016990f91988f5a3206ea63
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-07-12 19:09:29 +0200

    [fix_bug_stl_reader] Added a test for isStlDataBinary

--- code/external_file_formats/unit_tests/src/stl_readerTest.cpp
+++ code/external_file_formats/unit_tests/src/stl_readerTest.cpp
@@ -8,6 +8,13 @@ TEST_F(StlReaderTest, should_be_able_to_detect_ascii_file)
     ASSERT_FALSE(isStlDataBinary(test_data::single_facet()));
 }
 
+TEST_F(StlReaderTest, should_be_able_to_detect_ascii_file_even_with_starting_spaces)
+{
+    ASSERT_FALSE(isStlDataBinary("  " + test_data::single_facet()));
+    ASSERT_FALSE(isStlDataBinary("\t" + test_data::single_facet()));
+    ASSERT_FALSE(isStlDataBinary("   \t  " + test_data::single_facet()));
+}
+
 TEST_F(StlReaderTest, number_of_facets_should_be_correct_for_a_single_facet_file)
 {
     const VectorOfVectorOfPoints facets = read_stl(test_data::single_facet());
