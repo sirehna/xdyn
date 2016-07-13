4c11efef95da1e830f7ae92156f5f4385a04f67f
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-07-13 13:50:53 +0200

    [fix_tutorials] Updated command instructions for reference_data_for_validation/houlePointFixe

--- validation/reference_data_for_validation/houlePointFixe/donneesSimu/simu.sh
+++ validation/reference_data_for_validation/houlePointFixe/donneesSimu/simu.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 xdyn anthineas_essaisECN_run10.yml controlled_forces_commands.yml --dt 0.1 --tend 150 -o run10.h5
 xdyn anthineas_essaisECN_run15.yml controlled_forces_commands.yml --dt 0.1 --tend 150 -o run15.h5
 xdyn anthineas_essaisECN_run17.yml controlled_forces_commands.yml --dt 0.1 --tend 150 -o run17.h5
