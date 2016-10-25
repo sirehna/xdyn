91fb7e5e0cbad173070969443c3892598df55ac0
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2016-10-25 15:08:07 +0200

    [vagrant_conf] Updated the bootstrap.sh to have it worked with Vagrant SSC

--- bootstrap.sh
+++ bootstrap.sh
@@ -50,21 +50,32 @@ sudo apt-get install git -y
 # SUBVERSION
 sudo apt-get install subversion -y
 
-# BOOST 1.55
-svn co http://svn.boost.org/svn/boost/tags/release/Boost_1_55_0/
-svn co http://svn.boost.org/svn/boost/trunk/boost/geometry/extensions
-cd Boost_1_55_0
+# # BOOST 1.55
+# svn co http://svn.boost.org/svn/boost/tags/release/Boost_1_55_0/
+# svn co http://svn.boost.org/svn/boost/trunk/boost/geometry/extensions
+# cd Boost_1_55_0
+# ./bootstrap.sh
+# sudo ./b2 cxxflags=-fPIC link=static link=shared threading=single threading=multi --layout=tagged --prefix=/usr/local/boost_1_55_0 install
+# sudo cp -r /home/vagrant/extensions /usr/local/boost_1_55_0/include/boost/geometry/
+
+# BOOST 1.60
+BOOST_INSTALL=/usr/local/boost_1_60_0
+wget http://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz
+tar -xf boost_1_60_0.tar.gz
+cd boost_1_60_0
 ./bootstrap.sh
-sudo ./b2 cxxflags=-fPIC link=static link=shared threading=single threading=multi --layout=tagged --prefix=/usr/local/boost_1_55_0 install
-sudo cp -r /home/vagrant/extensions /usr/local/boost_1_55_0/include/boost/geometry/
+# link=shared threading=multi
+sudo ./b2 cxxflags=-fPIC link=static threading=single --layout=tagged --prefix=${BOOST_INSTALL} install
+sudo chown -R vagrant boost_1_60_0
+sudo chgrp -R vagrant boost_1_60_0
 
 # GMOCK
 cd /vagrant/code
 sudo rm -rf googletest
 git clone https://github.com/google/googletest.git googletest
-#cd googletest
-#git checkout release-1.7.0
-#cd ..
+cd googletest
+git checkout release-1.8.0
+cd ..
 sudo chown -R vagrant gmock
 sudo chgrp -R vagrant gmock
 
@@ -88,11 +99,11 @@ sudo chgrp -R vagrant eigen3-hdf5
 cd /vagrant/code
 sudo rm -rf yaml-cpp
 git clone https://github.com/jbeder/yaml-cpp.git
+sudo chown -R vagrant yaml-cpp
+sudo chgrp -R vagrant yaml-cpp
 cd yaml-cpp
 git checkout tags/release-0.3.0
 cd ..
-sudo chown -R vagrant yaml-cpp
-sudo chgrp -R vagrant yaml-cpp
 cp ../yaml-cpp-CMakeLists.txt yaml-cpp/CMakeLists.txt
 
 # GCOVR
@@ -105,17 +116,18 @@ cd ..
 sudo chown -R vagrant gcovr
 sudo chgrp -R vagrant gcovr
 
-
 # CMAKE
-wget https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
-tar xzf cmake-3.5.2.tar.gz
-cd cmake-3.5.2
-./configure
-make
-sudo make install
-
+sudo apt-get install -y cmake
+# wget https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
+# tar xzf cmake-3.5.2.tar.gz
+# cd cmake-3.5.2
+# ./configure
+# make
+# sudo make install
 
 # HDF5
+cd ~
+HDF5_INSTALL=/usr/local/hdf5
 wget https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.12/src/hdf5-1.8.12.tar.gz -O hdf5_source.tar.gz
 mkdir -p hdf5_source
 tar -xf hdf5_source.tar.gz --strip 1 -C ./hdf5_source
@@ -131,21 +143,20 @@ cmake \
 "-DHDF5_ENABLE_F2003:BOOL=ON" \
 "-DHDF5_BUILD_CPP_LIB:BOOL=ON" \
 "-DHDF5_BUILD_TOOLS:BOOL=ON" \
-"-DCMAKE_INSTALL_PREFIX:PATH=/usr/local/hdf5" \
+"-DCMAKE_INSTALL_PREFIX:PATH=${HDF5_INSTALL}" \
 "-DCMAKE_C_FLAGS=-fpic" \
 "-DCMAKE_CXX_FLAGS=-fpic"
 sudo make install
 
-
-# SSC
+# SSC (Sirehna Scientific Computing)
 cd /vagrant
 sudo dpkg -i ssc.deb
 
 # FIRST BUILD
 cd /vagrant/code
-mkdir build
-cd build
-cmake .. -DBOOST_ROOT=/usr/local/boost_1_55_0 -DHDF5_DIR=/usr/local/hdf5 -G Ninja
+mkdir build_vagrant
+cd build_vagrant
+cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT:PATH=${BOOST_INSTALL} -DHDF5_DIR:PATH=${HDF5_INSTALL} -G Ninja
 ninja
 cpack
 
