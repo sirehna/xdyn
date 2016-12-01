4b0a56e336bc5866528e40a70f7c08cdd8ab55f8
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2016-12-01 17:46:28 +0100

    [vagrant_conf] Can add flags to vagrant's CMake
    Eg. to compile in debug mode.

--- vagrant_cmake.sh
+++ vagrant_cmake.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vagrant ssh -c 'cd /vagrant/code/build_vagrant && cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=/usr/local/boost_1_60_0/ -DHDF5_DIR=/usr/local/hdf5 -G Ninja'
+vagrant ssh -c "cd /vagrant/code/build_vagrant && cmake .. -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=/usr/local/boost_1_60_0/ -DHDF5_DIR=/usr/local/hdf5 -G Ninja `echo $1`"
