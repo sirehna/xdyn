9b082fac8c9302e64041833b8dc1adae89ffc7f0
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2016-12-20 12:19:11 +0100

    [stretching] Stubbed StretchingTest

--- code/environment_models/unit_tests/src/StretchingTest.cpp
+++ code/environment_models/unit_tests/src/StretchingTest.cpp
@@ -7,6 +7,7 @@
 
 #include "StretchingTest.hpp"
 #include "Stretching.hpp"
+#include "YamlWaveModelInput.hpp"
 
 StretchingTest::StretchingTest() : a(ssc::random_data_generator::DataGenerator(88223232))
 {
@@ -27,6 +28,9 @@ void StretchingTest::TearDown()
 TEST_F(StretchingTest, single_frequency_single_direction_at_one_point)
 {
     //! [StretchingTest example]
+    YamlStretching yaml;
+    yaml.delta = 0.4;
+    yaml.h = 123;
     //! [StretchingTest example]
     //! [StretchingTest expected output]
     //! [StretchingTest expected output]
