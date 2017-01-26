0ceb7666bbae76b18137d3555ae90eeae0b30ede
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2017-01-26 17:45:26 +0100

    [ci_docker] Removed tabs in observers_and_api/unit_tests/src/ConfBuilderTest.cpp

--- code/observers_and_api/unit_tests/src/ConfBuilderTest.cpp
+++ code/observers_and_api/unit_tests/src/ConfBuilderTest.cpp
@@ -25,7 +25,7 @@ TEST_F(ConfBuilderTest, compute_right_value_of_Tmax_falling_ball)
 //! [ConfBuilderTest example]
     ConfBuilder builder(test_data::falling_ball_example());
 
-	ASSERT_EQ(builder.Tmax, 0);
+    ASSERT_EQ(builder.Tmax, 0);
 
 
 //! [ConfBuilderTest expected output]
