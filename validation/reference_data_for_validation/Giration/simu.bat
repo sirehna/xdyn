fd9d5e35d35b6ab244c36fe8668282457f084379
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2017-06-19 11:51:20 +0200

    [env_export] Added a batch script to run test case giration

--- validation/reference_data_for_validation/Giration/simu.bat
+++ validation/reference_data_for_validation/Giration/simu.bat
@@ -1,1 +1,1 @@
-xdyn anthineas_giration.yml --dt 0.2 --tend 1770 -c controlled_forces_commands.yml -o giration.h5
+xdyn anthineas_giration.yml controlled_forces_commands.yml --dt 0.2 --tend 1770 -o giration.h5
