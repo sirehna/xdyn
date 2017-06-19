41619802aafbf49da94171f8e8fb671da2894af9
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2017-06-19 12:09:27 +0200

    [env_export] Updated the spectra hdf5 export

--- code/observers_and_api/inc/Hdf5WaveSpectrumObserver.hpp
+++ code/observers_and_api/inc/Hdf5WaveSpectrumObserver.hpp
@@ -1,11 +1,10 @@
 #ifndef HDF5WAVESPECTRUMOBSERVER_HPP_
 #define HDF5WAVESPECTRUMOBSERVER_HPP_
 
-
 #include "DiscreteDirectionalWaveSpectrum.hpp"
-#include "SurfaceElevationFromWaves.hpp"
 
 #include <string>
+#include <vector>
 #include "H5Cpp.h"
 
 void hdf5WaveSpectrumObserver(
@@ -19,13 +18,13 @@ void hdf5WaveSpectrumObserver(
         const DiscreteDirectionalWaveSpectrum& s);
 
 void hdf5WaveSpectrumObserver(
-        const std::string& fileName,
-        const std::string& datasetName,
-        const SurfaceElevationFromWaves& s);
-
-void hdf5WaveSpectrumObserver(
         const H5::H5File& h5File,
         const std::string& datasetName,
-        const SurfaceElevationFromWaves& s);
+        const std::vector<DiscreteDirectionalWaveSpectrum>& s);
+
+void hdf5WaveSpectrumObserver(
+        const std::string& fileName,
+        const std::string& datasetName,
+        const std::vector<DiscreteDirectionalWaveSpectrum>& s);
 
 #endif
--- code/observers_and_api/src/Hdf5WaveSpectrumObserver.cpp
+++ code/observers_and_api/src/Hdf5WaveSpectrumObserver.cpp
@@ -1,7 +1,5 @@
 #include "Hdf5WaveSpectrumObserver.hpp"
-#include "eigen3-hdf5.hpp"
 #include "h5_tools.hpp"
-#include <vector>
 #include <iostream>
 #include <sstream>
 #include <iomanip>
@@ -9,7 +7,7 @@
 void hdf5WaveSpectrumObserver(
         const std::string& fileName,
         const std::string& datasetName,
-        const SurfaceElevationFromWaves& s)
+        const std::vector<DiscreteDirectionalWaveSpectrum>& s)
 {
     H5::H5File h5File = H5_Tools::openOrCreateAHdf5File(fileName);
     hdf5WaveSpectrumObserver(h5File, datasetName, s);
@@ -19,18 +17,77 @@ void hdf5WaveSpectrumObserver(
 void hdf5WaveSpectrumObserver(
         const H5::H5File& h5File,
         const std::string& datasetName,
-        const SurfaceElevationFromWaves& s)
+        const std::vector<DiscreteDirectionalWaveSpectrum>& s)
 {
     const std::string dn(H5_Tools::ensureStringStartsAndEndsWithAPattern(datasetName,"/"));
     H5::Group group = H5_Tools::createMissingGroups(h5File, dn);
     size_t index = 0;
-    for (const auto& ss: s.get_models())
+    for (const auto& ss: s)
     {
         std::stringstream ssindex;
         ssindex << std::setfill('0') << std::setw(2) << index;
-        hdf5WaveSpectrumObserver(h5File, dn + "/" + ssindex.str(), ss->get_spectrum());
+        hdf5WaveSpectrumObserver(h5File, dn + "/" + ssindex.str(), ss);
         index++;
     }
+}
+
+template <typename T>
+std::vector<T> flatten(const std::vector<std::vector<T> >& v);
+
+template <typename T>
+std::vector<T> flatten(const std::vector<std::vector<T> >& v)
+{
+    std::size_t total_size = 0;
+    for (const auto& sub : v)
+        total_size += sub.size();
+    std::vector<T> result;
+    result.reserve(total_size);
+    for (const auto& sub : v)
+        result.insert(result.end(), sub.begin(), sub.end());
+    return result;
+}
+
+void _writeHdf5Data(const std::string& name, const double& v, H5::Group& group);
+void _writeHdf5Data(const std::string& name, const double& v, H5::Group& group)
+{
+    H5::DataType datatype(H5::PredType::NATIVE_DOUBLE);
+    hsize_t dimsf[2];
+    dimsf[0] = 1;
+    dimsf[1] = 1;
+    H5::DataSpace dataspace(2, dimsf);
+    H5::DataSet dataset = group.createDataSet(name, datatype, dataspace);
+    dataset.write(&v, H5::PredType::NATIVE_DOUBLE);
+    dataset.close();
+    dataspace.close();
+}
+
+void _writeHdf5Data(const std::string& name, const std::vector<double>& v, H5::Group& group);
+void _writeHdf5Data(const std::string& name, const std::vector<double>& v, H5::Group& group)
+{
+    H5::DataType datatype(H5::PredType::NATIVE_DOUBLE);
+    hsize_t dimsf[2];
+    dimsf[0] = (hsize_t)v.size();
+    dimsf[1] = 1;
+    H5::DataSpace dataspace(2, dimsf);
+    H5::DataSet dataset = group.createDataSet(name, datatype, dataspace);
+    dataset.write(&v[0], H5::PredType::NATIVE_DOUBLE);
+    dataset.close();
+    dataspace.close();
+}
+
+void _writeHdf5Data(const std::string& name, const std::vector<std::vector<double> >& v, H5::Group& group);
+void _writeHdf5Data(const std::string& name, const std::vector<std::vector<double> >& v, H5::Group& group)
+{
+    H5::DataType datatype(H5::PredType::NATIVE_DOUBLE);
+    hsize_t dimsf[2];
+    dimsf[0] = (hsize_t)v.size();
+    dimsf[1] = (hsize_t)v.at(0).size();
+    H5::DataSpace dataspace(2, dimsf);
+    H5::DataSet dataset = group.createDataSet(name, datatype, dataspace);
+    const auto vflat = flatten(v);
+    dataset.write(&vflat[0], H5::PredType::NATIVE_DOUBLE);
+    dataset.close();
+    dataspace.close();
 }
 
 void hdf5WaveSpectrumObserver(
@@ -41,17 +98,12 @@ void hdf5WaveSpectrumObserver(
     const std::string dn(H5_Tools::ensureStringStartsAndEndsWithAPattern(datasetName,"/"));
     H5::Group group = H5_Tools::createMissingGroups(h5File, dn);
 
-    // dataset dimensions
-    hsize_t dimsf[2];
-    dimsf[0] = (hsize_t)s.omega.size();
-    dimsf[1] = 1;
-    H5::DataSpace dataspace(2, dimsf);
-
-    H5::DataType datatype(H5::PredType::NATIVE_DOUBLE);
-    H5::DataSet dataset = group.createDataSet("omega", datatype, dataspace);
-    dataset.write(&s.omega[0], H5::PredType::NATIVE_DOUBLE);
-    dataset.close();
-    dataspace.close();
+    _writeHdf5Data("omega", s.omega, group);
+    _writeHdf5Data("Si", s.Si, group);
+    _writeHdf5Data("Dj", s.Dj, group);
+    _writeHdf5Data("psi", s.psi, group);
+    _writeHdf5Data("k", s.k, group);
+    _writeHdf5Data("phase", s.phase, group);
 }
 
 void hdf5WaveSpectrumObserver(
