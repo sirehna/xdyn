5b98d8ada321915a2087a43fb611bd4348f1d610
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2017-06-28 17:11:44 +0200

    [docker] Fixed build dir

--- docker_build.sh
+++ docker_build.sh
@@ -3,32 +3,31 @@
 # Get SSC
 cd ssc_getter
 docker build -f Dockerfile -t get_ssc .
-#docker run -it --rm -v $(pwd):/out --net=host get_ssc
-docker run -it --rm -v /etc/group:/etc/group:ro  -v /etc/passwd:/etc/passwd:ro -u $( id -u $USER ):$( id -g $USER ) -v $(pwd):/out --net=host -w /out get_ssc python3 fetch_ssc.py
+docker run -it --rm -v /etc/group:/etc/group:ro  -v /etc/passwd:/etc/passwd:ro -u $( id -u $USER ):$( id -g $USER ) -v $(pwd):/out --net=host -w /out --net=host get_ssc python3 fetch_ssc.py
 unzip artifacts.zip
 rm artifacts.zip
 mv ssc.deb ..
 cd ..
 
 
-mkdir -p build
+mkdir -p code/build
 
 # Build X-DYN container
 docker build -f Dockerfile -t xdyn .
 
 # Run CMake
-docker run --rm -v $(pwd)/build:/build -w /build -v $(pwd):/opt/share -it xdyn cmake -Wno-dev \
+docker run --rm -v $(pwd):/opt/share -w /opt/share/code/build -it xdyn cmake -Wno-dev \
     -G Ninja \
     -DINSTALL_PREFIX:PATH=/opt/xdyn \
     -Dssc_DIR:PATH=/opt/ssc/lib/ssc/cmake \
     -DHDF5_DIR:PATH=/usr/local/hdf5 \
     -DBOOST_ROOT:PATH=/usr/local/boost_1_60_0 \
-    /opt/share/code 
+    ..
 
 
 # Build
-docker run --rm -v $(pwd)/build:/build -w /build -v $(pwd):/opt/share -it xdyn ninja
+docker run --rm -v $(pwd):/opt/share -w /opt/share/code/build -it xdyn ninja
 
 # Run all tests
-docker run --rm -v $(pwd)/build:/build -w /build -v $(pwd):/opt/share -it xdyn ./run_all_tests --gtest_output=xml:run_all_tests.xml 
+docker run --rm -v $(pwd):/opt/share -w /opt/share/code/build -it xdyn ./run_all_tests --gtest_output=xml:run_all_tests.xml 
 
