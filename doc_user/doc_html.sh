c15b8a8597ef77fae96201219388fe33c2b8c470
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2017-07-03 14:32:03 +0200

    [bug_3200] Fixed missing markdown in doc_html.sh

--- doc_user/doc_html.sh
+++ doc_user/doc_html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #!/bin/sh
-cat introduction.md interfaces.md modeles_environnementaux.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
+cat introduction.md interfaces.md reperes_et_conventions.md modeles_environnementaux.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
 pandoc -s --toc --mathml -f markdown concatenated_doc.md -t html --highlight-style pygments -o doc.html -c stylesheet.css
 rm -f concatenated_doc*.md
 
