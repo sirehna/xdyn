cf025dbbe16e8a676386ccfe17f554339c54e184
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2017-07-11 18:05:03 +0200

    [use_auto_report_for_tutorials] Works (with change in SPT)
    Cf. SPT commit e438ed9b8ac4dd64b394616d9aa6c0f404247bb3

--- deploy_xdyn/deploy.sh
+++ deploy_xdyn/deploy.sh
@@ -5,4 +5,5 @@ tar xzf data.tar.gz
 rm -rf user control.tar.gz  data.tar.gz  debian-binary usr/demos usr/doc usr/matlab
 docker build -t xdyn -f Dockerfile .
 rm -rf usr
+rm -rf xdyn.tar.gz
 docker save xdyn | gzip -c > xdyn.tar.gz
