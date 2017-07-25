8361e67deb2319239947ae3b2ebb657e55b7a67c
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2017-07-25 14:24:53 +0200

    [docker] Added compression when saving docker image

--- make_docker_image/make_docker_image.sh
+++ make_docker_image/make_docker_image.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-rm -rf user control.tar.gz  data.tar.gz  debian-binary usr
+rm -rf user control.tar.gz data.tar.gz debian-binary usr
 ar x xdyn.deb
 tar xzf data.tar.gz
-rm -rf user control.tar.gz  data.tar.gz  debian-binary usr/demos usr/doc usr/matlab
+rm -rf user control.tar.gz data.tar.gz  debian-binary usr/demos usr/doc usr/matlab
 docker build -t xdyn -f Dockerfile .
 rm -rf usr
-rm -rf xdyn.tar.gz
+rm -rf xdyn.tar.gz || true
 docker save xdyn | gzip -c > xdyn.tar.gz
