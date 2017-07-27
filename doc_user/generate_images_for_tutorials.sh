a2fb7cc5bced450152c755e2a6e40b5e38c87bfe
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2017-07-27 12:04:38 +0200

    [doc] Fixed broken command for tutorial

--- doc_user/generate_images_for_tutorials.sh
+++ doc_user/generate_images_for_tutorials.sh
@@ -5,6 +5,6 @@
 ./xdyn tutorial_02_fast_hydrostatic.yml --dt 0.1 --tend 10 -o csv| python plot.py "tutorial_02_fast_hydrostatic_y" 0 2
 ./xdyn tutorial_02_exact_hydrostatic.yml --dt 0.1 --tend 10 -o csv| python plot.py "tutorial_02_exact_hydrostatic_y" 0 2
 ./xdyn tutorial_03_waves.yml --dt 1 --tend 1 -w tutorial_03_results.yml
-./xdyn tutorial_06_propulsion.yml --dt 0.1 --tend 20 -c controlled_forces_commands.yml -o csv| python plot.py "tutorial_06_propulsion_u" 0 4
+./xdyn tutorial_06_propulsion.yml controlled_forces_commands.yml --dt 0.1 --tend 20 -o csv| python plot.py "tutorial_06_propulsion_u" 0 4
 
 mv *.svg "$1"
