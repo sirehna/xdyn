776aac85ef48505d0a866ece42501dc7c0b29e04
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2017-10-19 08:17:22 +0200

    [bug_3210] Added HDB test data
    Cf. Bugzilla file https://bugzilla.sirehna.com/attachment.cgi?id=205 (v00_complet_FK_diffr.HDB)

--- code/test_data_generator/inc/hdb_data.hpp
+++ code/test_data_generator/inc/hdb_data.hpp
@@ -8,6 +8,7 @@ namespace test_data
     std::string anthineas_hdb();
     double analytical_Br(const double omega);
     double analytical_K(const double tau);
+    std::string bug_3210();
 }
 
 #endif
