23b6082db7f9fa13a1061c3a0c0b2daef6b8a57b
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2017-11-29 15:47:35 +0100

    Created YamlHOS to store HOS' configuration parameters

---
+++ code/external_data_structures/src/YamlHOS.cpp
@@ -0,0 +1,28 @@
+/*
+ * YamlHOS.cpp
+ *
+ *  Created on: Nov 29, 2017
+ *      Author: cady
+ */
+
+
+
+#include "YamlHOS.hpp"
+
+YamlHOS::YamlHOS()
+:n1()
+,n2()
+,m()
+,p1()
+,p2()
+,err(ErrorType::RELATIVE)
+,toler()
+,xlen()
+,ylen()
+,depth()
+,gamma()
+,beta()
+,tp_real()
+,hs_real()
+,address_brokerHOS()
+{}
