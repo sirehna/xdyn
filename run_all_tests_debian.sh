4d20d40c3be4bef70ab0811e41c71de840a49a89
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2018-03-23 15:31:38 +0100

    Fixed Debian build
    Use shared libraries for Debian & static for Windows.
    Ugly as hell, but works.
    Static libraries for Debian don't work: run_all_tests segfaults when
    initializing the data source for reasons beyond human comprehension.

--- run_all_tests_debian.sh
+++ run_all_tests_debian.sh
@@ -2,4 +2,3 @@
 # Using --security-opt seccomp=unconfined to avoid GDB error: warning: Error disabling address space randomization: Operation not permitted
 # As per https://stackoverflow.com/questions/35860527/warning-error-disabling-address-space-randomization-operation-not-permitted
 docker run $TERMINAL --security-opt seccomp=unconfined --rm -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -u $( id -u $USER ):$( id -g $USER ) -v $(pwd)/build_debian:/build -w /build -v $(pwd):/opt/share -t build-xdyn-debian ./run_all_tests `echo $*`
---gtest_output=xml:run_all_tests.xml
