b3d7799afe0d195522eea083efb2724fd53d30cf
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2018-03-23 13:39:50 +0100

    Simplified cross-compilation using scripts & Makefiles

---
+++ ninja_windows.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --name xdyn-package-windows --rm -v /etc/group:/etc/group:ro  -v /etc/passwd:/etc/passwd:ro -u $(id -u $USER):$(id -g $USER) -v $(pwd):/opt/share -w /opt/share mydockcross/windows-x64 /bin/bash -c "mkdir -p /opt/share/.wine && export WINEPREFIX=/opt/share/.wine && wine winecfg && cd build_windows && ninja `echo $*` "
