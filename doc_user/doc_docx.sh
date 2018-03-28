5c310244e2caca1a98472d1cfc2f64827a2693d8
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-03-28 17:54:49 +0200

    [doc] Added missing markdown input file in doc_docx.sh

--- doc_user/doc_docx.sh
+++ doc_user/doc_docx.sh
@@ -5,7 +5,7 @@ then
 else
     f=user_guide_fr
 fi
-cat introduction.md interfaces.md modeles_environnementaux.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
+cat introduction.md interfaces.md reperes_et_conventions.md modeles_environnementaux.md calcul_de_GM.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
 cat concatenated_doc.md | sed -e 's/svg/png/g' > concatenated_doc2.md
 pandoc -s --latexmathml --highlight-style pygments -t docx -o ${f}.docx concatenated_doc2.md
 rm -f concatenated_doc.md concatenated_doc2.md
