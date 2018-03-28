7d1e8791cd03f856b04bc2a5316503cde22acaaa
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-03-28 18:59:25 +0200

    [doc] Updated doc_pdf.sh script not to crash if pdflatex is not installed

--- doc_user/doc_pdf.sh
+++ doc_user/doc_pdf.sh
@@ -2,8 +2,15 @@
 cat introduction.md interfaces.md reperes_et_conventions.md modeles_environnementaux.md calcul_de_GM.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.md
 cat concatenated_doc.md | sed -e 's/svg/png/g' > concatenated_doc2.md
 pandoc -s --highlight-style pygments -o concatenated_doc.tex concatenated_doc2.md
-pdflatex -interaction=nonstopmode concatenated_doc.tex > log.txt 2> err.txt
-mv concatenated_doc.pdf doc.pdf
+
+if which pdflatex &> /dev/null; then
+    pdflatex -interaction=nonstopmode concatenated_doc.tex > log.txt 2> err.txt
+    mv concatenated_doc.pdf doc.pdf
+else
+    echo "No pdflatex program installed"
+    echo "No pdflatex program installed" > doc.pdf
+fi
+
 rm -f *.tex
 rm -f *.log
 rm -f *.aux
