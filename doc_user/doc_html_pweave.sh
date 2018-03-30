73b56d856e67949fa33f78dfe6ce3c5531734f13
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-03-30 18:04:03 +0200

    [doc_pweave] Updated doc_html_pweave sheel script to run tutorials

--- doc_user/doc_html_pweave.sh
+++ doc_user/doc_html_pweave.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+# Script to be run from docker container to generate doc with tutorials
 cat introduction.md interfaces.md reperes_et_conventions.md modeles_environnementaux.md calcul_de_GM.md courbes_de_GZ.md solver.md diffraction_radiation.md modeles_efforts.md tutorial_*.md > concatenated_doc.pmd
 cd images
 python3 spectrum.py
 cd ..
+# Copy tuto directory to allow write operations for docker run
+rm -rf tuto_execution
+cp -rf /usr/demos/. tuto_execution
 pweave -f markdown -k python3 --cache-directory=cache concatenated_doc.pmd -o concatenated_doc.md
 pandoc --title-prefix=XDYN --standalone --self-contained -t html \
        --toc --toc-depth=3 --mathml --highlight-style pygments \
        -f markdown -V lang=fr concatenated_doc.md --css stylesheet.css \
        -o doc.html
 rm concatenated_doc.pmd concatenated_doc.md
+rm -rf tuto_execution
