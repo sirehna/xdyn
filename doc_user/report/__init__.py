9197de45d977fac2fe324ad1e03ee34243103c26
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-03-30 15:07:58 +0200

    [doc_pweave] Added report python file __init__.py

---
+++ doc_user/report/__init__.py
@@ -0,0 +1,17 @@
+# Requirements:
+# - pandas
+# - xlrd
+# - xlwt
+# - h5py
+# - jinja2
+# - matplotlib
+# - PyYAML
+
+# Also requires to have pandoc in the path
+# from . import global_settings
+
+from . import ApiGraphJinja
+from . import ApiDataJinja
+from . import ApiDataFormatting
+from . import ApiJinjaCommands
+from . import JinJaReport
