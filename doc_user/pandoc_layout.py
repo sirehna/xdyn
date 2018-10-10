8728a3fc9400843c84873b994121092dc689cecb
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-10-10 09:44:04 +0200

    [pweave] Added a python script to number equations with pandoc-eqnos

---
+++ doc_user/pandoc_layout.py
@@ -0,0 +1,64 @@
+import re
+
+
+text = """
+Equation 1 : $`x=\pi`$
+Equation 2 : $`y=\int x dx`$
+
+```math
+y=\int x dx
+```
+
+"""
+
+def equation_substitution(text):
+    eq_inline = re.compile('\$`(.*?)`\$')
+    eq_multiline = re.compile('^```math(.*?)^```', re.MULTILINE | re.DOTALL)
+    new_text = re.sub(eq_inline, r'$\1$', text)
+    new_text = re.sub(eq_multiline, r'$$\1$$  {#eq:}', new_text)
+    return new_text
+
+
+def equation_substitution2(text):
+    eq_multiline = re.compile('\n\$\$(.*?)\$\$', re.MULTILINE | re.DOTALL)
+    new_text = re.sub(eq_multiline, r'$$\1$$ {#eq:}\n', text)
+    return new_text
+
+
+def generate_metadata(**kwargs):
+    from textwrap import dedent
+    metadata = r"""
+    ---
+    numbersections: true
+    eqnos-cleveref: true
+    eqnos-plus-name: Eq.
+    xnos-number-sections: true
+    link-citations: true
+    ---
+    """
+    return dedent(metadata)
+
+
+def convert_file_for_pandoc(filename,
+    filename_output):
+    if filename_output is None or len(filename_output) == 0:
+        filename_output = filename + '.pandoc'
+    content = open(filename, 'r').read()
+    f = open(filename_output, 'w')
+    content = equation_substitution2(content)
+    content = generate_metadata() + content
+    f.write(content)
+    f.close()
+
+
+if __name__=='__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    pa = parser.add_argument
+    pa('input_filename', type=str, help='')
+    pa('-o', '--output', type=str,
+       help='Name of the output result file',
+       default='')
+    args = parser.parse_args()
+    convert_file_for_pandoc(args.input_filename, args.output)
+
