fb24d4d2577ccce1748ec216656147d3b0338dab
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-10-10 14:41:25 +0200

    [doc] Deleted metadata generation. Now we use a dedicated file

--- doc_user/pandoc_layout.py
+++ doc_user/pandoc_layout.py
@@ -15,31 +15,13 @@ def equation_substitution2(text):
     return new_text
 
 
-def generate_metadata(**kwargs):
-    from textwrap import dedent
-    metadata = r"""
-    ---
-    title: Simulateur X-Dyn
-    author: Sirehna
-    numbersections: true
-    eqnos-cleveref: true
-    eqnos-plus-name: Eq.
-    xnos-number-sections: true
-    link-citations: true
-    ---
-    """
-    return dedent(metadata)
-
-
 def convert_file_for_pandoc(filename,
     filename_output):
     if filename_output is None or len(filename_output) == 0:
         filename_output = filename + '.pandoc'
-    lines = open(filename, 'r').readlines()
-    content = '\n'.join(lines[2:])
+    content = open(filename, 'r').read()
     f = open(filename_output, 'w')
     content = equation_substitution2(content)
-    content = generate_metadata() + content
     f.write(content)
     f.close()
 
