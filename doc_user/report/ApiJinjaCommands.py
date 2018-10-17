d5fd82da3bbf8ffe139374f15f7b715fbf510c96
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2018-10-17 12:10:56 +0200

    [pweave] Added the possibility to display program output

--- doc_user/report/ApiJinjaCommands.py
+++ doc_user/report/ApiJinjaCommands.py
@@ -3,13 +3,20 @@ import subprocess
 import functools
 
 
-def execCmd(command_line, working_directory, verbose):
+def execCmd(command_line, working_directory, echo_output=False, verbose=False):
     if verbose:
         print("Executing: " + command_line)
         print("in directory: " + working_directory)
-    process = subprocess.Popen(command_line, cwd=working_directory, shell=True)
-    process.wait()
-    return "\n~~~~~~~~~~~~~{{.bash}}\n{}\n~~~~~~~~~~~~~".format(command_line)
+    process = subprocess.Popen(command_line, cwd=working_directory, shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    output, err = process.communicate()
+    output = output.decode()
+    err = err.decode()
+    result = "\n~~~~~~~~~~~~~{{.bash}}\n{}\n~~~~~~~~~~~~~".format(command_line)
+    if echo_output:
+        result += '\n    ' + '\n    '.join(output.split('\n'))
+    return result
 
 
 def api(params):
