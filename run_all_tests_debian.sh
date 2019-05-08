55292f6726df80ab2f14c56f70684c6535af4130
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2019-05-08 09:49:26 +0200

    Updated script run_all_tests_debian.sh

--- run_all_tests_debian.sh
+++ run_all_tests_debian.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 # Using --security-opt seccomp=unconfined to avoid GDB error: warning: Error disabling address space randomization: Operation not permitted
 # As per https://stackoverflow.com/questions/35860527/warning-error-disabling-address-space-randomization-operation-not-permitted
-docker run $TERMINAL --security-opt seccomp=unconfined --rm -u $( id -u $USER ):$( id -g $USER ) -v $(pwd)/build_debian:/build -w /build -v $(pwd):/opt/share -t build-xdyn-debian ./run_all_tests `echo $*`
+docker run $TERMINAL \
+    --security-opt seccomp=unconfined \
+    --rm \
+    -u $( id -u $USER ):$( id -g $USER ) \
+    -v $(pwd)/build_debian:/build \
+    -w /build \
+    -v $(pwd):/opt/share \
+    -t \
+    sirehna/base-image-debian9-gcc6-xdyn \
+    ./run_all_tests `echo $*`
