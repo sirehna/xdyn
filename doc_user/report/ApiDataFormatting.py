60377caeb540a1b0e57a74163b3222bfa9a802e8
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2019-05-15 10:02:12 +0200

    Updated Python yaml.load for security reason
    It created a nasty warning message in documentation
    
    See https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- doc_user/report/ApiDataFormatting.py
+++ doc_user/report/ApiDataFormatting.py
@@ -6,13 +6,17 @@ import os
 
 
 def load_yaml(path_yaml, original_path):
+    # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation
+    # Loader=yaml.BaseLoader
+    # Loader=yaml.SafeLoader
+    # Loader=yaml.FullLoader
+    # Loader=yaml.UnsafeLoader
     with open(os.path.join(original_path, path_yaml), 'r') as f:
         content = f.read()
-    return yaml.load(content)
+    return yaml.load(content, Loader=yaml.SafeLoader)
 
 
 def extract_yaml(yaml_data, dict_path):
-
     keys_list = [elt if re.search("[^0-9]", elt) else int(elt) for elt in dict_path.split('/')]
     return functools.reduce(lambda x, y: x[y], keys_list, yaml_data)
 
