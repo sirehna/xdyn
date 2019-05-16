b4c1b6756b30b9321a81bc34581be371866e92be
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-05-16 10:31:42 +0200

    Fixed volume mount in run_all_tests_windows.sh

--- run_all_tests_windows.sh
+++ run_all_tests_windows.sh
@@ -3,7 +3,7 @@ docker run \
     --name run-all-tests-windows \
     --rm \
     -u $( id -u $USER ):$( id -g $USER ) \
-    -v $(pwd):/opt/share \
+    -v $(pwd)/build_win:/opt/share \
     -w /opt/share \
     sirehna/base-image-win64-gcc540-win32threads-ssc-xdyn \
-    /bin/bash -c "mkdir -p /opt/share/.wine && export WINEPREFIX=/opt/share/.wine && wine winecfg && cd build_windows && wine ./run_all_tests `echo $*`"
+    /bin/bash -c "mkdir -p /opt/share/.wine && export WINEPREFIX=/opt/share/.wine && wine winecfg && wine ./run_all_tests `echo $*`"
