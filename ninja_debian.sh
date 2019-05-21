7512ee44a19a500ce27b396692609bf2a79e2141
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-05-21 10:05:07 +0200

    No need to specify $USER in id -u & id -g

--- ninja_debian.sh
+++ ninja_debian.sh
@@ -2,8 +2,8 @@
 docker run \
     --name xdyn-ninja-debian \
     --rm \
-    -u $( id -u $USER ):$( id -g $USER ) \
-    -v $(pwd)/build_debian:/build \
+    -u $( id -u ):$( id -g ) \
+    -v $(pwd)/build_deb8:/build \
     -w /build \
     -v $(pwd):/opt/share \
     sirehna/base-image-debian9-gcc6-xdyn \
