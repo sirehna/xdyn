fda000540e8242d783f8fbd979f9cf8de337408c
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-05-21 10:05:33 +0200

    Removed useless build step from ninja_debian.sh

--- ninja_debian.sh
+++ ninja_debian.sh
@@ -7,4 +7,4 @@ docker run \
     -w /build \
     -v $(pwd):/opt/share \
     sirehna/base-image-debian9-gcc6-xdyn \
-    /bin/sh -c "cd /opt/share/doc_user/images && make -i > /dev/null 2> /dev/null && cd /build && ninja `echo $*`"
+    /bin/sh -c "cd /build && ninja `echo $*`"
