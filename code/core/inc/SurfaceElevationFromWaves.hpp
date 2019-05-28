3f67f22264dc86785933b07b682681f08c45ff1e
Lea Lincker <lea.lincker@sirehna.com>
2019-05-28 15:44:52 +0200

    Added vectorized dynamic pressure method in SurfaceElevationFromWaves files

--- code/core/inc/SurfaceElevationFromWaves.hpp
+++ code/core/inc/SurfaceElevationFromWaves.hpp
@@ -104,6 +104,31 @@ class SurfaceElevationFromWaves: public SurfaceElevationInterface
                                 const double t    //!< Current time instant (in seconds)
                                 ) const;
 
+        /**  \brief Unsteady pressure field induced by undisturbed waves. Used to compute the Froude-Krylov forces.
+          *  \details Also called "subsurface pressure" (by DNV), "unsteady pressure" (by Faltinsen) or constant pressure contour (by Lloyd)
+          *           The dynamic pressure is in fact one of the terms of Bernoulli's equation, which can be derived from the conservation
+          *           of energy for a fluid in motion.
+          *           \f[\int_C \frac{\partial \Phi_I(x,y,z,t)}{\partial t}\f]
+          *  \returns Dynamic pressure in Pascal
+          *  \see "Environmental Conditions and Environmental Loads", April 2014, DNV-RP-C205, Det Norske Veritas AS, page 47
+          *  \see "Hydrodynamique des Structures Offshore", 2002, Bernard Molin, Editions TECHNIP, page 76
+          *  \see "Sea Loads on Ships and Offshore Structures", 1990, O.M. Faltinsen, Cambridge Ocean Technology Series, page 16
+          *  \see "Hydrodynamique navale : théorie et modèles", 2009, Alain Bovis, Les Presses de l'ENSTA, equation VI.34, page 183
+          *  \see "Seakeeping: ship behaviour in rough weather", 1989, A. R. J. M. Lloyd, Ellis Horwood Series in Marine Technology, page 68
+         *  \section ex1 Example
+         *  \snippet core/unit_tests/src/SurfaceElevationFromWavesTest.cpp SurfaceElevationFromWavesTest dynamic_pressure example
+         *  \section ex2 Expected output
+         *  \snippet core/unit_tests/src/SurfaceElevationFromWavesTest.cpp SurfaceElevationFromWavesTest dynamic_pressure expected output
+         *  */
+        std::vector<double> dynamic_pressure(const double rho,               //!< water density (in kg/m^3)
+                                             const double g,                 //!< gravity (in m/s^2)
+                                             const std::vector<double> &x,   //!< x-positions in the NED frame (in meters)
+                                             const std::vector<double> &y,   //!< y-positions in the NED frame (in meters)
+                                             const std::vector<double> &z,   //!< z-positions in the NED frame (in meters)
+                                             const std::vector<double> &eta, //!< Wave elevations at (x,y) in the NED frame (in meters)
+                                             const double t                  //!< Current time instant (in seconds)
+                                             ) const;
+
         std::vector<WaveModelPtr> models;
 };
 #endif /* SURFACEELEVATIONFROMWAVES_HPP_ */
--- code/core/src/SurfaceElevationFromWaves.cpp
+++ code/core/src/SurfaceElevationFromWaves.cpp
@@ -109,6 +109,33 @@ double SurfaceElevationFromWaves::dynamic_pressure(const double rho, //!< water 
     return pdyn;
 }
 
+std::vector<double> SurfaceElevationFromWaves::dynamic_pressure(const double rho,               //!< water density (in kg/m^3)
+                                                                const double g,                 //!< gravity (in m/s^2)
+                                                                const std::vector<double> &x,   //!< x-positions in the NED frame (in meters)
+                                                                const std::vector<double> &y,   //!< y-positions in the NED frame (in meters)
+                                                                const std::vector<double> &z,   //!< z-positions in the NED frame (in meters)
+                                                                const std::vector<double> &eta, //!< Wave elevations at (x,y) in the NED frame (in meters)
+                                                                const double t                  //!< Current time instant (in seconds)
+                                                                ) const
+{
+    if (x.size() != y.size() || x.size() != z.size() || x.size() != eta.size())
+    {
+        THROW(__PRETTY_FUNCTION__, InternalErrorException,
+              "Error when calculating dynamic pressure from waves: the x, y, z and eta vectors don't have the same size (size of x: " << x.size()
+                  << ", size of y: " << y.size() << ", size of z: " << z.size() << ", size of eta: " << eta.size() << ")");
+    }
+    std::vector<double> pdyn(x.size(), 0);
+    for (const auto model : models)
+    {
+        std::vector<double> dynamic_pressure_for_model = model->dynamic_pressure(rho, g, x, y, z, eta, t);
+        for (size_t i = 0; i < pdyn.size(); ++i)
+        {
+            pdyn[i] += dynamic_pressure_for_model.at(i);
+        }
+    }
+    return pdyn;
+}
+
 ssc::kinematics::Point SurfaceElevationFromWaves::orbital_velocity(const double g,   //!< gravity (in m/s^2)
                                                                    const double x,   //!< x-position in the NED frame (in meters)
                                                                    const double y,   //!< y-position in the NED frame (in meters)
