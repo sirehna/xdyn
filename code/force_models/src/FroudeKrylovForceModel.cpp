4eb08d2a9c46c86256b18679cba98ec7af3f2134
Lea Lincker <lea.lincker@sirehna.com>
2019-06-04 15:51:38 +0200

    Vectorized FroudeKrylovForceModel::operator() to directly use vectorized get_dynamic_pressure

--- code/force_models/src/FroudeKrylovForceModel.cpp
+++ code/force_models/src/FroudeKrylovForceModel.cpp
@@ -8,6 +8,8 @@
 #include "Body.hpp"
 #include "FroudeKrylovForceModel.hpp"
 #include "SurfaceElevationInterface.hpp"
+#include "InternalErrorException.hpp"
+#include <ssc/exception_handling.hpp>
 
 std::string FroudeKrylovForceModel::model_name() {return "non-linear Froude-Krylov";}
 
@@ -26,9 +28,39 @@ ssc::kinematics::Wrench FroudeKrylovForceModel::operator()(const BodyStates &sta
     const double orientation_factor = states.intersector->mesh->orientation_factor;
     const auto e = end(states.intersector);
 
+    std::vector<EPoint> dS;
+    std::vector<ssc::kinematics::Point> C;
+    // Calculate facet area and centre of gravity
     for (auto that_facet = begin(states.intersector); that_facet != e; ++that_facet)
     {
-        const DF f = dF(that_facet, env, states, t);
+        dS.push_back(EPoint(that_facet->area * that_facet->unit_normal));
+        C.push_back(ssc::kinematics::Point(states.M->get_frame(), that_facet->centre_of_gravity));
+    }
+    // Compute average elevation for each facet
+    std::vector<double> average_eta_per_facet;
+    for (auto that_facet = begin(states.intersector); that_facet != e; ++that_facet)
+    {
+        double eta_facet = 0;
+        for (auto it = that_facet->vertex_index.begin(); it != that_facet->vertex_index.end(); ++it)
+        {
+            eta_facet += states.intersector->all_absolute_wave_elevations.at(*it);
+        }
+        if (not(that_facet->vertex_index.empty()))
+            eta_facet /= (double)that_facet->vertex_index.size();
+        average_eta_per_facet.push_back(eta_facet);
+    }
+
+    const std::vector<double> pdyn = env.w->get_dynamic_pressure(env.rho, env.g, C, env.k, average_eta_per_facet, t);
+
+    if (pdyn.size() != C.size())
+    {
+        THROW(__PRETTY_FUNCTION__, InternalErrorException,
+              "Error when getting dynamic pression in Froude-Krylov force model: the points vector and the dynamic pressure vector don't have the same size (size of P: "
+                  << C.size() << ", size of pdyn: " << pdyn.size() << ")")
+    }
+    for (size_t i = 0; i < pdyn.size(); ++i)
+    {
+        const DF f = DF(-pdyn.at(i) * dS.at(i), C.at(i).v);
         const double x = (f.C(0) - states.G.v(0));
         const double y = (f.C(1) - states.G.v(1));
         const double z = (f.C(2) - states.G.v(2));
