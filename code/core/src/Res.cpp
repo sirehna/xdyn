710f5281b972a334092f9e18312f3a250d65b565
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-07-09 12:07:14 +0200

    :hammer: Added extra_observations to Res

--- code/core/src/Res.cpp
+++ code/core/src/Res.cpp
@@ -7,11 +7,11 @@
 
 #include "Res.hpp"
 
-Res::Res() : t(0), x()
+Res::Res() : t(0), x(), extra_observations()
 {
 }
 
-Res::Res(const double t_, const StateType& x_):t(t_),x(x_)
+Res::Res(const double t_, const StateType& x_) : t(t_), x(x_), extra_observations()
 {
 }
 
