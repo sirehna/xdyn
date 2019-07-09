a42b1f50bc92d464d24418569d0e0aacf105e3ac
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-07-09 16:20:55 +0200

    :fire: Removed useless comments from ConfBuilderTest

--- code/observers_and_api/unit_tests/src/ConfBuilderTest.cpp
+++ code/observers_and_api/unit_tests/src/ConfBuilderTest.cpp
@@ -21,15 +21,7 @@ void ConfBuilderTest::TearDown()
 
 TEST_F(ConfBuilderTest, compute_right_value_of_Tmax_falling_ball)
 {
-//! [ConfBuilderTest example]
-//! [ConfBuilderTest example]
     ConfBuilder builder(test_data::falling_ball_example());
-
     ASSERT_EQ(builder.Tmax, 0);
-
-
-//! [ConfBuilderTest expected output]
-//! [ConfBuilderTest expected output]
-
 
 }
