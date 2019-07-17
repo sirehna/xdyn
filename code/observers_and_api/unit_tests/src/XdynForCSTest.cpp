f91fda0c64b0e9129c7cf5137289edfbbdd9a615
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-07-17 14:49:12 +0200

    :white_check_mark: Improved XdynForCSTest.can_get_extra_observations

--- code/observers_and_api/unit_tests/src/XdynForCSTest.cpp
+++ code/observers_and_api/unit_tests/src/XdynForCSTest.cpp
@@ -73,5 +73,10 @@ TEST_F(XdynForCSTest, can_get_extra_observations)
     SimServer sim_server(yaml_model, unit_cube(), solver, dt);
     const std::vector<YamlState> outputs = sim_server.play_one_step(test_data::complete_yaml_message_for_falling_ball());
     ASSERT_EQ(11, outputs.size());
-    ASSERT_FALSE(outputs.front().extra_observations.empty());
+    ASSERT_FALSE(outputs.front().extra_observations. empty());
+    for (const auto output:outputs)
+    {
+        ASSERT_NE(output.extra_observations.find("GM(cube)"), output.extra_observations.end());
+        ASSERT_NE(output.extra_observations.find("GZ(cube)"), output.extra_observations.end());
+    }
 }
