e1c90d0c51072f1f2cc76ebdf65bf7670e8d0a1a
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2019-07-19 14:38:49 +0200

    :books: Updated comment on phase(i_freq)(j_dir)

--- code/environment_models/inc/DiscreteDirectionalWaveSpectrum.hpp
+++ code/environment_models/inc/DiscreteDirectionalWaveSpectrum.hpp
@@ -29,7 +29,7 @@ struct DiscreteDirectionalWaveSpectrum
     std::vector<double> omega;                                  //!< Angular frequencies the spectrum was discretized at (in rad/s)
     std::vector<double> psi;                                    //!< Directions between 0 & 2pi the spatial spreading was discretized at (in rad)
     std::vector<double> k;                                      //!< Discretized wave number (for each frequency) in rad/m
-    std::vector<std::vector<double> > phase;                    //!< Random phases, for each (direction,frequency) couple (but time invariant) in radian
+    std::vector<std::vector<double> > phase;                    //!< Random phases, for each (frequency, direction) couple (but time invariant) in radian phases *phase[i_freq][i_dir]*
 
 
     std::function<double(double,double,double)> pdyn_factor;    //!< Factor used when computing the dynamic pressure (no unit)
@@ -37,7 +37,7 @@ struct DiscreteDirectionalWaveSpectrum
 };
 
 /** \brief Used by the wave models (eg. Airy, Stokes, etc.)
-*   \details Avoids having to double loop on frequency and direction. All vectors should have the same size as each element corresponds to a (direction, omega) pair: that way we only need one loop (over, say, i) to go over all possible (omega[i],psi[i]).
+ *  \details Avoids having to double loop on frequency and direction. All vectors should have the same size as each element corresponds to a (direction, omega) pair: that way we only need one loop (over, say, i) to go over all possible (omega[i], psi[i]).
  *  \ingroup wave_models
  *  \section ex1 Example
  *  \snippet environment_models/unit_tests/src/DirectionalWaveSpectrumTest.cpp DirectionalWaveSpectrumTest example
@@ -53,7 +53,7 @@ struct FlatDiscreteDirectionalWaveSpectrum
     std::vector<double> cos_psi; //!< Cosinus directions between 0 & 2pi the spatial spreading was discretized at (so we do not compute it each time), for each angular frequency omega, and direction
     std::vector<double> sin_psi; //!< Sinus directions between 0 & 2pi the spatial spreading was discretized at (so we do not compute it each time), for each angular frequency omega, and direction
     std::vector<double> k;       //!< Discretized wave number (for each frequency) in rad/m, for each angular frequency omega, i.e. same size as omega
-    std::vector<double> phase;   //!< Random phases, for each (direction,frequency) couple (but time invariant) in radian, for each angular frequency omega, and direction
+    std::vector<double> phase;   //!< Random phases, for each (frequency, direction) couple (but time invariant) in radian, for each angular frequency omega, and direction
     std::function<double(double,double,double)> pdyn_factor;    //!< Factor used when computing the dynamic pressure (no unit)
     std::function<double(double,double,double)> pdyn_factor_sh; //!< Factor used when computing the orbital velocity (no unit)
 };
