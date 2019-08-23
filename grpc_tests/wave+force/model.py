c334902c8f1624d06bae085dcfe247a7402fdb8e
Guillaume Jacquenot <guillaume.jacquenot@sirehna.com>
2019-08-23 18:45:32 +0200

    :bug: Fixed demo grpc_tests/wave+force/model.py

--- grpc_tests/wave+force/model.py
+++ grpc_tests/wave+force/model.py
@@ -1,9 +1,9 @@
-"""Submarine hydrostatic effort"""
+"""Extra observation model"""
 
 import yaml
 import force
 import logging
-SERVICE_NAME = "sm_rao_qtf.py"
+SERVICE_NAME = "model.py"
 
 logging.basicConfig(
     format='%(asctime)s,%(msecs)d ['
@@ -17,40 +17,32 @@ LOGGER.setLevel(logging.INFO)
 class Model(force.Model):
     """
     """
-    def __init__(self):
-        """Initialize parameters from 'set_parameters' to None."""
-        pass
+    def __init__(self, _, body_name):
+        self.body_name = body_name
 
-    def set_parameters(self, parameters, body_name):
+    def get_parameters(self):
         return {'max_history_length': 0, 'needs_wave_outputs': True,
-                'commands': [],
-                'frame': body_name, 'x': 0, 'y': 0, 'z': 0, 'phi': 0, 'theta': 0, 'psi': 0}
+                'required_commands': [],
+                'frame': self.body_name, 'x': 0, 'y': 0, 'z': 0, 'phi': 0, 'theta': 0, 'psi': 0}
 
     def required_wave_information(self, t, x, y, z):
         return {'spectrum': {'t':t, 'x':x, 'y': y},
                 'angular_frequencies_for_rao' : True,
                 'directions_for_rao': True}
 
-    def force(self, states, _, wave_information):
+    def force(self, _, __, wave_information):
         """Force model."""
 
-        Fx = 0.0
-        Fy = 0.0
-        Fz = 0.0
-        Mx = 0.0
-        My = 0.0
-        Mz = 0.0
-
-        res = {'Fx': Fx,
-               'Fy': Fy,
-               'Fz': Fz,
-               'Mx': Mx,
-               'My': My,
-               'Mz': Mz,
+        res = {'Fx': 0.0,
+               'Fy': 0.0,
+               'Fz': 0.0,
+               'Mx': 0.0,
+               'My': 0.0,
+               'Mz': 0.0,
                'extra_observations':{'HF': 666.0, "phase0": wave_information.spectrum.spectrum[0].phase[0].phase[0]}}
 
         return res
 
 
 if __name__ == '__main__':
-    force.serve(Model())
+    force.serve(Model)
