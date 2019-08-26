2b569934e5ad26532720b3bff4c8e3251a573a65
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2019-08-26 09:16:36 +0200

    :hammer: Simplified model.py slightly

--- grpc_tests/wave+force/model.py
+++ grpc_tests/wave+force/model.py
@@ -23,25 +23,24 @@ class Model(force.Model):
     def get_parameters(self):
         return {'max_history_length': 0, 'needs_wave_outputs': True,
                 'required_commands': [],
-                'frame': self.body_name, 'x': 0, 'y': 0, 'z': 0, 'phi': 0, 'theta': 0, 'psi': 0}
+                'frame': self.body_name,
+                'x': 0, 'y': 0, 'z': 0, 'phi': 0, 'theta': 0, 'psi': 0}
 
     def required_wave_information(self, t, x, y, z):
         return {'spectrum': {'t':t, 'x':x, 'y': y},
-                'angular_frequencies_for_rao' : True,
+                'angular_frequencies_for_rao': True,
                 'directions_for_rao': True}
 
     def force(self, _, __, wave_information):
         """Force model."""
 
-        res = {'Fx': 0.0,
-               'Fy': 0.0,
-               'Fz': 0.0,
-               'Mx': 0.0,
-               'My': 0.0,
-               'Mz': 0.0,
-               'extra_observations':{'HF': 666.0, "phase0": wave_information.spectrum.spectrum[0].phase[0].phase[0]}}
-
-        return res
+        return {'Fx': 0.0,
+                'Fy': 0.0,
+                'Fz': 0.0,
+                'Mx': 0.0,
+                'My': 0.0,
+                'Mz': 0.0,
+                'extra_observations': {'HF': 666.0, 'phase0': wave_information.spectrum.spectrum[0].phase[0].phase[0]}}
 
 
 if __name__ == '__main__':
