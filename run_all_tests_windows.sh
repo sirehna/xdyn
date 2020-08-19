f5f6cdf6ed65471c334ab6c60ac3b0c06850b3e5
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2020-08-19 14:45:54 +0200

    :wrench: Fixed dev build scripts for Windows

--- run_all_tests_windows.sh
+++ run_all_tests_windows.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-docker run \
-    --name run-all-tests-windows \
-    --rm \
-    -u $( id -u $USER ):$( id -g $USER ) \
-    -v $(pwd)/build_win:/opt/share \
+docker run  --rm \
+    -u $(id -u):$(id -g) \
+    -v $(pwd):/opt/share \
     -w /opt/share \
-    sirehna/base-image-win64-gcc540-win32threads-ssc-xdyn \
-    /bin/bash -c "mkdir -p /opt/share/.wine && export WINEPREFIX=/opt/share/.wine && wine winecfg && wine ./run_all_tests `echo $*`"
+    sirehna/base-image-win64-gccx-posixthreads-ssc-xdyn /bin/bash -c \
+       "cd build_win_posix &&\
+        mkdir -p /opt/share/.wine;\
+        export WINEPREFIX=/opt/share/.wine;\
+        wine winecfg;\
+        wine ./run_all_tests `echo $*`"
