e6abab658b56707d7afe34c1b1d0cdd3620a228f
Charles-Edouard Cady <charles-edouard.cady@sirehna.com>
2020-08-19 16:48:01 +0200

    :wrench: Can launch run_all_tests using valgrind

---
+++ run_all_tests_valgrind.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Using --security-opt seccomp=unconfined to avoid GDB error: warning: Error disabling address space randomization: Operation not permitted
+# As per https://stackoverflow.com/questions/35860527/warning-error-disabling-address-space-randomization-operation-not-permitted
+docker build -t valgrind -f Dockerfile.valgrind .
+docker run $TERMINAL \
+    --security-opt seccomp=unconfined \
+    --rm \
+    -u $( id -u $USER ):$( id -g $USER ) \
+    -v $(pwd)/build_deb10_dbg:/build \
+    -w /build \
+    -t \
+    valgrind \
+    /bin/bash -c "export LD_LIBRARY_PATH=/build; valgrind --leak-check=full --track-origins=yes --error-exitcode=1 ./run_all_tests `echo $*`"
