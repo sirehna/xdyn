d90330494c62e8cce6522b27379d8fa5a1ec164b
Moran Charlou <moran.charlou@ec-nantes.fr>
2020-12-11 16:23:03 +0100

    - Added basic unit tests for LogWindVelocityProfile
    Signed-off-by: Moran Charlou <moran.charlou@ec-nantes.fr>

--- code/environment_models/unit_tests/src/WindMeanVelocityProfileTest.cpp
+++ code/environment_models/unit_tests/src/WindMeanVelocityProfileTest.cpp
@@ -6,14 +6,15 @@
  */
 
 #include <string>
-#include <math.h>
+#include <cmath>
 
 #include "UniformWindVelocityProfile.hpp"
 #include "PowerLawWindVelocityProfile.hpp"
+#include "LogWindVelocityProfile.hpp"
 
 #include "WindMeanVelocityProfileTest.hpp"
 
-WindMeanVelocityProfileTest::WindMeanVelocityProfileTest() : a(ssc::random_data_generator::DataGenerator(54200))
+WindMeanVelocityProfileTest::WindMeanVelocityProfileTest() : a(ssc::random_data_generator::DataGenerator(53217))
 {}
 
 WindMeanVelocityProfileTest::~WindMeanVelocityProfileTest()
@@ -69,6 +70,34 @@ TEST_F(WindMeanVelocityProfileTest, PowerLawWindVelocityProfile_returns_right_va
 	double time(a.random<double>());
 	Eigen::Vector3d wind_vector(wind_model.get_wind(position,time));
 	double wind_velocity = input.velocity*pow(-position(2)/input.z_ref,input.alpha);
+	ASSERT_DOUBLE_EQ(wind_velocity*cos(input.direction),wind_vector(0));
+	ASSERT_DOUBLE_EQ(wind_velocity*sin(input.direction),wind_vector(1));
+	ASSERT_DOUBLE_EQ(0.,wind_vector(2));
+}
+
+TEST_F(WindMeanVelocityProfileTest, can_parse_LogWindVelocityProfile_input)
+{
+	std::string yaml_input = "{velocity: {unit: m/s, value: 8}, direction: {unit: rad, value: 1}, roughness length: {unit: m, value: 0.005}, reference height: {unit: m, value: 10}}";
+	auto input = LogWindVelocityProfile::parse(yaml_input);
+	ASSERT_DOUBLE_EQ(input.velocity,8);
+	ASSERT_DOUBLE_EQ(input.direction,1);
+	ASSERT_DOUBLE_EQ(input.z_ref,10);
+	ASSERT_DOUBLE_EQ(input.z0,0.005);
+}
+
+TEST_F(WindMeanVelocityProfileTest, LogWindVelocityProfile_returns_right_value)
+{
+	LogWindVelocityProfile::Input input;
+	input.velocity = 8;
+	input.direction = 135*M_PI/180;
+	input.z0 = 0.005;
+	input.z_ref = 10;
+	LogWindVelocityProfile wind_model(input);
+	Eigen::Vector3d position;
+	position << a.random<double>(), a.random<double>(), -abs(a.random<double>());
+	double time(a.random<double>());
+	Eigen::Vector3d wind_vector(wind_model.get_wind(position,time));
+	double wind_velocity = input.velocity*log(-position(2)/input.z0)/log(input.z_ref/input.z0);
 	ASSERT_DOUBLE_EQ(wind_velocity*cos(input.direction),wind_vector(0));
 	ASSERT_DOUBLE_EQ(wind_velocity*sin(input.direction),wind_vector(1));
 	ASSERT_DOUBLE_EQ(0.,wind_vector(2));
