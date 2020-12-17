7cad37a3042132eecf4eeb9c9eb3ab384adc6296
Moran Charlou <moran.charlou@ec-nantes.fr>
2020-12-17 17:09:54 +0100

    - Added basic unit tests to check the behavior of get_rho_air in EnvironmentAndFrames
    Signed-off-by: Moran Charlou <moran.charlou@ec-nantes.fr>

---
+++ code/core/unit_tests/inc/EnvironmentAndFramesTest.hpp
@@ -0,0 +1,20 @@
+/*
+ * EnvironmentAndFramesTest.hpp
+ *
+ *  Created on: 17 déc. 2020
+ *      Author: mcharlou2016
+ */
+
+#ifndef CORE_UNIT_TESTS_INC_ENVIRONMENTANDFRAMESTEST_HPP_
+#define CORE_UNIT_TESTS_INC_ENVIRONMENTANDFRAMESTEST_HPP_
+
+#include "gtest/gtest.h"
+
+class EnvironmentAndFramesTest : public ::testing::Test
+{
+public:
+	EnvironmentAndFramesTest ();
+	virtual ~EnvironmentAndFramesTest ();
+};
+
+#endif /* CORE_UNIT_TESTS_INC_ENVIRONMENTANDFRAMESTEST_HPP_ */
